82c3d35564466225cc1062892715968d771297e2
PJ <pjcook@users.noreply.github.com>
2019-12-10 14:25:45 +0000

    Day10
    Signed-off-by: PJ <pjcook@users.noreply.github.com>

--- AdventOfCode/Year2019/Day10.swift
+++ AdventOfCode/Year2019/Day10.swift
@@ -7,3 +7,125 @@
 //
 
 import Foundation
+
+extension CGPoint: Hashable {
+    func angle(to point: CGPoint, _ startAngle: Double = 0) -> Double {
+        let dx = point.x - x
+        let dy = point.y - y
+        let angle = Double(atan2f(Float(dy), Float(dx))) + startAngle
+        if angle < 0 { return angle + degreesToRadians(360) }
+        return angle
+    }
+    
+    func distance(to point: CGPoint) -> Float {
+        let dx = point.x - x
+        let dy = point.y - y
+        return Float(sqrt(dx * dx + dy * dy))
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
+}
+
+func radiansToDegrees(_ number: Double) -> Double {
+    return number * 180 / .pi
+}
+
+func degreesToRadians(_ number: Double) -> Double {
+    return number * .pi / 180
+}
+
+func readAsteroidMap(_ input: [[String]]) -> [CGPoint] {
+    var asteroidPoints = [CGPoint]()
+    for y in 0..<input.count {
+        for x in 0..<input[y].count {
+            if input[y][x] == "#" {
+                asteroidPoints.append(CGPoint(x: x, y: y))
+            }
+        }
+    }
+    return asteroidPoints
+}
+
+func calculateBestAsteroidBaseCount(_ asteroidPoints: [CGPoint]) -> (Int, CGPoint) {
+    var bestAsteroid = CGPoint.zero
+    var bestCount = 0
+    for asteroid in asteroidPoints {
+        var count = 0
+        var otherAsteroidPoints = asteroidPoints.filter { $0 != asteroid }
+        while !otherAsteroidPoints.isEmpty {
+            let coords = otherAsteroidPoints.removeFirst()
+            let angle = asteroid.angle(to: coords)
+            let otherCGPoints = otherAsteroidPoints.filter { asteroid.angle(to: $0) == angle }
+            count += 1
+            otherAsteroidPoints.removeAll { otherCGPoints.contains($0) }
+        }
+        if count > bestCount {
+            bestCount = count
+            bestAsteroid = asteroid
+        }
+    }
+
+    return (bestCount, bestAsteroid)
+}
+
+extension Array where Element: Hashable {
+    func removingDuplicates() -> [Element] {
+        var dict = [Element: Bool]()
+        return filter { dict.updateValue(true, forKey: $0) == nil }
+    }
+    
+    mutating func removeDuplicates() {
+        self = self.removingDuplicates()
+    }
+}
+
+func drawStarMap(size: CGSize, points: [CGPoint], dead: [CGPoint], station: CGPoint) {
+    var map = ""
+    for y in 0..<Int(size.height) {
+        var row = ""
+        for x in 0..<Int(size.width) {
+            let p = CGPoint(x: x, y: y)
+            if points.first(where: { $0 == p }) != nil {
+                row += "🟤"
+            } else if p == station {
+                row += "🔆"
+            } else if dead.first(where: { $0 == p }) != nil {
+                row += "🔴"
+            } else {
+                row += "⚫️"
+            }
+        }
+        map += row + "\n"
+    }
+    print(map + "\n")
+}
+
+func vaporizeAsteroids(_ asteroidPoints: [CGPoint], stationCoords: CGPoint, mapSize: CGSize) -> [CGPoint] {
+    var count = 0
+    var otherAsteroids = [CGPoint:(Float, Double)]()
+    var deadAsteroids = [CGPoint]()
+    let startAngle = degreesToRadians(90.1)
+    _ = asteroidPoints.map {
+        if $0 != stationCoords {
+            otherAsteroids[$0] = (stationCoords.distance(to: $0), stationCoords.angle(to: $0, startAngle))
+        }
+    }
+    let angles = otherAsteroids.map { $0.value.1 }.removingDuplicates().sorted()
+    drawStarMap(size: mapSize, points: asteroidPoints, dead: deadAsteroids, station: stationCoords)
+    while !otherAsteroids.isEmpty {
+        for angle in angles {
+            let asteroids = otherAsteroids.filter { $0.value.1 == angle }.sorted(by: { $0.value.0 < $1.value.0 })
+            if !asteroids.isEmpty, let first = asteroids.first?.key {
+                otherAsteroids.removeValue(forKey: first)
+                deadAsteroids.append(first)
+                drawStarMap(size: mapSize, points: otherAsteroids.map { $0.key }, dead: deadAsteroids, station: stationCoords)
+                count += 1
+            }
+        }
+    }
+    
+    return deadAsteroids
+}
--- AdventOfCode/Year2019Tests/Day10Tests.swift
+++ AdventOfCode/Year2019Tests/Day10Tests.swift
@@ -12,24 +12,79 @@ import InputReader
 
 class Day10Tests: XCTestCase {
 
-    func test_part1_sample_data1() {
-        
+    func test_part1_sample_data1() throws {
+        let input = try readInput(filename: "Day10_sample1.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let (maxCount, _) = calculateBestAsteroidBaseCount(asteroidPoints)
+        XCTAssertEqual(8, maxCount)
     }
     
-    func test_part1_sample_data2() {
-        
+    func test_part1_sample_data2() throws {
+        let input = try readInput(filename: "Day10_sample2.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let (maxCount, _) = calculateBestAsteroidBaseCount(asteroidPoints)
+        XCTAssertEqual(33, maxCount)
     }
     
-    func test_part1_sample_data3() {
-        
+    func test_part1_sample_data3() throws {
+        let input = try readInput(filename: "Day10_sample3.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let (maxCount, _) = calculateBestAsteroidBaseCount(asteroidPoints)
+        XCTAssertEqual(35, maxCount)
     }
     
+    func test_part1_sample_data4() throws {
+        let input = try readInput(filename: "Day10_sample4.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let (maxCount, _) = calculateBestAsteroidBaseCount(asteroidPoints)
+        XCTAssertEqual(41, maxCount)
+    }
+    
+    func test_part1_sample_data5() throws {
+        let input = try readInput(filename: "Day10_sample5.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let (maxCount, _) = calculateBestAsteroidBaseCount(asteroidPoints)
+        XCTAssertEqual(210, maxCount)
+    }
+        
     func test_part1() throws {
-        guard let input = try readInput(filename: "Day10.input", delimiter: ",", cast: Int.init, bundle: Year2019.bundle) as? [Int] else { throw Errors.invalidInput }
+        let input = try readInput(filename: "Day10.input", delimiter: "\n", cast: String.init, bundle: Year2019.bundle).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let (maxCount, point) = calculateBestAsteroidBaseCount(asteroidPoints)
+        XCTAssertEqual(267, maxCount)
+        XCTAssertEqual(CGPoint(x: 26, y: 28), point)
+    }
+    
+    func test_part2_sample_data1() throws {
+        let input = try readInput(filename: "Day10_part2_sample1.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let points = vaporizeAsteroids(asteroidPoints, stationCoords: CGPoint(x: 8, y: 3), mapSize: CGSize(width: 17, height: 5))
+        XCTAssertEqual(CGPoint(x: 14, y: 3), points[asteroidPoints.count-1])
+        XCTAssertEqual(1403, points[asteroidPoints.count-1].x * 100 + points[asteroidPoints.count-1].y)
+    }
+    
+    func test_part2_sample_data2() throws {
+        let input = try readInput(filename: "Day10_sample5.input", delimiter: "\n", cast: String.init, bundle: Bundle(for: Self.self)).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let points = vaporizeAsteroids(asteroidPoints, stationCoords: CGPoint(x: 11, y: 13), mapSize: CGSize(width: 20, height: 20))
+        XCTAssertEqual(CGPoint(x: 11, y: 12), points[0])
+        XCTAssertEqual(CGPoint(x: 12, y: 1), points[1])
+        XCTAssertEqual(CGPoint(x: 12, y: 2), points[2])
+        XCTAssertEqual(CGPoint(x: 12, y: 8), points[9])
+        XCTAssertEqual(CGPoint(x: 16, y: 0), points[19])
+        XCTAssertEqual(CGPoint(x: 16, y: 9), points[49])
+        XCTAssertEqual(CGPoint(x: 10, y: 16), points[99])
+        XCTAssertEqual(CGPoint(x: 9, y: 6), points[198])
+        XCTAssertEqual(CGPoint(x: 8, y: 2), points[199])
+        XCTAssertEqual(CGPoint(x: 10, y: 9), points[200])
+        XCTAssertEqual(CGPoint(x: 11, y: 1), points[298])
     }
     
     func test_part2() throws {
-        guard let input = try readInput(filename: "Day10.input", delimiter: ",", cast: Int.init, bundle: Year2019.bundle) as? [Int] else { throw Errors.invalidInput }
+        let input = try readInput(filename: "Day10.input", delimiter: "\n", cast: String.init, bundle: Year2019.bundle).map { $0.map { String($0) } }
+        let asteroidPoints = readAsteroidMap(input)
+        let points = vaporizeAsteroids(asteroidPoints, stationCoords: CGPoint(x: 26, y: 28), mapSize: CGSize(width: 34, height: 34))
+        XCTAssertEqual(CGPoint(x: 22, y: 3), points[200])
+        XCTAssertEqual(2203, points[200].x * 100 + points[200].y)
     }
-
 }
