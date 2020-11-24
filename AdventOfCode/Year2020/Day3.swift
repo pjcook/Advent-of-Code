0f0a478f8500883a58f1d2b2178504532b64f325
PJ <pjcook@users.noreply.github.com>
2020-11-24 11:55:44 +0000

    Setting up 2020

---
+++ AdventOfCode/Year2020/Day3.swift
@@ -0,0 +1,9 @@
+//
+//  Day1.swift
+//  Year2020
+//
+//  Created by PJ on 24/11/2020.
+//  Copyright © 2020 Software101. All rights reserved.
+//
+
+import Foundation
---
+++ AdventOfCode/Year2020/Year2020.h
@@ -0,0 +1,19 @@
+//
+//  Year2020.h
+//  Year2020
+//
+//  Created by PJ on 24/11/2020.
+//  Copyright © 2020 Software101. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Year2020.
+FOUNDATION_EXPORT double Year2020VersionNumber;
+
+//! Project version string for Year2020.
+FOUNDATION_EXPORT const unsigned char Year2020VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Year2020/PublicHeader.h>
+
+
