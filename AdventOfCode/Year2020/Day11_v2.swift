f752244750a0de7669bd844f9e26efd6b26d8a04
PJ <pjcook@users.noreply.github.com>
2020-12-11 12:20:24 +0000

    2020 Day11 trying different solutions

---
+++ AdventOfCode/Year2020/Day11_v2.swift
@@ -0,0 +1,95 @@
+import Foundation
+import StandardLibraries
+
+public struct Day11_v2 {
+    public func part1(_ input: [Point:Character]) -> Int {
+        return calculate(input, visibleSeats: 4, useFilter: true, countOccupied: countOccupied_part1)
+    }
+    
+    public func part2(_ input: [Point:Character]) -> Int {
+        return calculate(input, visibleSeats: 5, useFilter: false, countOccupied: countOccupied_part2)
+    }
+    
+    func calculate(_ input: [Point:Character], visibleSeats: Int, useFilter: Bool, countOccupied: (Point, Point, [Point:Character]) -> Int) -> Int {
+        var previous = input, isSame = true
+        while isSame {
+            let updated = step(previous, visibleSeats: visibleSeats, countOccupied: countOccupied, useFilter: useFilter)
+            isSame = updated != previous
+            previous = updated
+        }
+        return previous.filter({ $0.value == "#" }).count
+    }
+    
+    func step(_ input: [Point:Character], visibleSeats: Int, countOccupied: (Point, Point, [Point:Character]) -> Int, useFilter: Bool) -> [Point:Character] {
+        var output = input
+        let max = Point(x: 96, y: 90)
+        let filtered = useFilter ? input.filter { $0.value == "#" } : input
+        
+        for element in input {
+            let point = element.key
+            let value = element.value
+            guard value != "." else { continue }
+            let count = countOccupied(point, max, filtered)
+            if value == "L", count == 0 {
+                output[point] = "#"
+            } else if value == "#", count >= visibleSeats {
+                output[point] = "L"
+            }
+        }
+        
+        return output
+    }
+    
+    let points: [Point] = [
+        Point(x: -1, y: -1),
+        Point(x: 0, y: -1),
+        Point(x: 1, y: -1),
+        Point(x: -1, y: 0),
+        Point(x: 1, y: 0),
+        Point(x: -1, y: 1),
+        Point(x: 0, y: 1),
+        Point(x: 1, y: 1),
+    ]
+    func countOccupied_part1(_ point: Point, max: Point, input: [Point:Character]) -> Int {
+        return points.reduce(0) { $0 + (input[point + $1] != nil ? 1 : 0) }
+    }
+
+    func countOccupied_part2(_ point: Point, max: Point, input: [Point:Character]) -> Int {
+        var count = 0
+        
+        outerLoop: for p in points {
+            var exit = false
+            var pos = point
+            while !exit {
+                pos = pos + p
+                guard let value = input[pos] else {
+                    exit = true
+                    continue
+                }
+                if value != "." {
+                    count += value == "#" ? 1 : 0
+                    exit = true
+                    if count >= 5 {
+                        break outerLoop
+                    }
+                }
+            }
+        }
+        
+        return count
+    }
+
+    public static func toDict(_ input: [String]) -> [Point:Character] {
+        var output = [Point:Character]()
+
+        var y = 0
+        for line in input {
+            y += 1
+            for (x, value) in line.enumerated() {
+                output[Point(x: x, y: y)] = value
+            }
+        }
+        
+        return output
+    }
+}
--- AdventOfCode/Year2020Tests/Day11Tests.swift
+++ AdventOfCode/Year2020Tests/Day11Tests.swift
@@ -4,6 +4,7 @@ import InputReader
 
 class Day11Tests: XCTestCase {
     let input = Input("Day11.input", Year2020.bundle).lines.compactMap({ $0.compactMap({ Character(extendedGraphemeClusterLiteral: $0) }) })
+    let inputDict = Day11_v2.toDict(Input("Day11.input", Year2020.bundle).lines)
 
     func test_part1() throws {
         let day = Day11()
@@ -47,5 +48,49 @@ class Day11Tests: XCTestCase {
         """.lines.compactMap({ $0.compactMap({ Character(extendedGraphemeClusterLiteral: $0) }) })
         let day = Day11()
         XCTAssertEqual(26, day.part2(input))
+    }
+    
+    func test_part1_example_v2() {
+        let input = """
+        L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL
+        """.lines
+        let day = Day11_v2()
+        XCTAssertEqual(37, day.part1(Day11_v2.toDict(input)))
+    }
+    
+    func test_part1_v2() {
+        let day = Day11_v2()
+        XCTAssertEqual(2243, day.part1(inputDict))
+    }
+    
+    func test_part2_example_v2() {
+        let input = """
+        L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL
+        """.lines
+        let day = Day11_v2()
+        XCTAssertEqual(26, day.part2(Day11_v2.toDict(input)))
+    }
+    
+    func test_part2_v2() {
+        let day = Day11_v2()
+        XCTAssertEqual(2027, day.part2(inputDict))
     }
 }
