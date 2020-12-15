a48d000afb6cb7e94048f37e4c319f4986b24756
PJ <pjcook@users.noreply.github.com>
2020-12-15 06:33:31 +0000

    2020 Day 15

--- AdventOfCode/Year2020/Day15.swift
+++ AdventOfCode/Year2020/Day15.swift
@@ -4,11 +4,26 @@ import StandardLibraries
 public struct Day15 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
-    }
-    
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func solve(_ input: [Int], maxTime: Int = 2020) -> Int {
+        var time = input.count + 1
+        var lastSpoken = 0
+        var numbers = [Int:Int]()
+        
+        for i in 0..<input.count {
+            numbers[input[i]] = i+1
+        }
+
+        while time < maxTime {
+            if let spoken = numbers[lastSpoken] {
+                numbers[lastSpoken] = time
+                lastSpoken = time - spoken
+            } else {
+                numbers[lastSpoken] = time
+                lastSpoken = 0
+            }
+            time += 1
+        }
+        
+        return lastSpoken
     }
 }
--- AdventOfCode/Year2020Tests/Day15Tests.swift
+++ AdventOfCode/Year2020Tests/Day15Tests.swift
@@ -3,14 +3,53 @@ import InputReader
 import Year2020
 
 class Day15Tests: XCTestCase {
-    let input = Input("Day15.input", Year2020.bundle).lines
+    let input = [6,19,0,5,7,13,1]
     let day = Day15()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        measure {
+        XCTAssertEqual(468, day.solve(input))
+        }
     }
     
     func test_part2() {
-        XCTAssertEqual(0, day.part2(input))
+        measure {
+        XCTAssertEqual(1801753, day.solve(input, maxTime: 30000000))
+        }
+    }
+    
+    func test_part1_example1() {
+        let input = [0,3,6]
+        XCTAssertEqual(436, day.solve(input))
+    }
+    
+    func test_part1_example2() {
+        let input = [1,3,2]
+        XCTAssertEqual(1, day.solve(input))
+    }
+    
+    func test_part1_example3() {
+        let input = [2,1,3]
+        XCTAssertEqual(10, day.solve(input))
+    }
+    
+    func test_part1_example4() {
+        let input = [1,2,3]
+        XCTAssertEqual(27, day.solve(input))
+    }
+    
+    func test_part1_example5() {
+        let input = [2,3,1]
+        XCTAssertEqual(78, day.solve(input))
+    }
+    
+    func test_part1_example6() {
+        let input = [3,2,1]
+        XCTAssertEqual(438, day.solve(input))
+    }
+    
+    func test_part1_example7() {
+        let input = [3,1,2]
+        XCTAssertEqual(1836, day.solve(input))
     }
 }
