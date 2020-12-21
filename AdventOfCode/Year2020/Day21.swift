d44fc89fb8a7076f18ba7477207c76f8b6391770
PJ <pjcook@users.noreply.github.com>
2020-12-21 06:10:47 +0000

    2020 Day 21

--- AdventOfCode/Year2020/Day21.swift
+++ AdventOfCode/Year2020/Day21.swift
@@ -4,11 +4,119 @@ import StandardLibraries
 public struct Day21 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(_ input: [String]) throws -> Int {
+        let recipes = try input.map(Recipe.init)
+        
+        var alegens = [String: [String]]()
+        var allIngredients = [String:Int]()
+
+        for recipe in recipes {
+            for alegen in recipe.alegens {
+                let otherRecipes = findRecipesWithAlegen(alegen, recipes: recipes)
+                let matches = findMatchesInAll(recipe, recipes: otherRecipes)
+                let previousMatches = alegens[alegen] ?? []
+                alegens[alegen] = matches + previousMatches
+            }
+            
+            for ingredient in recipe.ingredients {
+                var count = allIngredients[ingredient] ?? 0
+                count += 1
+                allIngredients[ingredient] = count
+            }
+        }
+
+        let alegenFood = Set(alegens.map { $0.value }.reduce([]) { $0 + $1 })
+        for alegen in alegenFood {
+            allIngredients.removeValue(forKey: alegen)
+        }
+        
+        return allIngredients.map { $0.value }.reduce(0, +)
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func part2(_ input: [String]) throws -> String {
+        let recipes = try input.map(Recipe.init)
+        
+        var alegens = [String: Set<String>]()
+
+        for recipe in recipes {
+            for alegen in recipe.alegens {
+                let otherRecipes = findRecipesWithAlegen(alegen, recipes: recipes)
+                let matches = findMatchesInAll(recipe, recipes: otherRecipes)
+                let previousMatches = alegens[alegen] ?? []
+                alegens[alegen] = Set(matches + previousMatches)
+            }
+        }
+
+        while alegens.map({ $0.value }).reduce([], { $0 + $1 }).count > alegens.count {
+            for alegen in alegens.filter({ $0.value.count == 1 }) {
+                for alegen2 in alegens.filter({ $0.value.count > 1 && $0.value.intersection(alegen.value).count == 1 }) {
+                    var list = alegen2.value
+                    list.remove(alegen.value.first!)
+                    alegens[alegen2.key] = list
+                }
+            }
+        }
+        
+        let alegenNames = alegens.keys.sorted()
+        var output = [String]()
+        
+        for name in alegenNames {
+            output.append(alegens[name]!.first!)
+        }
+        
+        return output.joined(separator: ",")
+    }
+}
+
+public extension Day21 {
+    func findRecipesWithAlegen(_ alegen: String, recipes: [Recipe]) -> [Recipe] {
+        var results = [Recipe]()
+        
+        for recipe in recipes {
+            if recipe.alegens.contains(alegen) {
+                results.append(recipe)
+            }
+        }
+        
+        return results
+    }
+    
+    func findMatchesInAll(_ recipe: Recipe, recipes: [Recipe]) -> [String] {
+        var matches = [String]()
+        let match = recipes.count
+        for ingredient in recipe.ingredients {
+            if recipes.filter({ $0.ingredients.contains(ingredient) }).count == match {
+                matches.append(ingredient)
+            }
+        }
+        
+        return matches
+    }
+}
+
+public extension Day21 {
+    struct Recipe: Hashable {
+        public let ingredients: Set<String>
+        public let alegens: Set<String>
+        public static let regex = try! RegularExpression(pattern: #"([a-z]+)"#)
+
+        public init(_ input: String) throws {
+            let matches = Self.regex.matches(in: input)
+            var ingredients = [String]()
+            var alegens = [String]()
+            var foundContains = false
+            for match in matches {
+                let word = try match.string(at: 0)
+                if word == "contains" {
+                    foundContains = true
+                } else if foundContains {
+                    alegens.append(word)
+                } else {
+                    ingredients.append(word)
+                }
+            }
+            self.ingredients = Set(ingredients)
+            self.alegens = Set(alegens)
+        }
     }
 }
--- AdventOfCode/Year2020Tests/Day21Tests.swift
+++ AdventOfCode/Year2020Tests/Day21Tests.swift
@@ -6,11 +6,21 @@ class Day21Tests: XCTestCase {
     let input = Input("Day21.input", Year2020.bundle).lines
     let day = Day21()
 
-    func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+    func test_part1() throws {
+        XCTAssertEqual(2542, try day.part1(input))
     }
     
-    func test_part2() {
-        XCTAssertEqual(0, day.part2(input))
+    func test_part2() throws {
+        XCTAssertEqual("hkflr,ctmcqjf,bfrq,srxphcm,snmxl,zvx,bd,mqvk", try day.part2(input))
+    }
+    
+    func test_part2_example() throws {
+        let input = """
+        mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+        trh fvjkl sbzzf mxmxvkd (contains dairy)
+        sqjhc fvjkl (contains soy)
+        sqjhc mxmxvkd sbzzf (contains fish)
+        """.lines
+        XCTAssertEqual("mxmxvkd,sqjhc,fvjkl", try day.part2(input))
     }
 }
