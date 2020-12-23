9a9bdcc04f7b22f56fe5e23abdd4cde25b3c00ec
PJ <pjcook@users.noreply.github.com>
2020-12-23 09:02:37 +0000

    2020 day 23 part 1

--- AdventOfCode/Year2020/Day23.swift
+++ AdventOfCode/Year2020/Day23.swift
@@ -4,11 +4,131 @@ import StandardLibraries
 public struct Day23 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(_ input: [Int], turns: Int = 100) -> Int {
+        var input = input
+        var index = 0
+        
+        for _ in 0..<turns {
+//            let before = input
+            let current = input[index]
+            var destination = input[index] - 1
+            
+            let next = input[findIndex(start: index, increment: 4, valueCount: input.count)]
+            let one = input.remove(at: findIndex(start: input.firstIndex(of: current)!, increment: 1, valueCount: input.count))
+            let two = input.remove(at: findIndex(start: input.firstIndex(of: current)!, increment: 1, valueCount: input.count))
+            let three = input.remove(at: findIndex(start: input.firstIndex(of: current)!, increment: 1, valueCount: input.count))
+
+            var insertIndex = 0
+            while true {
+                if let index = input.firstIndex(of: destination) {
+                    insertIndex = index
+                    break
+                } else if destination > 0 {
+                    destination -= 1
+                } else {
+                    destination = input.sorted().last!
+                    insertIndex = input.firstIndex(of: destination)!
+                    break
+                }
+            }
+            
+            if insertIndex < input.count {
+                insertIndex += 1
+            }
+
+            input.insert(three, at: insertIndex)
+            input.insert(two, at: insertIndex)
+            input.insert(one, at: insertIndex)
+
+            index = input.firstIndex(of: next)!
+//            while input.firstIndex(of: next)! != index {
+//                input.append(input.removeFirst())
+//            }
+            
+//            printResult(cups: before, pickUp: [one, two, three], destination: destination, result: input, move: i+1, next: next)
+        }
+        
+        var output = [Int]()
+        for _ in 0..<input.count {
+            output.append(input.remove(at: findIndex(start: input.firstIndex(of: 1)!, increment: 1, valueCount: input.count)))
+        }
+        output.removeLast()
+        return Int(output.map({ String($0) }).joined())!
+    }
+        
+    public func part2(_ input: [Int], turns: Int = 10_000_000) -> Int {
+        var cups = Array((1...1_000_000))
+        
+        for i in 0..<input.count {
+            cups[i] = input[i]
+        }
+        var index = 0
+        
+        for i in 0..<turns {
+//            let before = cups
+            let current = cups[index]
+            var destinationValue = cups[index] - 1
+            
+            let next = cups[findIndex(start: index, increment: 4, valueCount: cups.count)]
+            let one = cups.remove(at: findIndex(start: cups.firstIndex(of: current)!, increment: 1, valueCount: cups.count))
+            let two = cups.remove(at: findIndex(start: cups.firstIndex(of: current)!, increment: 1, valueCount: cups.count))
+            let three = cups.remove(at: findIndex(start: cups.firstIndex(of: current)!, increment: 1, valueCount: cups.count))
+
+            var insertIndex = 0
+            while true {
+                if let index = cups.firstIndex(of: destinationValue) {
+                    insertIndex = index
+                    break
+                } else if destinationValue > 0 {
+                    destinationValue -= 1
+                } else {
+                    destinationValue = cups.sorted().last!
+                    insertIndex = cups.firstIndex(of: destinationValue)!
+                    break
+                }
+            }
+            
+            if insertIndex < cups.count {
+                insertIndex += 1
+            }
+
+            cups.insert(three, at: insertIndex)
+            cups.insert(two, at: insertIndex)
+            cups.insert(one, at: insertIndex)
+
+            index = cups.firstIndex(of: next)!
+            
+//            printResult(cups: before, pickUp: [one, two, three], destination: destination, result: input, move: i+1, next: next)
+            if i % 10000 == 0 {
+                print(i)
+            }
+        }
+        
+        var output = [Int]()
+        for _ in 0..<2 {
+            output.append(cups.remove(at: findIndex(start: cups.firstIndex(of: 1)!, increment: 1, valueCount: cups.count)))
+        }
+        
+        return output.reduce(1, *)
+    }
+}
+
+public extension Day23 {
+    func printResult(cups: [Int], pickUp: [Int], destination: Int, result: [Int], move: Int, next: Int) {
+        print("-- move", move, "--")
+        print("cups:", cups.map({ String($0) }).joined(separator: " "))
+        print("pick up:", pickUp.map({ String($0) }).joined(separator: ", "))
+        print("destination:", destination)
+        print("next:", next)
+//        print("result:", result.map({ String($0) }).joined(separator: " "))
+        print()
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    func findIndex(start: Int, increment: Int, valueCount: Int) -> Int {
+        var index = start + increment
+        if index >= valueCount {
+            index -= valueCount
+        }
+        return index
     }
 }
--- AdventOfCode/Year2020Tests/Day23Tests.swift
+++ AdventOfCode/Year2020Tests/Day23Tests.swift
@@ -3,14 +3,27 @@ import InputReader
 import Year2020
 
 class Day23Tests: XCTestCase {
-    let input = Input("Day23.input", Year2020.bundle).lines
+    let input = [1,5,7,6,2,3,9,8,4]
     let day = Day23()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(58427369, day.part1(input))
     }
     
     func test_part2() {
         XCTAssertEqual(0, day.part2(input))
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(92658374, day.part1([3,8,9,1,2,5,4,6,7], turns: 10))
+        XCTAssertEqual(67384529, day.part1([3,8,9,1,2,5,4,6,7], turns: 100))
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(149245887792, day.part2([3,8,9,1,2,5,4,6,7]))
+    }
+    
+    func test_pick() {
+        XCTAssertEqual(5, day.findIndex(start: 5, increment: 5, valueCount: input.count))
     }
 }
