54fba1f1947cbf833b6d88b735429c9826c89940
PJ <pjcook@users.noreply.github.com>
2020-12-25 15:35:50 +0000

    2020 day 25

--- AdventOfCode/Year2020/Day25.swift
+++ AdventOfCode/Year2020/Day25.swift
@@ -4,11 +4,42 @@ import StandardLibraries
 public struct Day25 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(cardKey: Int, doorKey: Int) -> Int {
+        var value = 1
+        var loops = 0
+        while value != cardKey {
+            loops += 1
+            value = (value * 7) % 20201227
+        }
+        
+        value = 1
+        for _ in 0..<loops {
+            value = (doorKey * value) % 20201227
+        }
+        return value
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func part1_chris(cardKey: Int, doorKey: Int) -> Int {
+        let subject = 7
+        var value = 1
+        var loops = 0
+        var notSolved = true
+        
+        while notSolved {
+            if value != cardKey {
+                loops += 1
+                value = subject * value
+                value = value % 20201227
+            } else {
+                notSolved = false
+            }
+        }
+        
+        value = 1
+        for _ in 0..<loops {
+            value = (doorKey * value) % 20201227
+        }
+        
+        return value
     }
 }
--- AdventOfCode/Year2020Tests/Day25Tests.swift
+++ AdventOfCode/Year2020Tests/Day25Tests.swift
@@ -3,14 +3,24 @@ import InputReader
 import Year2020
 
 class Day25Tests: XCTestCase {
-    let input = Input("Day25.input", Year2020.bundle).lines
     let day = Day25()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        measure {
+        XCTAssertEqual(4968512, day.part1(cardKey: 4126658, doorKey: 10604480))
+        }
     }
     
-    func test_part2() {
-        XCTAssertEqual(0, day.part2(input))
+    func test_part1_chris() {
+        measure {
+        XCTAssertEqual(4968512, day.part1_chris(cardKey: 4126658, doorKey: 10604480))
+        }
+    }
+    
+    func test_part1_example() {
+        let cardPublicKey = 5764801
+        let doorPublicKey = 17807724
+        let encryptionKey = day.part1_chris(cardKey: cardPublicKey, doorKey: doorPublicKey)
+        XCTAssertEqual(14897079, encryptionKey)
     }
 }
