f5cf4e75ec0c36dac26e0c4c7782a1832c576220
PJ <pjcook@users.noreply.github.com>
2020-12-26 14:30:38 +0000

    ..

--- AdventOfCode/Year2019/Day22.swift
+++ AdventOfCode/Year2019/Day22.swift
@@ -1,39 +1,122 @@
 import Foundation
 
-public class DeckShuffle {
-    private let sizeOfDeck: Int
+public struct Day22 {
+    public init() {}
     
-    public init(sizeOfDeck: Int) {
-        self.sizeOfDeck = sizeOfDeck
-    }
-    
-    public func process(_ input: [String]) -> Int {
-        var pos = 2019
-        for line in input {
-            if line == "deal into new stack" {
-                pos = sizeOfDeck - 1 - pos
-            } else if line.hasPrefix("cut") {
-                var n = line.getNumber()
-                if n > 0 {
-                    if pos < n {
-                        pos = (sizeOfDeck-n) + pos
-                    } else {
-                        pos -= n
-                    }
-                } else {
-                    n = sizeOfDeck - n
-                    if pos >= n {
-                        pos -= sizeOfDeck - n
-                    } else {
-                        pos += n
-                    }
-                }
+    public enum Technique {
+        case dealNewStack
+        case cut(Int)
+        case dealWithIncrement(Int)
+        
+        public init(with string: String) {
+            if string == "deal into new stack" {
+                self = .dealNewStack
+            } else if string.hasPrefix("cut") {
+                self = .cut(string.getNumber())
+            } else if string.hasPrefix("deal with increment") {
+                self = .dealWithIncrement(string.getNumber())
             } else {
-                let n = line.getNumber()
-                pos = (n * pos) % sizeOfDeck
+                preconditionFailure()
             }
         }
-        return pos
+
+        public func apply(to deck: inout [Int]) {
+            switch self {
+            case .dealNewStack: deck.reverse()
+            
+            case .cut(let count):
+                let n = count < 0 ? deck.count + count : count
+                let head = deck.prefix(n)
+                let tail = deck.dropFirst(n)
+                deck = Array(tail + head)
+                
+            case .dealWithIncrement(let inc):
+                let count = deck.count
+                for (offset, card) in deck.enumerated() {
+                    deck[(offset * inc) % count] = card
+                }
+            }
+        }
+    }
+            
+    public func part1(_ input: [String]) -> Int {
+        let techniques = input.map(Technique.init)
+        var deck = Array(0..<10007)
+        for technique in techniques {
+            technique.apply(to: &deck)
+        }
+        return deck.firstIndex(of: 2019)!
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        let techniques = input.map(Technique.init).reversed()
+        
+        let len = BInt(119315717514047) // deck size
+        let times = BInt(101741582076661) // number of repititions
+        let startpos = BInt(2020)
+        var pos = startpos
+        var t = 0
+
+        while true {
+            var a = BInt(2), b = BInt(0)
+            for technique in techniques {
+                switch technique {
+                case .dealNewStack:
+                    pos = len - 1 - pos
+                    a = -a
+                    b = -b + len - 1
+                    
+                case let .cut(n):
+                    var n2 = BInt(n)
+                    if n < 0 {
+                        n2 = len - abs(n2)
+                    }
+                    pos = (pos + n2) % len
+                    b += n2
+                    
+                case let .dealWithIncrement(n):
+                    let ninv = primeModInverse(BInt(n), len)
+                    a *= ninv
+                    b *= ninv
+                }
+            }
+            a %= len
+            b %= len
+            t += 1
+            if t > 10 || t == times {
+                let answer = (startpos * modpow(a, times, len) + (modpow(a, times, len) - 1) * b * primeModInverse(a-1, len)) % len
+                print(answer, Int(answer))
+                return Int(answer)
+            }
+        }
+    }
+}
+
+public func primeModInverse(_ a: Int, _ m: Int) -> Int {
+    return modpow(a, m - 2, m)
+}
+
+public func modpow(_ b: Int, _ e: Int, _ m: Int) -> Int {
+    if e == 0 {
+        return 1
+    } else if e % 2 == 0 {
+        return modpow((b * b) % m, e / 2, m)
+    } else {
+        return (b * modpow(b, e - 1, m)) % m
+    }
+}
+
+public func primeModInverse(_ a: BInt, _ m: BInt) -> BInt {
+    return modpow(a, m - 2, m)
+}
+
+public func modpow(_ b: BInt, _ e: BInt, _ m: BInt) -> BInt {
+    if e == 0 {
+        return 1
+    } else if e % 2 == 0 {
+        return modpow((b * b) % m, e / 2, m)
+    } else {
+        return (b * modpow(b, e - 1, m)) % m
     }
 }
 
--- AdventOfCode/Year2019Tests/Day22Tests.swift
+++ AdventOfCode/Year2019Tests/Day22Tests.swift
@@ -14,12 +14,12 @@ class Day22Tests: XCTestCase {
     let input = try! readInputAsStrings(filename: "Day22.input", bundle: Year2019.bundle)
 
     func test_part1() throws {
-        let shuffler = DeckShuffle(sizeOfDeck: 10007)
-        XCTAssertEqual(2558, shuffler.process(input))
+        let day = Day22()
+        XCTAssertEqual(2558, day.part1(input))
     }
     
     func test_part2() throws {
-        let shuffler = DeckShuffle(sizeOfDeck: 119_315_717_514_047)
-        XCTAssertEqual(66036767136129, shuffler.process(input))
+        let day = Day22()
+        XCTAssertEqual(63967243502561, day.part2(input))
     }
 }
