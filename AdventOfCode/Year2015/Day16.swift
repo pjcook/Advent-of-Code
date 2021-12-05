8c97920257ec7f83b0c05a15814632c29fe3b205
PJ <code@software101.co.uk>
2021-12-05 06:40:15 +0000

    2015 day 16

--- AdventOfCode/Year2015/Day16.swift
+++ AdventOfCode/Year2015/Day16.swift
@@ -4,11 +4,86 @@ import StandardLibraries
 public struct Day16 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(_ input: [String], tickerTape: [String:Int]) throws -> Int {
+        let info = try parse(input)
+        
+    outerLoop: for (index, i) in info.enumerated() {
+            for item in i {
+                if let found = tickerTape[item.key] {
+                    if found != item.value {
+                        continue outerLoop
+                    }
+                } else {
+                    continue outerLoop
+                }
+            }
+         return index + 1
+        }
+        
+        return -1
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func part2(_ input: [String]) throws -> Int {
+        let info = try parse(input)
+        
+        var newInfo = info
+        var tickerTape = [
+            "children": 3,
+//            "cats": 7,
+            "samoyeds": 2,
+//            "pomeranians": 3,
+            "akitas": 0,
+            "vizslas": 0,
+//            "goldfish": 5,
+//            "trees": 3,
+            "cars": 2,
+            "perfumes": 1
+        ]
+        
+        for item in tickerTape {
+            newInfo = newInfo.filter {
+                $0[item.key, default: item.value] == item.value
+            }
+        }
+        
+        tickerTape = [
+            "cats": 7,
+            "trees": 3
+        ]
+        
+        for item in tickerTape {
+            newInfo = newInfo.filter {
+                $0[item.key, default: item.value+1] > item.value
+            }
+        }
+        
+        tickerTape = [
+            "pomeranians": 3,
+            "goldfish": 5,
+        ]
+        
+        for item in tickerTape {
+            newInfo = newInfo.filter {
+                $0[item.key, default: item.value-1] < item.value
+            }
+        }
+        
+        return (info.firstIndex(of: newInfo[0]) ?? 0) + 1
+    }
+    
+    public func parse(_ input: [String]) throws -> [[String:Int]] {
+        var info = [[String:Int]]()
+        let regex = try RegularExpression(pattern: "^\\w+ \\d+: (\\w+): (\\d+), (\\w+): (\\d+), (\\w+): (\\d+)$")
+        
+        for line in input {
+            let match = try regex.match(line)
+            var i = [String:Int]()
+            i[try match.string(at: 0)] = try match.integer(at: 1)
+            i[try match.string(at: 2)] = try match.integer(at: 3)
+            i[try match.string(at: 4)] = try match.integer(at: 5)
+            info.append(i)
+        }
+        
+        return info
     }
 }
--- AdventOfCode/Year2015Tests/Day16Tests.swift
+++ AdventOfCode/Year2015Tests/Day16Tests.swift
@@ -4,13 +4,31 @@ import Year2015
 
 class Day16Tests: XCTestCase {
     
-    let input = Input("Day16.input", Year2015.bundle)
-
-    func test_part1() {
-//        XCTAssertEqual(381699, Day1.findMatch(input: input, value: 2020))
+    let input = Input("Day16.input", Year2015.bundle).lines
+    let day = Day16()
+    let tickerTape = [
+        "children": 3,
+        "cats": 7,
+        "samoyeds": 2,
+        "pomeranians": 3,
+        "akitas": 0,
+        "vizslas": 0,
+        "goldfish": 5,
+        "trees": 3,
+        "cars": 2,
+        "perfumes": 1
+    ]
+    
+    func test_part1() throws {
+        XCTAssertEqual(40, try day.part1(input, tickerTape: tickerTape))
     }
     
-    func test_part2() {
-//        XCTAssertEqual(111605670, Day1.findMatch2(input: input, value: 2020))
+    func test_part2() throws {
+        XCTAssertEqual(241, try day.part2(input))
+    }
+    
+    func test_parsing() throws {
+        let info = try day.parse(input)
+        XCTAssertEqual(500, info.count)
     }
 }
