3404dde8d8ac5bfd2d5ced89936d60312ff80e73
PJ <code@software101.co.uk>
2021-12-06 06:31:25 +0000

    2021 day 6

--- AdventOfCode/Year2021/Day6.swift
+++ AdventOfCode/Year2021/Day6.swift
@@ -4,11 +4,31 @@ import StandardLibraries
 public struct Day6 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(_ input: [Int], days: Int) -> Int {
+        var fishes = input
+        for _ in (0..<days) {
+            for (index, fish) in fishes.enumerated() {
+                let timer = fish - 1
+                fishes[index] = timer
+                if timer == -1 {
+                    fishes.append(8)
+                    fishes[index] = 6
+                }
+            }
+        }
+        return fishes.count
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func part2(_ input: [Int], days: Int) -> Int {
+        var fishes = Array(repeating: 0, count: 9)
+        for i in input {
+            fishes[i] = fishes[i] + 1
+        }
+        for _ in (0..<days) {
+            let value = fishes.removeFirst()
+            fishes[6] = fishes[6] + value
+            fishes.append(value)
+        }
+        return fishes.reduce(0, +)
     }
 }
--- AdventOfCode/Year2021Tests/Day6Tests.swift
+++ AdventOfCode/Year2021Tests/Day6Tests.swift
@@ -4,14 +4,30 @@ import Year2021
 
 class Day6Tests: XCTestCase {
     
-    let input = Input("Day6.input", Year2021.bundle).lines
+    let input = Input("Day6.input", Year2021.bundle).delimited(",", cast: { Int($0)! })
     let day = Day6()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        measure {
+        XCTAssertEqual(354564, day.part2(input, days: 80))
+        }
     }
     
     func test_part2() {
-        XCTAssertEqual(0, day.part2(input))
+        measure {
+        XCTAssertEqual(1609058859115, day.part2(input, days: 256))
+        }
+    }
+    
+    func test_example() {
+        XCTAssertEqual(26, day.part1([3,4,3,1,2], days: 18))
+        XCTAssertEqual(5934, day.part1([3,4,3,1,2], days: 80))
+        XCTAssertEqual(26984457539, day.part1([3,4,3,1,2], days: 256))
+    }
+    
+    func test_example2() {
+        XCTAssertEqual(26, day.part2([3,4,3,1,2], days: 18))
+        XCTAssertEqual(5934, day.part2([3,4,3,1,2], days: 80))
+        XCTAssertEqual(26984457539, day.part2([3,4,3,1,2], days: 256))
     }
 }
