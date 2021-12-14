67aa31051d6cb2d5205ef23eb785b691722d41c4
PJ <code@software101.co.uk>
2021-12-14 07:57:54 +0000

    2021 day 14 part 1

--- AdventOfCode/Year2021/Day14.swift
+++ AdventOfCode/Year2021/Day14.swift
@@ -4,11 +4,67 @@ import StandardLibraries
 public struct Day14 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(_ input: [String], steps: Int) -> Int {
+        var (template, pairs) = parse(input)
+        for step in (0..<steps) {
+            var i = 1
+            while i < template.count {
+                let key = template[i-1] + template[i]
+                if let value = pairs[key] {
+                    template.insert(value, at: i)
+                    i += 1
+                }
+                
+                i += 1
+            }
+        }
+        var chars = [String:Int]()
+        for char in template {
+            chars[char, default: 0] += 1
+        }
+        let sorted = chars.values.sorted()
+        return sorted.last! - sorted.first!
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func part2(_ input: [String], steps: Int) -> Int {
+        let (template, pairs) = parse(input)
+        var found = [String:Int]()
+        
+        var i = 1
+        while i < template.count {
+            let key = template[i-1] + template[i]
+            found[key, default: 0] += 1
+            i += 1
+        }
+        
+        for _ in (0..<steps) {
+            var newFound = [String:Int]()
+            for item in found {
+                let c = pairs[item.key]!
+                let key1 = String(item.key[0]) + c
+                let key2 = c + String(item.key[1])
+                newFound[key1, default: 0] += item.value
+                newFound[key2, default: 0] += item.value
+            }
+            found = newFound
+        }
+        
+        var chars = [String:Int]()
+        for item in found {
+            chars[String(item.key[0]), default: 0] += item.value
+            chars[String(item.key[1]), default: 0] += item.value
+        }
+        let sorted = chars.values.sorted()
+        return Int((Double(sorted.last! - sorted.first!) / 2) + 0.5)
+    }
+    
+    public func parse(_ input: [String]) -> ([String], [String:String]) {
+        let template = input[0].map { String($0) }
+        var pairs = [String:String]()
+        for i in (2..<input.count) {
+            let split = input[i].components(separatedBy: " -> ")
+            pairs[split[0]] = split[1]
+        }
+        return (template, pairs)
     }
 }
--- AdventOfCode/Year2021Tests/Day14Tests.swift
+++ AdventOfCode/Year2021Tests/Day14Tests.swift
@@ -8,10 +8,43 @@ class Day14Tests: XCTestCase {
     let day = Day14()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(2233, day.part1(input, steps: 10))
     }
     
     func test_part2() {
-        XCTAssertEqual(0, day.part2(input))
+        // 2884513602165 too high
+        // 2884513602165
+        XCTAssertEqual(2884513602165, day.part2(input, steps: 40))
+    }
+    
+    func test_parsing() {
+        let (template, pairs) = day.parse(input)
+        XCTAssertEqual(20, template.count)
+        XCTAssertEqual(100, pairs.count)
+    }
+
+    func test_example() {
+        let input = """
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+""".lines
+        XCTAssertEqual(1588, day.part2(input, steps: 10))
+        XCTAssertEqual(2188189693529, day.part2(input, steps: 40))
     }
 }
