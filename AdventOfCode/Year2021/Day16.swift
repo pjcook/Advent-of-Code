6719e6e616bd30d10427bea15026432ca2ccb315
PJ <code@software101.co.uk>
2021-12-16 09:37:29 +0000

    2021 day 16 part 1

--- AdventOfCode/Year2021/Day16.swift
+++ AdventOfCode/Year2021/Day16.swift
@@ -4,11 +4,103 @@ import StandardLibraries
 public struct Day16 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
+    public struct Packet {
+        public let version: Int
+        public let typeID: Int
+        public let value: Int
+    }
+    
+    public func part1(_ input: String) -> Int {
+        var values = parse(input)
+        let packets = processPacket(&values, depth: 1)
+        return packets.reduce(0, { $0 + $1.version })
+    }
+    
+    public func part2(_ input: String) -> Int {
         return 0
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func processPacket(_ input: inout [Int], depth: Int) -> [Packet] {
+        guard input.count > 6 else {
+            input = []
+            return []
+        }
+        var packets = [Packet]()
+        let version = Int(input[0..<3].map(String.init).joined(), radix: 2)!
+        input.removeFirst(3)
+        let typeID = Int(input[0..<3].map(String.init).joined(), radix: 2)!
+        input.removeFirst(3)
+        print(depth, version, typeID)
+        if typeID == 4 {        // Literal
+            packets.append(processLiteral(&input, version: version, typeID: typeID, depth: depth))
+        } else { // operator
+            packets.append(Packet(version: version, typeID: typeID, value: 0))
+            processOperator(&input, depth: depth).forEach { packets.append($0) }
+        }
+        
+        while !input.isEmpty {
+            processPacket(&input, depth: depth + 1).forEach {
+                packets.append($0)
+            }
+        }
+        
+        return packets
+    }
+    
+    public func processOperator(_ input: inout [Int], depth: Int) -> [Packet] {
+        guard input.count > 11 else {
+            input = []
+            return []
+        }
+        let i = input.removeFirst()
+        var length = 0
+        if i == 0 {     // number of bits in the sub-packets
+            length = Int(input[0..<15].map(String.init).joined(), radix: 2)!
+            input.removeFirst(15)
+            var remainder = Array(input[0..<length])
+            input.removeFirst(length)
+            return processPacket(&remainder, depth: depth + 1)
+        } else {        // the number of subpackets
+            length = Int(input[0..<11].map(String.init).joined(), radix: 2)!
+            input.removeFirst(11)
+            var packets = [Packet]()
+            for i in (0..<length) {
+                processPacket(&input, depth: depth + 1).forEach {
+                    packets.append($0)
+                }
+            }
+            return packets
+        }
+    }
+    
+    public func processLiteral(_ input: inout [Int], version: Int, typeID: Int, depth: Int) -> Packet {
+        var shouldLoop = true
+        var remainder = [Int]()
+        while shouldLoop {
+            let next = input[0..<5]
+            input.removeFirst(5)
+            if next[0] == 1 {
+                next[1...].forEach { remainder.append($0) }
+            } else {
+                next[1...].forEach { remainder.append($0) }
+                shouldLoop = false
+            }
+        }
+        return Packet(version: version, typeID: typeID, value: Int(remainder.map(String.init).joined(), radix: 2)!)
+    }
+    
+    public func parse(_ input: String) -> [Int] {
+        var results = [[Int]]()
+        for x in input {
+            var values = [Int]()
+            for c in String(Int(String(x), radix: 16)!, radix: 2) {
+                values.append(Int(String(c))!)
+            }
+            while values.count < 4 {
+                values.insert(0, at: 0)
+            }
+            results.append(values)
+        }
+        return results.reduce([], +)
     }
 }
--- AdventOfCode/Year2021Tests/Day16Tests.swift
+++ AdventOfCode/Year2021Tests/Day16Tests.swift
@@ -4,14 +4,38 @@ import Year2021
 
 class Day16Tests: XCTestCase {
     
-    let input = Input("Day16.input", Year2021.bundle).lines
+    let input = Input("Day16.input", Year2021.bundle).input
     let day = Day16()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(1007, day.part1(input))
     }
     
     func test_part2() {
         XCTAssertEqual(0, day.part2(input))
+    }
+    
+    func test_parsing() {
+        XCTAssertEqual(5464, day.parse(input).count)
+    }
+    
+    func test_example_parsing() {
+        var values = day.parse("D2FE28")
+        print(values)
+        print(values.count % 3)
+        print(values.count / 3)
+        let packets = day.processPacket(&values, depth: 1)
+        XCTAssertEqual(1, packets.count)
+        print(packets)
+        XCTAssertEqual(6, packets[0].version)
+        XCTAssertEqual(4, packets[0].typeID)
+        XCTAssertEqual(2021, packets[0].value)
+    }
+    
+    func test_examples_part1() {
+        XCTAssertEqual(16, day.part1("8A004A801A8002F478"))
+        XCTAssertEqual(12, day.part1("620080001611562C8802118E34"))
+        XCTAssertEqual(23, day.part1("C0015000016115A2E0802F182340"))
+        XCTAssertEqual(31, day.part1("A0016C880162017C3686B18A3D4780"))
     }
 }
