4175795da030693bed3ad3758ffb06ecb458fb93
PJ <code@software101.co.uk>
2021-12-17 09:30:51 +0000

    docs

--- AdventOfCode/Year2021/Day6.swift
+++ AdventOfCode/Year2021/Day6.swift
@@ -4,6 +4,9 @@ import StandardLibraries
 public struct Day6 {
     public init() {}
     
+    /*
+     Naive brute force solution, just use Part 2 solution for both solutions
+     */
     public func part1(_ input: [Int], days: Int) -> Int {
         var fishes = input
         for _ in (0..<days) {
@@ -19,6 +22,11 @@ public struct Day6 {
         return fishes.count
     }
     
+    /*
+     There are 8 days, but the first day is zero, so there needs to be 9 positions to map the fish growth.
+     
+     You can cycle the fish in an array by dropping the first element, copying all the fish from that to position 6 (because existing fish take 6 days to spawn), and appending the same value at the end (effectively day 8)
+     */
     public func part2(_ input: [Int], days: Int) -> Int {
         var fishes = Array(repeating: 0, count: 9)
         for i in input {
