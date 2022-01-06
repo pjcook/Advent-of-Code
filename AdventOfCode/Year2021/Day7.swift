28b0f6d46ad0f8db6c3d37c49509895ab65412bc
PJ <code@software101.co.uk>
2022-01-06 08:39:12 +0000

    optimisations

--- AdventOfCode/Year2021/Day7.swift
+++ AdventOfCode/Year2021/Day7.swift
@@ -10,10 +10,11 @@ public struct Day7 {
      */
     public func part1(_ input: [Int]) -> Int {
         let sorted = input.sorted()
-        let min = sorted.first!
+//        let min = sorted.first!
         let max = sorted.last!
+        let median = sorted[input.count / 2]
         var mini = Int.max
-        for i in (min...max) {
+        for i in (median...max) {
             let result = input.reduce(0) {
                 $0 + abs(i - $1)
             }
@@ -28,9 +29,12 @@ public struct Day7 {
     }
     
     public func part1b(_ input: [Int]) -> Int {
-        let (dict, min, max) = reduce(input)
+        let dict = reduce(input)
         var mini = Int.max
-        for i in (min...max) {
+        let sorted = input.sorted()
+        let max = sorted.last!
+        let median = sorted[sorted.count / 2]
+        for i in (median...max) {
             let result = dict.reduce(0) {
                 $0 + abs(i - $1.0) * $1.1
             }
@@ -46,10 +50,11 @@ public struct Day7 {
     
     public func part2(_ input: [Int]) -> Int {
         let sorted = input.sorted()
-        let min = sorted.first!
+//        let min = sorted.first!
         let max = sorted.last!
+        let median = sorted[input.count / 2]
         var mini = Int.max
-        for i in (min...max) {
+        for i in (median...max) {
             let result = input.reduce(0) {
                 let n = abs(i - $1)
                 return $0 + (n * (n + 1) / 2)
@@ -63,9 +68,12 @@ public struct Day7 {
     }
     
     public func part2b(_ input: [Int]) -> Int {
-        let (dict, min, max) = reduce(input)
+        let dict = reduce(input)
         var mini = Int.max
-        for i in (min...max) {
+        let sorted = input.sorted()
+        let max = sorted.last!
+        let median = sorted[sorted.count / 2]
+        for i in (median...max) {
             let result = dict.reduce(0) {
                 let n = abs(i - $1.0)
                 return $0 + (n * (n + 1) / 2) * $1.1
@@ -80,19 +88,11 @@ public struct Day7 {
         return mini
     }
     
-    public func reduce(_ input: [Int]) -> ([Int:Int], Int, Int) {
+    public func reduce(_ input: [Int]) -> [Int:Int] {
         var dict = [Int:Int]()
-        var min = Int.max
-        var max = 0
         for i in input {
             dict[i] = dict[i, default: 0] + 1
-            if i < min {
-                min = i
-            }
-            if i > max {
-                max = i
-            }
         }
-        return (dict, min, max)
+        return dict
     }
 }
