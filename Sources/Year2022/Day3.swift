c2345203fc75b104a3cb799bca7ed0d52348f342
PJ <code@software101.co.uk>
2022-12-03 05:39:48 +0000

    2022-day-3 initial commit

--- Sources/Year2022/Day3.swift
+++ Sources/Year2022/Day3.swift
@@ -7,15 +7,40 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day3 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public let lookup = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".map { $0 }
+    public func valueOf(_ char: Character) -> Int {
+        lookup.firstIndex(of: char)! + 1
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        var total = 0
+        for line in input {
+            let hp = line.count/2
+            let a = Set(line[0..<hp])
+            let b = Set(line[hp..<line.count])
+            let match = Character(String(a.intersection(b)))
+            total += valueOf(match)
+        }
+        return total
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        var lines = input
+        var total = 0
+        
+        while !lines.isEmpty {
+            let a = Set(lines.removeFirst())
+            let b = Set(lines.removeFirst())
+            let c = Set(lines.removeFirst())
+            let match = Character(String(a.intersection(b).intersection(c)))
+            total += valueOf(match)
+        }
+        
+        return total
     }
 }
--- Tests/Year2022Tests/Day3Tests.swift
+++ Tests/Year2022Tests/Day3Tests.swift
@@ -4,18 +4,47 @@ import Year2022
 
 class Day3Tests: XCTestCase {
     
-    let input = Input("Day3.input", Bundle.module).integers
+    let input = Input("Day3.input", Bundle.module).lines
     let day = Day3()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(7917, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() throws {
+        let input = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""".lines
+        XCTAssertEqual(157, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(2585, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() throws {
+        let input = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""".lines
+        XCTAssertEqual(70, day.part2(input))
+    }
+    
+    func test_lookup() throws {
+        XCTAssertEqual(52, day.lookup.count)
+        XCTAssertEqual(16, day.lookup.firstIndex(of: "p")! + 1)
     }
 }
