e847bad88fc2307364928046c24eef5b683b6260
PJ <code@software101.co.uk>
2022-12-05 06:08:14 +0000

    2022-day-5 initial commit

--- Sources/Year2022/Day5.swift
+++ Sources/Year2022/Day5.swift
@@ -7,15 +7,111 @@
 //
 
 import Foundation
+import StandardLibraries
+
+public typealias CreateStack = [Character]
+public typealias Stacks = [Int: CreateStack]
+public typealias Instruction = (Int, Int, Int)
+public typealias Instructions = [Instruction]
 
 public struct Day5 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String], crateIndex: Int = 8) throws -> String {
+        var (stacks, instructions) = try parse(input, crateIndex: crateIndex)
+        var result = ""
+        
+        for instruction in instructions {
+            move(stacks: &stacks, instruction: instruction)
+        }
+        
+        for i in (1...9) {
+            if let stack = stacks[i], let char = stack.last {
+                result.append(char)
+            }
+        }
+        return result
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String], crateIndex: Int = 8) throws -> String {
+        var (stacks, instructions) = try parse(input, crateIndex: crateIndex)
+        var result = ""
+        
+        for instruction in instructions {
+            move9001(stacks: &stacks, instruction: instruction)
+        }
+        
+        for i in (1...9) {
+            if let stack = stacks[i], let char = stack.last {
+                result.append(char)
+            }
+        }
+        return result
+    }
+}
+
+extension Day5 {
+    public func move(stacks: inout Stacks, instruction: Instruction) {
+        var fromStack = stacks[instruction.1, default: CreateStack()]
+        var toStack = stacks[instruction.2, default: CreateStack()]
+
+        for _ in (0..<instruction.0) {
+            if let crate = fromStack.popLast() {
+                toStack.append(crate)
+            }
+        }
+        
+        stacks[instruction.1] = fromStack
+        stacks[instruction.2] = toStack
+    }
+    
+    public func move9001(stacks: inout Stacks, instruction: Instruction) {
+        var fromStack = stacks[instruction.1, default: CreateStack()]
+        var toStack = stacks[instruction.2, default: CreateStack()]
+        var stack = CreateStack()
+        
+        for _ in (0..<instruction.0) {
+            if let crate = fromStack.popLast() {
+                stack.insert(crate, at: 0)
+            }
+        }
+        
+        toStack.append(contentsOf: stack)
+        stacks[instruction.1] = fromStack
+        stacks[instruction.2] = toStack
+    }
+    
+    public func parse(_ lines: [String], crateIndex: Int) throws -> (Stacks, Instructions) {
+        var stacks = Stacks()
+        let stackPositions = [1, 5, 9, 13, 17, 21, 25, 29, 33]
+        
+        for i in (0..<crateIndex) {
+            let line = lines[i]
+            for j in (1...9) {
+                if line.count > j {
+                    let position = stackPositions[j-1]
+                    let value = line[position]
+                    guard value != " " && !value.isEmpty else { continue }
+                    var stack = stacks[j, default: CreateStack()]
+                    stack.insert(Character(String(value)), at: 0)
+                    stacks[j] = stack
+                }
+            }
+        }
+        
+//        let regex = try RegularExpression(pattern: "[\\d]+")
+        var instructions = Instructions()
+        
+        for i in (crateIndex+2..<lines.count) {
+            let line = lines[i]
+            let components = line.components(separatedBy: " ")
+            let instruction = (Int(components[1])!, Int(components[3])!, Int(components[5])!)
+//            guard !line.isEmpty else { continue }
+//            let match = try regex.match(line)
+//            let instruction = try (match.integer(at: 0), match.integer(at: 1), match.integer(at: 2))
+            instructions.append(instruction)
+        }
+        
+        return (stacks, instructions)
     }
 }
--- Tests/Year2022Tests/Day5Tests.swift
+++ Tests/Year2022Tests/Day5Tests.swift
@@ -4,18 +4,56 @@ import Year2022
 
 class Day5Tests: XCTestCase {
     
-    let input = Input("Day5.input", Bundle.module).integers
+    let input = Input("Day5.input", Bundle.module).lines
     let day = Day5()
 
-    func test_part1() {
+    func test_part1() throws {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual("WHTLRMZRC", try day.part1(input))
 //        }
     }
     
-    func test_part2() {
+    func test_part2() throws {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual("GMPMLWNMG", try day.part2(input))
 //        }
+    }
+}
+
+extension Day5Tests {
+    func test_parse() throws {
+        let input = """
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+""".lines
+        let (stacks, instructions) = try day.parse(input, crateIndex: 3)
+        print(stacks)
+        print(instructions)
+    }
+    
+    func test_part1_example() throws {
+        let input = """
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+""".lines
+        XCTAssertEqual("CMZ", try day.part1(input, crateIndex: 3))
+    }
+    
+    func test_regex() throws {
+        let regex = try RegularExpression(pattern: "[\\d]+")
     }
 }
