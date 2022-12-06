e2dbfc6a80c3d978543e359af530b61405ab60a9
PJ <code@software101.co.uk>
2022-12-06 05:14:06 +0000

    2022-day-6 initial commit

--- Sources/Year2022/Day6.swift
+++ Sources/Year2022/Day6.swift
@@ -11,11 +11,19 @@ import Foundation
 public struct Day6 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: String) -> Int {
+        for i in (3..<input.count) {
+            let values = Set([input[i], input[i-1], input[i-2], input[i-3]])
+            if values.count == 4 { return i + 1 }
+        }
+        return -1
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: String) -> Int {
+        for i in (13..<input.count) {
+            let values = Set([input[i], input[i-1], input[i-2], input[i-3], input[i-4], input[i-5], input[i-6], input[i-7], input[i-8], input[i-9], input[i-10], input[i-11], input[i-12], input[i-13]])
+            if values.count == 14 { return i + 1 }
+        }
+        return -1
     }
 }
--- Tests/Year2022Tests/Day6Tests.swift
+++ Tests/Year2022Tests/Day6Tests.swift
@@ -4,18 +4,29 @@ import Year2022
 
 class Day6Tests: XCTestCase {
     
-    let input = Input("Day6.input", Bundle.module).integers
+    let input = Input("Day6.input", Bundle.module).input
     let day = Day6()
 
-    func test_part6() {
-//        measure {
-        XCTAssertEqual(1, day.part1(input))
-//        }
+    func test_part1() {
+        measure {
+        XCTAssertEqual(1640, day.part1(input))
+        }
     }
     
     func test_part2() {
+        measure {
+        XCTAssertEqual(3613, day.part2(input))
+        }
+    }
+}
+
+extension Day6Tests {
+    func test_part1_examples() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(5, day.part1("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+        XCTAssertEqual(6, day.part1("nppdvjthqldpwncqszvftbrmjlhg"))
+        XCTAssertEqual(10, day.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+        XCTAssertEqual(11, day.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
 //        }
     }
 }
