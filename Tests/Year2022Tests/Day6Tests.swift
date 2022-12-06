6e67bafc70e1618c8dcf4b8b045e9f818ed396aa
PJ <code@software101.co.uk>
2022-12-06 05:43:52 +0000

    2022-day-6 optimised and cleaned up

--- Tests/Year2022Tests/Day6Tests.swift
+++ Tests/Year2022Tests/Day6Tests.swift
@@ -8,25 +8,23 @@ class Day6Tests: XCTestCase {
     let day = Day6()
 
     func test_part1() {
-        measure {
+//        measure {
         XCTAssertEqual(1640, day.part1(input))
-        }
+//        }
     }
     
     func test_part2() {
-        measure {
+//        measure {
         XCTAssertEqual(3613, day.part2(input))
-        }
+//        }
     }
 }
 
 extension Day6Tests {
     func test_part1_examples() {
-//        measure {
         XCTAssertEqual(5, day.part1("bvwbjplbgvbhsrlpgdmjqwftvncz"))
         XCTAssertEqual(6, day.part1("nppdvjthqldpwncqszvftbrmjlhg"))
         XCTAssertEqual(10, day.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
         XCTAssertEqual(11, day.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
-//        }
     }
 }
