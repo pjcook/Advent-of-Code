58eb4eeef9104784b23d2a6cbacb8c6da3352fe1
PJ <code@software101.co.uk>
2022-12-07 05:59:55 +0000

    2022-day-7 initial commit

--- Sources/Year2022/Day7.swift
+++ Sources/Year2022/Day7.swift
@@ -11,11 +11,133 @@ import Foundation
 public struct Day7 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let dir = parse(input)
+        return part1Traverse(total: 0, dir: dir)
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String], requiredFreeSpace: Int = 30_000_000, fileSystemSize: Int = 70_000_000) -> Int {
+        let dir = parse(input)
+        let additionalSpaceNeeded = requiredFreeSpace - (fileSystemSize - dir.size)
+        var folders = [Dir]()
+        part2Traverse(dir: dir, additionalSpaceNeeded: additionalSpaceNeeded, folders: &folders)
+        return folders.sorted(by: { $0.size < $1.size }).first!.size
+    }
+}
+
+extension Day7 {
+    public func part1Traverse(total: Int, dir: Dir) -> Int {
+        var total = total
+        let size = dir.size
+        if size <= 100000 {
+            total += size
+        }
+        
+        for item in dir.contents where item is Dir {
+            total = part1Traverse(total: total, dir: item as! Dir)
+        }
+        
+        return total
+    }
+    
+    public func part2Traverse(dir: Dir, additionalSpaceNeeded: Int, folders: inout [Dir]) {
+        if dir.size > additionalSpaceNeeded {
+            folders.append(dir)
+        }
+        
+        for item in dir.contents where item is Dir {
+            part2Traverse(dir: item as! Dir, additionalSpaceNeeded: additionalSpaceNeeded, folders: &folders)
+        }
+    }
+}
+
+public protocol FolderItem {
+    var name: String { get }
+    var size: Int { get }
+}
+
+extension Day7 {
+    public struct File: FolderItem {
+        public let name: String
+        public let size: Int
+        
+        public init(name: String, size: Int) {
+            self.name = name
+            self.size = size
+        }
+    }
+    
+    public class Dir: FolderItem {
+        public let name: String
+        public let parent: Dir?
+        public var contents: [FolderItem]
+        
+        private var calculatedSpace: Int?
+        public var size: Int {
+            if let calculatedSpace {
+                return calculatedSpace
+            }
+            
+            calculatedSpace = contents.reduce(0) {
+                $0 + $1.size
+            }
+            
+            return calculatedSpace ?? 0
+        }
+        
+        public init(name: String, parent: Dir?, contents: [FolderItem]) {
+            self.name = name
+            self.parent = parent
+            self.contents = contents
+        }
+    }
+}
+
+extension Day7 {
+    public func parse(_ input: [String]) -> Dir {
+        let root = Dir(name: "/", parent: nil, contents: [])
+        var current = root
+        
+        for line in input {
+            let components = line.components(separatedBy: " ")
+            switch components[0] {
+            case "$":
+                switch components[1] {
+                case "cd":
+                    switch components[2] {
+                    case "/":
+                        current = root
+                        
+                    case "..":
+                        if let parent = current.parent {
+                            current = parent
+                        }
+                        
+                    default:
+                        if let item = current.contents.first(where: { $0.name == components[2] }) as? Dir {
+                            current = item
+                        } else {
+                            let dir = Dir(name: components[1], parent: current, contents: [])
+                            current.contents.append(dir)
+                            current = dir
+                        }
+                        
+                    }
+                    
+                case "ls":
+                    break
+                    
+                default: break
+                }
+                
+            case "dir":
+                current.contents.append(Dir(name: components[1], parent: current, contents: []))
+                
+            default:
+                current.contents.append(File(name: components[1], size: Int(components[0]) ?? 0))
+            }
+        }        
+        
+        return root
     }
 }
--- Tests/Year2022Tests/Day7Tests.swift
+++ Tests/Year2022Tests/Day7Tests.swift
@@ -4,18 +4,113 @@ import Year2022
 
 class Day7Tests: XCTestCase {
     
-    let input = Input("Day7.input", Bundle.module).integers
+    let input = Input("Day7.input", Bundle.module).lines
     let day = Day7()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(1583951, day.part1(input))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(214171, day.part2(input))
 //        }
+    }
+}
+
+extension Day7Tests {
+    func test_parse() throws {
+        let result = day.parse(input)
+        print(result)
+    }
+    
+    func test_parse_example() throws {
+        let input = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+""".lines
+        let result = day.parse(input)
+        print(result.contents.map { $0.name })
+    }
+    
+    func test_part1_example() throws {
+        let input = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+""".lines
+        XCTAssertEqual(95437, day.part1(input))
+    }
+    
+    func test_part2_example() throws {
+        let input = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+""".lines
+        XCTAssertEqual(24933642, day.part2(input))
     }
 }
