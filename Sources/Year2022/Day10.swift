6108be32b6b4acc9d9dd6b9da8a40126dc3280af
PJ <code@software101.co.uk>
2022-12-10 06:09:12 +0000

    2022-day-10 initial commit

--- Sources/Year2022/Day10.swift
+++ Sources/Year2022/Day10.swift
@@ -7,15 +7,136 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day10 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let clock = ClockCircuit()
+        let instructions = parse(input)
+        return clock.run_part1(program: instructions, cycleIndexes: [20, 60, 100, 140, 180, 220])
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) {
+        let clock = ClockCircuit()
+        let instructions = parse(input)
+        clock.run_part2(program: instructions)
+    }
+}
+
+extension Day10 {
+    public func parse(_ input: [String]) -> [Instruction] {
+        var instructions = [Instruction]()
+        
+        for line in input {
+            let components = line.components(separatedBy: " ")
+            if components.count == 2 {
+                instructions.append(.addx(Int(components[1])!))
+            } else {
+                instructions.append(.noop)
+            }
+        }
+        
+        return instructions
+    }
+}
+
+extension Day10 {
+    public enum Instruction {
+        case addx(Int)
+        case noop
+        
+        var cycles: Int {
+            switch self {
+            case .addx: return 2
+            case .noop: return 1
+            }
+        }
+    }
+    public struct CRT {
+        let rows = 40
+        let cols = 6
+    }
+    
+    public struct ClockCircuit {
+        public init() {}
+        
+        public func run_part1(program: [Instruction], cycleIndexes: [Int]) -> Int {
+            var total = 0
+            var cycleIndexes = cycleIndexes
+            var nextCycle = cycleIndexes.removeFirst()
+            var x = 1
+            var timestamp = 1
+            var instructionPointer = 0
+            
+            while true {
+                let instruction = program[instructionPointer]
+                timestamp += instruction.cycles
+                if timestamp > nextCycle {
+                    total += nextCycle * x
+                    if !cycleIndexes.isEmpty {
+                        nextCycle = cycleIndexes.removeFirst()
+                    } else {
+                        return total
+                    }
+                }
+                
+                switch instruction {
+                case .noop:
+                    break
+                    
+                case .addx(let value):
+                    x += value
+                }
+                instructionPointer += 1
+                if instructionPointer >= program.count {
+                    instructionPointer = 0
+                }
+            }
+        }
+        
+        public func run_part2(program: [Instruction]) {
+            var grid = Grid<Character>(columns: 40, items: Array(repeating: ".", count: 240))
+            
+            var x = 1
+            var timestamp = 0
+            var instructionPointer = 0
+            
+            func lightPixel() {
+                let crtPointer = ((timestamp / 40) * 40) + x
+                if (crtPointer-1...crtPointer+1).contains(timestamp) {
+                    grid.items[timestamp] = "#"
+                } else {
+                    grid.items[timestamp] = "."
+                }
+            }
+            
+            while timestamp < 240 {
+                let instruction = program[instructionPointer]
+                
+                lightPixel()
+                
+                switch instruction {
+                case .noop:
+                    timestamp += 1
+                    
+                case .addx(let value):
+                    timestamp += 1
+                    lightPixel()
+                    timestamp += 1
+                    x += value
+                }
+                
+                instructionPointer += 1
+                if instructionPointer >= program.count {
+                    instructionPointer = 0
+                }
+            }
+            
+            print()
+            grid.draw()
+            print()
+        }
     }
 }
--- Tests/Year2022Tests/Day10Tests.swift
+++ Tests/Year2022Tests/Day10Tests.swift
@@ -4,18 +4,326 @@ import Year2022
 
 class Day10Tests: XCTestCase {
     
-    let input = Input("Day10.input", Bundle.module).integers
+    let input = Input("Day10.input", Bundle.module).lines
     let day = Day10()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(13180, day.part1(input))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        day.part2(input)    // look in console
 //        }
+    }
+}
+
+extension Day10Tests {
+    func test_part1_example1() {
+        let input = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+""".lines
+        
+        XCTAssertEqual(13140, day.part1(input))
+    }
+    
+    func test_part2_example1() {
+        let input = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+""".lines
+        
+        day.part2(input)
     }
 }
