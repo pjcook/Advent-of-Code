f2e0496fe90b9890d60b7980fa42ac5ced29afc2
PJ <code@software101.co.uk>
2022-12-13 20:25:06 +0000

    2022-day-13 initial commit

--- Sources/Year2022/Day13.swift
+++ Sources/Year2022/Day13.swift
@@ -1,9 +1,8 @@
 //
-//
 //  File.swift
 //  
 //
-//  Created by PJ on 26/11/2022.
+//  Created by PJ on 13/12/2022.
 //
 
 import Foundation
@@ -11,11 +10,78 @@ import Foundation
 public struct Day13 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) throws -> Int {
+        let pairs = input.split(separator: "")
+        var count = 0
+
+        for (index, pair) in pairs.enumerated() {
+            let line1 = try parse(pair.first!)
+            let line2 = try parse(pair.last!)
+            if process(list1: line1, list2: line2) == .rightOrder {
+                count += (index + 1)
+            }
+        }
+        
+        return count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) throws -> Int {
+        var lines: [[Any]] = input.compactMap { line in
+            guard !line.isEmpty else { return nil }
+            return try! parse(line)
+        }
+        
+        let divider1: [[Any]] = [[2]]
+        let divider2: [[Any]] = [[6]]
+        lines.append(divider1)
+        lines.append(divider2)
+        
+        lines.sort { list1, list2 in
+            process(list1: list1, list2: list2) == .rightOrder
+        }
+        
+        let index1 = lines.firstIndex(where: { $0.description == divider1.description })! + 1
+        let index2 = lines.firstIndex(where: { $0.description == divider2.description })! + 1
+
+        return index1 * index2
+    }
+}
+
+extension Day13 {
+    public func process(list1: [Any], list2: [Any]) -> CompareResult {
+        for (item1, item2) in zip(list1, list2) {
+            if let item1 = item1 as? Int, let item2 = item2 as? Int {
+                if item1 < item2 { return .rightOrder }
+                else if item1 > item2 { return .wrongOrder }
+            } else if let item1 = item1 as? [Any], let item2 = item2 as? [Any] {
+                let result = process(list1: item1, list2: item2)
+                guard result != .sameOrder else { continue }
+                return result
+            } else if let item1 = item1 as? Int, let item2 = item2 as? [Any] {
+                let result = process(list1: [item1], list2: item2)
+                guard result != .sameOrder else { continue }
+                return result
+            } else if let item1 = item1 as? [Any], let item2 = item2 as? Int {
+                let result = process(list1: item1, list2: [item2])
+                guard result != .sameOrder else { continue }
+                return result
+            }
+        }
+        
+        if list1.count < list2.count { return .rightOrder }
+        else if list1.count > list2.count { return .wrongOrder }
+        return .sameOrder
+    }
+    
+    public func parse(_ line: String) throws -> [Any] {
+        try JSONSerialization.jsonObject(with: line.data(using: .utf8)!, options: .fragmentsAllowed) as! [Any]
+    }
+}
+
+extension Day13 {
+    public enum CompareResult {
+        case rightOrder
+        case wrongOrder
+        case sameOrder
     }
 }
--- Tests/Year2022Tests/Day13Tests.swift
+++ Tests/Year2022Tests/Day13Tests.swift
@@ -4,18 +4,160 @@ import Year2022
 
 class Day13Tests: XCTestCase {
     
-    let input = Input("Day13.input", Bundle.module).integers
+    let input = Input("Day13.input", Bundle.module).lines
     let day = Day13()
 
-    func test_part1() {
+    func test_part1() throws {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(5720, try! day.part1(input))
 //        }
     }
     
-    func test_part2() {
+    func test_part2() throws {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(23504, try! day.part2(input))
 //        }
+    }
+}
+
+extension Day13Tests {
+    func test_part1_parsing() throws {
+        let result = try day.parse("[[[],4],4,4,4]")
+        XCTAssertEqual(4, result.count)
+        XCTAssertTrue(result[0] is [Any])
+        XCTAssertTrue((result[0] as! [Any])[0] is [Any])
+        XCTAssertTrue(((result[0] as! [Any])[0] as! [Any]).isEmpty)
+        print(result)
+        print()
+        print(((result[0] as! [Any])[0] as! [Any]))
+        print(((result[0] as! [Any])[1] as! Int))
+        print(result[1])
+        print(result[2])
+        print(result[3])
+    }
+    
+    func test_compare1() throws {
+        let line1 = try day.parse("[1,1,3,1,1]")
+        let line2 = try day.parse("[1,1,5,1,1]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.rightOrder, result)
+    }
+    
+    func test_compare2() throws {
+        let line1 = try day.parse("[[1],[2,3,4]]")
+        let line2 = try day.parse("[[1],4]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.rightOrder, result)
+    }
+    
+    func test_compare3() throws {
+        let line1 = try day.parse("[9]")
+        let line2 = try day.parse("[[8,7,6]]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.wrongOrder, result)
+    }
+    
+    func test_compare4() throws {
+        let line1 = try day.parse("[[4,4],4,4]")
+        let line2 = try day.parse("[[4,4],4,4,4]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.rightOrder, result)
+    }
+    
+    func test_compare5() throws {
+        let line1 = try day.parse("[]")
+        let line2 = try day.parse("[3]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.rightOrder, result)
+    }
+    
+    func test_compare6() throws {
+        let line1 = try day.parse("[[[]]]")
+        let line2 = try day.parse("[[]]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.wrongOrder, result)
+    }
+    
+    func test_compare7() throws {
+        let line1 = try day.parse("[1,[2,[3,[4,[5,6,7]]]],8,9]")
+        let line2 = try day.parse("[1,[2,[3,[4,[5,6,0]]]],8,9]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.wrongOrder, result)
+    }
+    
+    func test_compare8() throws {
+        let line1 = try day.parse("[[[0,0,0]]]")
+        let line2 = try day.parse("[2]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.rightOrder, result)
+    }
+
+    func test_compare9() throws {
+        let line1 = try day.parse("[[1,[4],4,2]]")
+        let line2 = try day.parse("[[],[8,[6],1,3],[[]],[[9,8],[[]]]]")
+        let result = day.process(list1: line1, list2: line2)
+        XCTAssertEqual(.wrongOrder, result)
+    }
+}
+
+extension Day13Tests {
+    func test_part1_example() throws {
+        let input = """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+""".lines
+        let day = Day13()
+        XCTAssertEqual(13, try day.part1(input))
+    }
+    
+    func test_part2_example() throws {
+        let input = """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+""".lines
+        let day = Day13()
+        XCTAssertEqual(140, try day.part2(input))
     }
 }
