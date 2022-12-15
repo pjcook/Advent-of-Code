0b8c1b309fb60d717e0ddd2530ae2ce4fabeed07
PJ <code@software101.co.uk>
2022-12-15 05:54:23 +0000

    2022-day-15 initial commit

--- Sources/Year2022/Day15.swift
+++ Sources/Year2022/Day15.swift
@@ -7,15 +7,122 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day15 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    let beaconChar = "B"
+    let sensorChar = "S"
+    let airChar = "."
+    let noBeacon = "#"
+    
+    public func part1(_ input: [String], rowToCheck: Int) -> Int {
+        let pairs = parse(input)
+        var points = [Point: String]()
+        var minX = 0
+        var maxX = 0
+        
+        for pair in pairs {
+            let sensor = pair.0
+            let beacon = pair.1
+            
+            // for any space within manhattan from sensor to beacon make `noBeacon`
+            let distance = sensor.manhattanDistance(to: beacon)
+            let range = ((sensor.y - distance)..<(sensor.y + distance))
+            guard range.contains(rowToCheck) else { continue}
+            for x in ((sensor.x - distance)..<(sensor.x + distance)) {
+                if x < minX {
+                    minX = x
+                }
+                if x > maxX {
+                    maxX = x
+                }
+                let point = Point(x,rowToCheck)
+                if sensor.manhattanDistance(to: point) <= distance {
+                    points[point] = noBeacon
+                }
+            }
+        }
+        
+        for pair in pairs {
+            let sensor = pair.0
+            let beacon = pair.1
+            points[sensor] = sensorChar
+            points[beacon] = beaconChar
+        }
+        
+        return points.reduce(0, { $0 + ($1.value == noBeacon ? 1 : 0) })
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String], upperBound: Int) -> Int {
+        let pairs = parse(input)
+        let sensorData = pairs.map { SensorData(sensor: $0, beacon: $1) }
+                
+        for y in (0..<upperBound) {
+            var ranges = sensorData.filter({ $0.isInRange(for: y) }).map({ $0.range(for: y) })
+            var range = ranges.removeFirst()
+            while !ranges.isEmpty {
+                if let nextRange = ranges.first(where: { range.overlaps($0) }) {
+                    ranges.remove(at: ranges.firstIndex(of: nextRange)!)
+                    range = (min(range.lowerBound, nextRange.lowerBound)...max(range.upperBound, nextRange.upperBound))
+                } else if let nextRange = ranges.first(where: { ((range.lowerBound-1)...(range.upperBound+1)).overlaps($0) }) {
+                    ranges.remove(at: ranges.firstIndex(of: nextRange)!)
+                    range = (min(range.lowerBound, nextRange.lowerBound)...max(range.upperBound, nextRange.upperBound))
+                } else {
+                    if ranges.first(where: { ((range.lowerBound)...(range.upperBound+2)).overlaps($0) }) != nil {
+                        let x = range.upperBound + 1
+                        return x * 4_000_000 + y
+                    } else {
+                        let x = range.lowerBound - 1
+                        return x * 4_000_000 + y
+                    }
+                }
+            }
+        }
+        
+        return -1
+    }
+}
+
+extension Day15 {
+    public class SensorData {
+        public let sensor: Point
+        public let beacon: Point
+        public let manhattanDistance: Int
+        public let minY: Int
+        public let maxY: Int
+        
+        public init(sensor: Point, beacon: Point) {
+            self.sensor = sensor
+            self.beacon = beacon
+            self.manhattanDistance = sensor.manhattanDistance(to: beacon)
+            self.minY = sensor.y - manhattanDistance
+            self.maxY = sensor.y + manhattanDistance
+        }
+        
+        public func isInRange(for y: Int) -> Bool {
+            (minY..<maxY).contains(y)
+        }
+        
+        public func range(for y: Int) -> ClosedRange<Int> {
+            let distance = manhattanDistance - abs(sensor.y - y)
+            return ((sensor.x - distance)...(sensor.x + distance))
+        }
+    }
+    
+    public func parse(_ input: [String]) -> [(Point, Point)] {
+        var output = [(Point, Point)]()
+        
+        for line in input {
+            let components = line.components(separatedBy: " ")
+            let x1 = Int(components[2].replacingOccurrences(of: "x=", with: "").replacingOccurrences(of: ",", with: ""))!
+            let y1 = Int(components[3].replacingOccurrences(of: "y=", with: "").replacingOccurrences(of: ":", with: ""))!
+            let x2 = Int(components[8].replacingOccurrences(of: "x=", with: "").replacingOccurrences(of: ",", with: ""))!
+            let y2 = Int(components[9].replacingOccurrences(of: "y=", with: ""))!
+            output.append((Point(x1, y1), Point(x2, y2)))
+        }
+        
+        return output
     }
 }
--- Tests/Year2022Tests/Day15Tests.swift
+++ Tests/Year2022Tests/Day15Tests.swift
@@ -4,18 +4,91 @@ import Year2022
 
 class Day15Tests: XCTestCase {
     
-    let input = Input("Day15.input", Bundle.module).integers
+    let input = Input("Day15.input", Bundle.module).lines
     let day = Day15()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(5256611, day.part1(input, rowToCheck: 2_000_000))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(13337919186981, day.part2(input, upperBound: 4_000_000))
 //        }
+    }
+}
+
+extension Day15Tests {
+    func test_part1_parse() {
+        let input = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+""".lines
+        let result = day.parse(input)
+        XCTAssertEqual(14, result.count)
+        
+        XCTAssertEqual(2, result[0].0.x)
+        XCTAssertEqual(18, result[0].0.y)
+        XCTAssertEqual(-2, result[0].1.x)
+        XCTAssertEqual(15, result[0].1.y)
+        
+        XCTAssertEqual(20, result[13].0.x)
+        XCTAssertEqual(1, result[13].0.y)
+        XCTAssertEqual(15, result[13].1.x)
+        XCTAssertEqual(3, result[13].1.y)
+    }
+    
+    func test_part1_example() {
+        let input = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+""".lines
+        XCTAssertEqual(26, day.part1(input, rowToCheck: 10))
+    }
+    
+    func test_part2_example() {
+        let input = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+""".lines
+        XCTAssertEqual(56000011, day.part2(input, upperBound: 20))
     }
 }
