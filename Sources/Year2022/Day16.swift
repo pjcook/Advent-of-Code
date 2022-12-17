2df304195737a13d836a5a2776b054f1df641d46
PJ <code@software101.co.uk>
2022-12-17 07:56:46 +0000

    2022-day-16 initial commit

--- Sources/Year2022/Day16.swift
+++ Sources/Year2022/Day16.swift
@@ -7,15 +7,170 @@
 //
 
 import Foundation
+import StandardLibraries
 
-public struct Day16 {
+public class Day16 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
+    public func part1(_ input: String) -> Int {
+        let chambers = parse(input)
+        let start = chambers["AA"]!
+        return explore(current: start, chambers: chambers, depth: 1, maxDepth: 30, pressure: 0, runningTotal: 0, history: [start.id])
+    }
+    
+    public func part2(_ input: String) -> Int {
         return 1
     }
     
-    public func part2(_ input: [Int]) -> Int {
+    var maxValue = 0
+}
+
+extension Day16 {
+    public func explore(current: Chamber, chambers: [String: Chamber], depth: Int, maxDepth: Int, pressure: Int, runningTotal: Int, history: [String]) -> Int {
+//        if runningTotal > maxValue {
+//            maxValue = runningTotal
+//        }
+//        print("explore:", depth, maxValue, current.id, pressure, runningTotal, history.joined(separator: ","))
+//        guard depth < maxDepth else { return pressure }
+//        let queue = PriorityQueue<Chamber>()
+//
+//        for id in current.tunnels {
+//            let chamber = chambers[id]!
+//            let priority = pressure + history.filter({ $0 == chamber.id }).count
+//
+//            // TODO: look forward to find best path forward not zero and not already open
+////            guard !history.suffix(min(chamber.tunnels.count, maxDepth - depth)).contains(chamber.id) else { continue }
+//            if chamber.flowRate == 0 {
+//                queue.enqueue(chamber, priority: priority)
+//            } else if chamber.valveOpen {
+//                queue.enqueue(chamber, priority: priority)
+//            } else {
+////                queue.enqueue(chamber, priority: pressure)
+//                queue.enqueue(chamber.toggleWillOpen(), priority: priority - chamber.flowRate)
+//            }
+//        }
+//
+//        if queue.isEmpty {
+//            print("explore:", depth, current.id, pressure, runningTotal, history.joined(separator: ","))
+//            return runningTotal + ((maxDepth - depth) * pressure)
+//        }
+//
+//        var currentMaxTotal = runningTotal
+//
+//        while !queue.isEmpty {
+//            var chamber = queue.dequeue()!
+//            var chambers = chambers
+//            var depth = depth
+//            var pressure = pressure
+//            var runningTotal = runningTotal
+//            chamber = chamber.visiting(from: current.id)
+//            if chamber.willOpen {
+//                chamber = chamber.toggleWillOpen()
+//                chamber = chamber.openValue()
+//                chambers[chamber.id] = chamber
+//                if depth + 2 <= maxDepth {
+//                    depth += 2
+//                    runningTotal += pressure * 2
+//                    pressure += chamber.flowRate
+//                } else {
+//                    depth += 1
+//                    runningTotal += pressure
+//                }
+//            } else {
+//                depth += 1
+//                runningTotal += pressure
+//            }
+//
+//            let result = explore(current: chamber, chambers: chambers, depth: depth, maxDepth: maxDepth, pressure: pressure, runningTotal: runningTotal, history: history + [chamber.id])
+//            if result > currentMaxTotal {
+//                currentMaxTotal = result
+//            }
+//        }
+        
+//        return currentMaxTotal
         return 1
+    }
+}
+
+extension Day16 {
+    public struct Destination: Hashable {
+        public let id: String
+        public let distance: Int
+    }
+    
+    public struct Node: Hashable {
+        public let id: String
+        public let flowRate: Int
+        public let destinations: [Destination]
+        public let valveOpen: Bool
+    }
+    
+    public struct Chamber: Hashable, CustomDebugStringConvertible {
+        public let id: String
+        public let flowRate: Int
+        public let tunnels: [String]
+        public let valveOpen: Bool
+        
+        public init(id: String, tunnels: [String], flowRate: Int, valveOpen: Bool = false) {
+            self.id = id
+            self.tunnels = tunnels
+            self.flowRate = flowRate
+            self.valveOpen = valveOpen
+        }
+        
+        public func openValue() -> Chamber {
+            Chamber(id: id, tunnels: tunnels, flowRate: flowRate, valveOpen: true)
+        }
+        
+        public var debugDescription: String {
+            "\(id); \(flowRate); \(tunnels.joined(separator: ","));"
+        }
+    }
+    
+    public func compressGraph(chambers: [String: Chamber]) -> [String: Node] {
+        var results = [String: Node]()
+        let valveChambers = chambers.filter({ $0.value.flowRate > 0 || $0.value.id == "AA" })
+
+        for valveChamber in valveChambers {
+            var destinations = [Destination]()
+            var distance = 1
+            for chamberId in valveChamber.value.tunnels {
+                var nextId = chamberId
+                var prevId = valveChamber.key
+                while true {
+                    let node = chambers[nextId]!
+                    if node.flowRate == 0 {
+                        distance += 1
+                        let id = node.tunnels.first(where: { $0 != prevId })!
+                        prevId = nextId
+                        nextId = id
+                    } else {
+                        destinations.append(Destination(id: node.id, distance: distance))
+                        break
+                    }
+                }
+            }
+            results[valveChamber.value.id] = Node(id: valveChamber.value.id, flowRate: valveChamber.value.flowRate, destinations: destinations, valveOpen: false)
+        }
+        
+        return results
+    }
+    
+    public func parse(_ input: String) -> [String: Chamber] {
+        var output = [String: Chamber]()
+        let lines = input
+            .replacingOccurrences(of: "Valve ", with: "")
+            .replacingOccurrences(of: " has flow rate=", with: "|")
+            .replacingOccurrences(of: "; tunnel leads to valve ", with: "|")
+            .replacingOccurrences(of: "; tunnels lead to valves ", with: "|")
+            .components(separatedBy: "\n")
+        
+        for line in lines {
+            let parts = line.components(separatedBy: "|")
+            let destinations = parts[2].components(separatedBy: ", ")
+            output[parts[0]] = Chamber(id: parts[0], tunnels: destinations, flowRate: Int(parts[1])!)
+        }
+        
+        return output
     }
 }
--- Tests/Year2022Tests/Day16Tests.swift
+++ Tests/Year2022Tests/Day16Tests.swift
@@ -4,7 +4,7 @@ import Year2022
 
 class Day16Tests: XCTestCase {
     
-    let input = Input("Day16.input", Bundle.module).integers
+    let input = Input("Day16.input", Bundle.module).input
     let day = Day16()
 
     func test_part1() {
@@ -17,5 +17,54 @@ class Day16Tests: XCTestCase {
 //        measure {
         XCTAssertEqual(1, day.part2(input))
 //        }
+    }
+}
+
+extension Day16Tests {
+    func test_compressGraph() {
+        let results = day.parse(input)
+        let compressed = day.compressGraph(chambers: results)
+        XCTAssertEqual(16, compressed.count)
+        
+        let start = compressed.first(where: { $0.key == "AA" })!
+        XCTAssertEqual(5, start.value.destinations.count)
+        XCTAssertNotNil(start.value.destinations.first(where: { compressed[$0.id]!.flowRate == 18 }))
+        XCTAssertNotNil(start.value.destinations.first(where: { compressed[$0.id]!.flowRate == 7 }))
+        XCTAssertNotNil(start.value.destinations.first(where: { compressed[$0.id]!.flowRate == 5 }))
+        XCTAssertNotNil(start.value.destinations.first(where: { compressed[$0.id]!.flowRate == 4 }))
+        XCTAssertNotNil(start.value.destinations.first(where: { compressed[$0.id]!.flowRate == 13 }))
+    }
+    
+    func test_part1_example() {
+        let input = """
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+"""
+        XCTAssertEqual(1651, day.part1(input))
+    }
+    
+    func test_part1_parse() {
+        let input = """
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+"""
+        let results = day.parse(input)
+        print(results)
     }
 }
