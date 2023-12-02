6722c94015b5c996b2662be065b25a1c555c8ade
PJ <code@software101.co.uk>
2023-12-02 08:09:17 +0000

    2023 Day 2

--- Sources/Year2023/Day2.swift
+++ Sources/Year2023/Day2.swift
@@ -3,4 +3,100 @@ import StandardLibraries
 
 public struct Day2 {
     public init() {}
+    
+    public struct CubeGameLimits {
+        public let red: Int
+        public let green: Int
+        public let blue: Int
+        
+        public init(red: Int, green: Int, blue: Int) {
+            self.red = red
+            self.green = green
+            self.blue = blue
+        }
+    }
+    
+    public func part1(_ input: [String], limits: CubeGameLimits) -> Int {
+        let results = parse(input)
+        var sum = 0
+        
+        for result in results {
+            guard
+                limits.red >= result.maxRed,
+                limits.green >= result.maxGreen,
+                limits.blue >= result.maxBlue
+            else { continue }
+            sum += result.id
+        }
+        
+        return sum
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        let results = parse(input)
+        var sum = 0
+        
+        for result in results {
+            let power = result.maxRed * result.maxGreen * result.maxBlue
+            sum += power
+        }
+        
+        return sum
+    }
+}
+
+extension Day2 {
+    struct CubeBagGameResult {
+        let id: Int
+        let entries: [CubeBagSelection]
+        
+        var maxRed: Int {
+            findMax(cubeType: .red)
+        }
+        
+        var maxGreen: Int {
+            findMax(cubeType: .green)
+        }
+        
+        var maxBlue: Int {
+            findMax(cubeType: .blue)
+        }
+        
+        private func findMax(cubeType: CubeType) -> Int {
+            entries
+                .filter({ $0.cubeType == cubeType })
+                .sorted(by: { $0.numberOfCubes > $1.numberOfCubes })
+                .first?
+                .numberOfCubes ?? 0
+        }
+    }
+    
+    struct CubeBagSelection {
+        let cubeType: CubeType
+        let numberOfCubes: Int
+        
+        init(_ input: String) {
+            let components = input.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: " ")
+            self.numberOfCubes = Int(components[0])!
+            self.cubeType = CubeType(rawValue: components[1])!
+        }
+    }
+    
+    enum CubeType: String {
+        case red, green, blue
+    }
+    
+    func parse(_ input: [String]) -> [CubeBagGameResult] {
+        var results = [CubeBagGameResult]()
+        
+        for line in input {
+            let components = line.components(separatedBy: CharacterSet(arrayLiteral: ":", ",", ";"))
+            let selections = components[1...].map(CubeBagSelection.init)
+            let gameComponents = components[0].components(separatedBy: " ")
+            let gameResult = CubeBagGameResult(id: Int(gameComponents[1])!, entries: selections)
+            results.append(gameResult)
+        }
+        
+        return results
+    }
 }
--- Tests/Year2023Tests/Day2Tests.swift
+++ Tests/Year2023Tests/Day2Tests.swift
@@ -7,15 +7,26 @@ class Day2Tests: XCTestCase {
     let input = Input("Day2.input", Bundle.module).lines
     let day = Day2()
 
-//    func test_part1() {
-////        measure {
-//        XCTAssertEqual(1, day.part1(input))
-////        }
-//    }
-//    
-//    func test_part2() {
-////        measure {
-//        XCTAssertEqual(1, day.part2(input))
-////        }
-//    }
+    func test_part1() {
+//        measure {
+        XCTAssertEqual(2265, day.part1(input, limits: Day2.CubeGameLimits(red: 12, green: 13, blue: 14)))
+//        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+""".lines
+        XCTAssertEqual(8, day.part1(input, limits: Day2.CubeGameLimits(red: 12, green: 13, blue: 14)))
+    }
+    
+    func test_part2() {
+//        measure {
+        XCTAssertEqual(64097, day.part2(input))
+//        }
+    }
 }
