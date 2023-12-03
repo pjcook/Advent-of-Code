69344690346f670d7af6aa906a7f689dea8e5ad0
PJ <code@software101.co.uk>
2023-12-03 08:13:25 +0000

    2023 day 3

--- Tests/Year2023Tests/Day3Tests.swift
+++ Tests/Year2023Tests/Day3Tests.swift
@@ -7,15 +7,47 @@ class Day3Tests: XCTestCase {
     let input = Input("Day3.input", Bundle.module).lines
     let day = Day3()
 
-//    func test_part1() {
-////        measure {
-//        XCTAssertEqual(1, day.part1(input))
-////        }
-//    }
-//    
-//    func test_part2() {
-////        measure {
-//        XCTAssertEqual(1, day.part2(input))
-////        }
-//    }
+    func test_part1() {
+//        measure {
+        XCTAssertEqual(529618, day.part1(input))
+//        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+""".lines
+        XCTAssertEqual(4361, day.part1(input))
+    }
+    
+    func test_part2() {
+//        measure {
+        XCTAssertEqual(77509019, day.part2(input))
+//        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+""".lines
+        XCTAssertEqual(467835, day.part2(input))
+    }
 }
