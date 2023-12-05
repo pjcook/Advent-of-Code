d6217a4b47359e7e0b17f9545700e798ca46765b
PJ <code@software101.co.uk>
2023-12-05 14:14:24 +0000

    2023 Day 5

--- Sources/Year2023/Day5.swift
+++ Sources/Year2023/Day5.swift
@@ -3,4 +3,184 @@ import StandardLibraries
 
 public struct Day5 {
     public init() {}
+    
+    public func part1(_ input: [String]) -> Int {
+        let mapRanges = parse(input)
+        return mapRanges.lowestLocation(seeds: mapRanges.seeds)
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        let mapRanges = parse(input)
+
+        var smallestDistance = Int.max
+        for (startSeedNumber, rangeLength) in mapRanges.seedRanges {
+            let validSeedToSoilRanges = mapRanges.ranges[.seedToSoil]!
+                .filter({
+                    ($0.sourceRangeStart...$0.sourceRangeStart+$0.rangeLength).overlaps(startSeedNumber...startSeedNumber+rangeLength)
+                })
+            
+            let seedValues = validSeedToSoilRanges.map { max($0.sourceRangeStart, startSeedNumber) }
+            let result = mapRanges.lowestLocation(seeds: seedValues)
+            smallestDistance = min(result, smallestDistance)
+        }
+        
+        return smallestDistance
+    }
+
+    func divideAndConquer(start: Int, length: Int, mapRanges: MapRanges) -> Int {
+        var startSeedNumber = start
+        var rangeLength = length
+        
+        while rangeLength > 5000 {
+            let halfRange = rangeLength / 2
+            let quarterRange = halfRange / 2
+            let lower = mapRanges.lowestLocation(seeds: [startSeedNumber + quarterRange])
+            let upper = mapRanges.lowestLocation(seeds: [startSeedNumber + halfRange + quarterRange])
+            
+            rangeLength = halfRange
+            if upper < lower {
+                startSeedNumber = startSeedNumber + halfRange
+            }
+        }
+        
+        let result = mapRanges.lowestLocation(seeds: (startSeedNumber...startSeedNumber+rangeLength))
+        return result
+    }
+    
+    public func test(_ input: [String], seeds: [Int]) -> Int {
+        let mapRanges = parse(input)
+        return mapRanges.lowestLocation(seeds: seeds)
+    }
+    
+    struct MapRange {
+        let destinationRangeStart: Int
+        let sourceRangeStart: Int
+        let rangeLength: Int
+        
+        func destination(input: Int) -> Int? {
+            guard (sourceRangeStart..<sourceRangeStart + rangeLength).contains(input) else { return nil }
+            return destinationRangeStart + (input - sourceRangeStart)
+        }
+    }
+    
+    struct MapRanges {
+        let seeds: [Int]
+        let ranges: [RangeType: [MapRange]]
+        
+        var seedRanges: [(Int,Int)] {
+            var index = 0
+            var results = [(Int,Int)]()
+            while index < seeds.count {
+                let startSeedNumber = seeds[index]
+                let range = seeds[index+1]
+                index += 2
+                results.append((startSeedNumber, range))
+            }
+            return results
+        }
+        
+        func destination(for rangeType: RangeType, input: Int) -> Int {
+            for range in self.ranges[rangeType]! {
+                if let result = range.destination(input: input) {
+                    return result
+                }
+            }
+            return input
+        }
+        
+        func locations(seeds: [Int]) -> [Int] {
+            var results = [Int]()
+
+            for seed in seeds {
+                var result = seed
+                for rangeType in RangeType.allCases {
+                    result = destination(for: rangeType, input: result)
+                }
+                results.append(result)
+            }
+            
+            return results
+        }
+        
+        func lowestLocation(seeds: [Int]) -> Int {
+            var lowest = Int.max
+
+            for seed in seeds {
+                var result = seed
+                for rangeType in RangeType.allCases {
+                    result = destination(for: rangeType, input: result)
+                }
+                
+                if result < lowest {
+                    lowest = result
+                }
+            }
+            
+            return lowest
+        }
+        
+        func lowestLocation(seeds: ClosedRange<Int>) -> Int {
+            var lowest = Int.max
+
+            for seed in seeds {
+                var result = seed
+                for rangeType in RangeType.allCases {
+                    result = destination(for: rangeType, input: result)
+                }
+                
+                if result < lowest {
+                    lowest = result
+                }
+            }
+            
+            return lowest
+        }
+    }
+    
+    enum RangeType: String, CaseIterable {
+        case seedToSoil = "seed-to-soil"
+        case soilToFertilizer = "soil-to-fertilizer"
+        case fertilizerToWater = "fertilizer-to-water"
+        case waterToLight = "water-to-light"
+        case lightToTemperature = "light-to-temperature"
+        case temperatureToHumidity = "temperature-to-humidity"
+        case humidityToLocation = "humidity-to-location"
+    }
+}
+
+extension Day5 {
+    func parse(_ input: [String]) -> MapRanges {
+        var results = [RangeType: [MapRange]]()
+        var rangeName = ""
+        var ranges = [MapRange]()
+        var seeds = [Int]()
+        
+        for line in input {
+            guard !line.isEmpty else {
+                if !rangeName.isEmpty {
+                    results[RangeType(rawValue: rangeName)!] = ranges
+                }
+                ranges = []
+                continue
+            }
+            
+            let components = line.components(separatedBy: " ")
+            if let destinationRangeStart = Int(components[0]) {
+                let sourceRangeStart = Int(components[1])!
+                let rangeLength = Int(components[2])!
+                let range = MapRange(destinationRangeStart: destinationRangeStart, sourceRangeStart: sourceRangeStart, rangeLength: rangeLength)
+                ranges.append(range)
+            } else if components[0] == "seeds:" {
+                seeds = components[1...].map({ Int($0)! })
+            } else {
+                rangeName = components[0]
+            }
+        }
+        
+        if !rangeName.isEmpty {
+            results[RangeType(rawValue: rangeName)!] = ranges
+        }
+        
+        return MapRanges(seeds: seeds, ranges: results)
+    }
 }
--- Tests/Year2023Tests/Day5Tests.swift
+++ Tests/Year2023Tests/Day5Tests.swift
@@ -1,22 +1,130 @@
 import XCTest
 import InputReader
 import StandardLibraries
-import Year2023
+@testable import Year2023
 
 class Day5Tests: XCTestCase {
     
     let input = Input("Day5.input", Bundle.module).lines
     let day = Day5()
 
-//    func test_part1() {
-////        measure {
-//        XCTAssertEqual(1, day.part1(input))
-////        }
-//    }
-//    
-//    func test_part2() {
-////        measure {
-//        XCTAssertEqual(1, day.part2(input))
-////        }
-//    }
+    func test_part1() {
+//        measure {
+        XCTAssertEqual(3374647, day.part1(input))
+//        }
+    }
+    
+    func test_part2() {
+//        measure {
+        XCTAssertEqual(6082852, day.part2(input))
+//        }
+    }
+    
+    func test_rangeType_iterationOrder() {
+        XCTAssertEqual(Day5.RangeType.allCases[0], .seedToSoil)
+        XCTAssertEqual(Day5.RangeType.allCases[1], .soilToFertilizer)
+        XCTAssertEqual(Day5.RangeType.allCases[2], .fertilizerToWater)
+        XCTAssertEqual(Day5.RangeType.allCases[3], .waterToLight)
+        XCTAssertEqual(Day5.RangeType.allCases[4], .lightToTemperature)
+        XCTAssertEqual(Day5.RangeType.allCases[5], .temperatureToHumidity)
+        XCTAssertEqual(Day5.RangeType.allCases[6], .humidityToLocation)
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(46, day.part2(testInput))
+    }
+    
+    func test_part2_example2() {
+        XCTAssertEqual(82, day.test(testInput, seeds: [79]))
+        XCTAssertEqual(83, day.test(testInput, seeds: [80]))
+        XCTAssertEqual(84, day.test(testInput, seeds: [81]))
+        XCTAssertEqual(46, day.test(testInput, seeds: [82]))
+        XCTAssertEqual(47, day.test(testInput, seeds: [83]))
+        
+        XCTAssertEqual(46, day.test(testInput, seeds: [79,80,81,82,83,84,85,86,87,88,89,90,91,92]))
+        XCTAssertEqual(56, day.test(testInput, seeds: [55,56,57,58,59,60,61,62,63,64,64,65,66]))
+    }
+    
+    func test_stuff2() {
+        let mapRanges = day.parse(input)
+        //let result = day.divideAndConquer(start: 485_317_202, length: 73_175_807, mapRanges: mapRanges)
+        
+        print(
+            mapRanges.ranges[.seedToSoil]!
+                .filter({
+                    ($0.sourceRangeStart...$0.sourceRangeStart+$0.rangeLength).overlaps(485_317_202...485_317_202+73_175_807)
+                })
+        )
+        
+        // 485_317_202 +  73_175_807 = 558_493_009
+        // 377_104_678 + 167_790_244 = 544_894_922
+        // 544_894_922 +  55_279_380 = 600_174_302
+        
+        // 10533961
+//        print(result)
+
+        // 3493343155
+        // 3493443255
+        // 3503443255
+        // 2677653796
+        // 2677353796
+        
+        // 10533961
+        // 6082852  > 544_894_922
+        // 11082852 > 549_894_922
+        // 16082852 > 554_894_922
+        print(mapRanges.locations(seeds: [544_894_923]))
+    }
+    
+    func test_stuff() {
+        var smallestDistance = Int.max
+        let mapRanges = day.parse(input)
+        for (startSeedNumber, rangeLength) in mapRanges.seedRanges {
+            let validSeedToSoilRanges = mapRanges.ranges[.seedToSoil]!
+                .filter({
+                    ($0.sourceRangeStart...$0.sourceRangeStart+$0.rangeLength).overlaps(startSeedNumber...startSeedNumber+rangeLength)
+                })
+            
+            let seedValues = validSeedToSoilRanges.map { $0.sourceRangeStart }
+            let result = mapRanges.lowestLocation(seeds: seedValues)
+            smallestDistance = min(result, smallestDistance)
+        }
+        XCTAssertEqual(6082852, smallestDistance)
+    }
+    
+    let testInput = """
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+""".lines
 }
