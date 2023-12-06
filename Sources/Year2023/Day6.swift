0cd35ec19419c570f7be4c2da5d7b2c9fdc8b1f7
PJ <code@software101.co.uk>
2023-12-06 06:14:36 +0000

    2023 Day 6

--- Sources/Year2023/Day6.swift
+++ Sources/Year2023/Day6.swift
@@ -3,4 +3,49 @@ import StandardLibraries
 
 public struct Day6 {
     public init() {}
+    
+    /*
+     Time:      7  15   30
+     Distance:  9  40  200
+     */
+    public func part1(time: [Int], distance: [Int]) -> Int {
+        var results = [Int]()
+        for (t,d) in zip(time, distance) {
+            var count = 0
+            for i in (1..<t) {
+                let result = (t-i) * i
+                if result > d {
+                    count += 1
+                }
+            }
+            if count > 0 {
+                results.append(count)
+            }
+        }
+        return results.reduce(1, *)
+    }
+    
+    public func part2(time: Int, distance: Int) -> Int {
+        let halfway = time / 2
+        var count = 0
+        for i in (halfway..<time) {
+            let result = (time-i) * i
+            if result > distance {
+                count += 1
+            } else {
+                break
+            }
+        }
+        
+        for j in (1..<halfway) {
+            let i = halfway - j
+            let result = (time-i) * i
+            if result > distance {
+                count += 1
+            } else {
+                break
+            }
+        }
+        return count
+    }
 }
--- Tests/Year2023Tests/Day6Tests.swift
+++ Tests/Year2023Tests/Day6Tests.swift
@@ -4,18 +4,27 @@ import Year2023
 
 class Day6Tests: XCTestCase {
     
-    let input = Input("Day6.input", Bundle.module).input
+//    let input = Input("Day6.input", Bundle.module).input
     let day = Day6()
 
-//    func test_part1() {
-////        measure {
-//        XCTAssertEqual(1, day.part1(input))
-////        }
-//    }
-//    
-//    func test_part2() {
-////        measure {
-//        XCTAssertEqual(1, day.part2(input))
-////        }
-//    }
+    func test_part1() {
+//        measure {
+        XCTAssertEqual(393120, day.part1(time: [62,73,75,65], distance: [644,1023,1240,1023]))
+//        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(288, day.part1(time: [7,15,30], distance: [9,40,200]))
+    }
+    
+    func test_part2() {
+//        measure {
+        XCTAssertEqual(36872656, day.part2(time: 62737565, distance: 644102312401023))
+//        }
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(71503, day.part2(time: 71530, distance: 940200))
+    }
+
 }
