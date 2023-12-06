494dc01b57b21b74c292ad3d0b79ebb0b64fca78
PJ <code@software101.co.uk>
2023-12-06 07:57:57 +0000

    2023 day 6 performance optimisations

--- Sources/Year2023/Day6.swift
+++ Sources/Year2023/Day6.swift
@@ -4,48 +4,37 @@ import StandardLibraries
 public struct Day6 {
     public init() {}
     
-    /*
-     Time:      7  15   30
-     Distance:  9  40  200
-     */
     public func part1(time: [Int], distance: [Int]) -> Int {
         var results = [Int]()
         for (t,d) in zip(time, distance) {
-            var count = 0
-            for i in (1..<t) {
-                let result = (t-i) * i
-                if result > d {
-                    count += 1
-                }
-            }
-            if count > 0 {
-                results.append(count)
-            }
+            results.append(countWins(time: t, distance: d))
         }
         return results.reduce(1, *)
     }
     
     public func part2(time: Int, distance: Int) -> Int {
+        countWins(time: time, distance: distance)
+    }
+    
+    func countWins(time: Int, distance: Int) -> Int {
         let halfway = time / 2
-        var count = 0
-        for i in (halfway..<time) {
-            let result = (time-i) * i
-            if result > distance {
+        var count = 1
+        for i in (1..<halfway) {
+            let upper = halfway+i
+            let lower = halfway-i
+            let result1 = (time-upper) * upper
+            let result2 = (time-lower) * lower
+            if result1 > distance {
                 count += 1
-            } else {
+            }
+            if result2 > distance {
+                count += 1
+            }
+            if result1 <= distance || result2 <= distance {
                 break
             }
         }
         
-        for j in (1..<halfway) {
-            let i = halfway - j
-            let result = (time-i) * i
-            if result > distance {
-                count += 1
-            } else {
-                break
-            }
-        }
         return count
     }
 }
