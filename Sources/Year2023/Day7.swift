dcc2fc6436f6d70b8c7831f26fb0da57500c4084
PJ <code@software101.co.uk>
2023-12-07 13:48:53 +0000

    2023 day 7 cleaned up code

--- Sources/Year2023/Day7.swift
+++ Sources/Year2023/Day7.swift
@@ -18,54 +18,19 @@ public struct Day7 {
         let cards: String
         let handType: WinningHandType
         let bid: Int
-        
-        init(cards: String, bid: Int, handType: WinningHandType) {
-            self.cards = cards
-            self.bid = bid
-            self.handType = handType
-        }
     }
     
     public func part1(_ input: [String]) -> Int {
-        let hands = parse(input, calculateWinningHandType: calculateWinningHandTypePart1).sorted(by: <)
-        var sum = 0
-        for (index, hand) in hands.enumerated() {
-            let rank = index + 1
-            sum += hand.bid * rank
-        }
-        return sum
-    }
-    
-    func calculateWinningHandTypePart1(from cards: String) -> Day7.WinningHandType {
-        var components = [Character:Int]()
-        for card in cards {
-            components[card] = components[card, default: 0] + 1
-        }
-        
-        if components.count == 1 {
-            return .fiveOfAKind
-        } else if components.count == 2 {
-            if [1,4].contains(components.first!.value) {
-                return .fourOfAKind
-            } else {
-                return .fullHouse
-            }
-        } else if components.count == 3 {
-            for value in components.values {
-                if value == 3 {
-                    return .threeOfAKind
-                }
-            }
-            return .twoPair
-        } else if components.count == 4 {
-            return .onePair
-        } else {
-            return .highCard
-        }
+        calculateAnswer(input, useJokers: false, cardStrength: cardStrengthPart1)
     }
     
     public func part2(_ input: [String]) -> Int {
-        let hands = parse(input, calculateWinningHandType: calculateWinningHandTypePart2).sorted(by: sortPart2)
+        calculateAnswer(input, useJokers: true, cardStrength: cardStrengthPart2)
+    }
+    
+    func calculateAnswer(_ input: [String], useJokers: Bool, cardStrength: [String:Int]) -> Int {
+        let hands = parse(input, useJokers: useJokers)
+            .sorted(by: { sortHands(lhs: $0, rhs: $1, cardStrength: cardStrength) })
         var sum = 0
         for (index, hand) in hands.enumerated() {
             let rank = index + 1
@@ -74,45 +39,11 @@ public struct Day7 {
         return sum
     }
     
-    func calculateWinningHandTypePart2(from cards: String) -> Day7.WinningHandType {
-        var components = [Character:Int]()
-        for card in cards {
-            components[card] = components[card, default: 0] + 1
-        }
-        
-        if components.count > 1, let jokerCount = components["J"] {
-            components.removeValue(forKey: "J")
-            let key = components.sorted(by: { $0.value > $1.value }).first!.key
-            components[key] = components[key]! + jokerCount
-        }
-        
-        if components.count == 1 {
-            return .fiveOfAKind
-        } else if components.count == 2 {
-            if [1,4].contains(components.first!.value) {
-                return .fourOfAKind
-            } else {
-                return .fullHouse
-            }
-        } else if components.count == 3 {
-            for value in components.values {
-                if value == 3 {
-                    return .threeOfAKind
-                }
-            }
-            return .twoPair
-        } else if components.count == 4 {
-            return .onePair
-        } else {
-            return .highCard
-        }
-    }
-    
-    func sortPart2(lhs: Hand, rhs: Hand) -> Bool {
+    func sortHands(lhs: Hand, rhs: Hand, cardStrength: [String:Int]) -> Bool {
         if lhs.handType == rhs.handType {
             for i in (0..<lhs.cards.count) {
-                let lhsCardStrength = cardStrengthPart2[String(lhs.cards[i]), default: 0]
-                let rhsCardStrength = cardStrengthPart2[String(rhs.cards[i]), default: 0]
+                let lhsCardStrength = cardStrength[String(lhs.cards[i]), default: 0]
+                let rhsCardStrength = cardStrength[String(rhs.cards[i]), default: 0]
                 
                 if lhsCardStrength == rhsCardStrength {
                     continue
@@ -125,6 +56,22 @@ public struct Day7 {
         
         return lhs.handType.rawValue < rhs.handType.rawValue
     }
+    
+    let cardStrengthPart1 = [
+        "2":1,
+        "3":2,
+        "4":3,
+        "5":4,
+        "6":5,
+        "7":6,
+        "8":7,
+        "9":8,
+        "T":9,
+        "J":10,
+        "Q":11,
+        "K":12,
+        "A":13
+    ]
     
     let cardStrengthPart2 = [
         "2":1,
@@ -143,53 +90,42 @@ public struct Day7 {
     ]
 }
 
-extension Day7.Hand {
-    static func < (lhs: Day7.Hand, rhs: Day7.Hand) -> Bool {
-        if lhs.handType == rhs.handType {
-            for i in (0..<lhs.cards.count) {
-                let lhsCardStrength = cardStrength[String(lhs.cards[i]), default: 0]
-                let rhsCardStrength = cardStrength[String(rhs.cards[i]), default: 0]
-                
-                if lhsCardStrength == rhsCardStrength {
-                    continue
-                }
-                return lhsCardStrength < rhsCardStrength
-            }
-            // they match completely so keep existing order
-            return true
-        }
-        
-        return lhs.handType.rawValue < rhs.handType.rawValue
-    }
-    
-    static let cardStrength = [
-        "2":1,
-        "3":2,
-        "4":3,
-        "5":4,
-        "6":5,
-        "7":6,
-        "8":7,
-        "9":8,
-        "T":9,
-        "J":10,
-        "Q":11,
-        "K":12,
-        "A":13
-    ]
-}
-
 extension Day7 {
-    func parse(_ input: [String], calculateWinningHandType: (String) -> Day7.WinningHandType) -> [Hand] {
+    func parse(_ input: [String], useJokers: Bool) -> [Hand] {
         var hands = [Hand]()
         
         for line in input {
             let components = line.components(separatedBy: " ")
-            let handType = calculateWinningHandType(components[0])
-            let hand = Hand(cards: components[0], bid: Int(String(components[1]))!, handType: handType)
+            let handType = calculateWinningHandType(from: components[0], useJokers: useJokers)
+            let hand = Hand(cards: components[0], handType: handType, bid: Int(String(components[1]))!)
             hands.append(hand)
         }
         
         return hands
+    }
+    
+    func calculateWinningHandType(from cards: String, useJokers: Bool) -> Day7.WinningHandType {
+        var components = [Character:Int]()
+        for card in cards {
+            components[card] = components[card, default: 0] + 1
+        }
+        
+        if useJokers, components.count > 1, let jokerCount = components["J"] {
+            components.removeValue(forKey: "J")
+            let key = components.sorted(by: { $0.value > $1.value }).first!.key
+            components[key] = components[key]! + jokerCount
+        }
+        
+        let sortedCounts = components.values.sorted()
+        
+        switch sortedCounts {
+            case [5]: return .fiveOfAKind
+            case [1,4]: return .fourOfAKind
+            case [2,3]: return .fullHouse
+            case [1,1,3]: return .threeOfAKind
+            case [1,2,2]: return .twoPair
+            case [1,1,1,2]: return .onePair
+            default: return .highCard
+        }
     }
 }
