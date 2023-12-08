275aeab0fc0198c8703d66048936a97955c8ae3c
PJ <code@software101.co.uk>
2023-12-08 08:38:55 +0000

    2023 Day 8

--- Sources/StandardLibraries/Int+Additions.swift
+++ Sources/StandardLibraries/Int+Additions.swift
@@ -9,4 +9,11 @@ public extension Int {
         let binary = self.binary
         return String(repeating: character, count: padLength - binary.count) + binary
     }
+    
+    var isPrime: Bool {
+        guard self >= 2     else { return false }
+        guard self != 2     else { return true  }
+        guard self % 2 != 0 else { return false }
+        return !stride(from: 3, through: Int(sqrt(Double(self))), by: 2).contains { self % $0 == 0 }
+    }
 }
