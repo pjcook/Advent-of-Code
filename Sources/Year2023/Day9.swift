e9447664ea366d496faed30b628d291fca1f6469
PJ <code@software101.co.uk>
2023-12-09 07:26:42 +0000

    2023 Day 9

--- Sources/Year2023/Day9.swift
+++ Sources/Year2023/Day9.swift
@@ -3,4 +3,60 @@ import StandardLibraries
 
 public struct Day9 {
     public init() {}
+    
+    public func part1(_ input: [String]) -> Int {
+        parse(input)
+            .map({ extrapolateNextValue($0) })
+            .reduce(0, +)
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        parse(input)
+            .map({ extrapolatePreviousValue($0) })
+            .reduce(0, +)
+    }
+}
+
+extension Day9 {
+    func extrapolateNextValue(_ items: [Int], depth: Int = 0) -> Int {
+        var results = [Int]()
+        for i in (1..<items.count) {
+            results.append(items[i] - items[i-1])
+        }
+        
+        guard !(results.first == 0 && results.last == 0) else { return 0 }
+        let result = results.last! + extrapolateNextValue(results, depth: depth + 1)
+        
+        if depth == 0 {
+            return result + items.last!
+        } else {
+            return result
+        }
+    }
+    
+    public func extrapolatePreviousValue(_ items: [Int], depth: Int = 0) -> Int {
+        var results = [Int]()
+        for i in (1..<items.count) {
+            results.append(items[i] - items[i-1])
+        }
+        
+        guard !(results.first == 0 && results.last == 0) else { return 0 }
+        let result = results.first! - extrapolatePreviousValue(results, depth: depth + 1)
+        
+        if depth == 0 {
+            return items.first! - result
+        } else {
+            return result
+        }
+    }
+}
+
+extension Day9 {
+    func parse(_ input: [String]) -> [[Int]] {
+        input.map {
+            $0.components(separatedBy: " ").map {
+                Int(String($0))!
+            }
+        }
+    }
 }
--- Tests/Year2023Tests/Day9Tests.swift
+++ Tests/Year2023Tests/Day9Tests.swift
@@ -7,15 +7,33 @@ class Day9Tests: XCTestCase {
     let input = Input("Day9.input", Bundle.module).lines
     let day = Day9()
 
-//    func test_part1() {
-////        measure {
-//        XCTAssertEqual(1, day.part1(input))
-////        }
-//    }
-//    
-//    func test_part2() {
-////        measure {
-//        XCTAssertEqual(1, day.part2(input))
-////        }
-//    }
+    func test_part1() {
+        measure {
+        XCTAssertEqual(2075724761, day.part1(input))
+        }
+    }
+    
+    func test_part1_example1() {
+        let input = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+""".lines
+        XCTAssertEqual(114, day.part1(input))
+    }
+    
+    func test_part2() {
+        measure {
+        XCTAssertEqual(1072, day.part2(input))
+        }
+    }
+    
+    func test_part2_example1() {
+        let input = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+""".lines
+        XCTAssertEqual(2, day.part2(input))
+    }
 }
