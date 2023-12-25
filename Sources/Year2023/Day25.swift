27f697ebae1de6d1c9c44283ad4ffaf2a2d6a997
PJ <pj@MacBook-Pro.local>
2023-12-25 10:14:09 +0000

    2023 day 25

--- Sources/Year2023/Day25.swift
+++ Sources/Year2023/Day25.swift
@@ -3,12 +3,78 @@ import StandardLibraries
 
 public struct Day25 {
     public init() {}
-    
-    public func part1(_ input: [String]) -> Int {
-        1
+    // Code credit goes to: https://www.reddit.com/user/noonan1487/
+    // https://www.reddit.com/user/noonan1487/
+    struct Edge: Hashable, CustomStringConvertible {
+        let a: String
+        let b: String
+        
+        static func == (lhs: Edge, rhs: Edge) -> Bool {
+            (lhs.a == rhs.a && lhs.b == rhs.b) || (lhs.a == rhs.b && lhs.b == rhs.a)
+        }
+        
+        var description: String {
+            "(\(a):\(b))"
+        }
     }
     
-    public func part2(_ input: [String]) -> Int {
-        1
+    public func part1(_ input: [String]) -> Int {
+        let (vertices, edges) = parse(input)
+        
+        var subsets = [[String]]()
+        
+        repeat {
+            subsets = vertices.map { [$0] }
+            var indices = (0..<edges.count).map { $0 }
+            
+            while subsets.count > 2 {
+                let i = indices.randomElement()!
+                indices.removeAll(where: { $0 == i })
+                let edge = edges[i]
+                guard var s1 = subsets.first(where: { $0.contains(edge.a) }) else { continue }
+                guard let s2 = subsets.first(where: { $0.contains(edge.b) }) else { continue }
+                if s1 == s2 { continue }
+                let index = subsets.firstIndex(of: s1)!
+                s1.append(contentsOf: s2)
+                subsets[index] = s1
+                subsets.remove(at: subsets.firstIndex(of: s2)!)
+            }
+        } while countCuts(subsets: subsets, edges: edges) != 3
+        
+        return subsets.reduce(1) { $0 * $1.count }
+    }
+    
+    func countCuts(subsets: [[String]], edges: [Edge]) -> Int {
+        var cuts = 0
+        
+        for edge in edges {
+            let s1 = subsets.first { $0.contains(edge.a) }
+            let s2 = subsets.first { $0.contains(edge.b) }
+            if s1 != s2 { cuts += 1 }
+        }
+        
+        return cuts
+    }
+}
+
+extension Day25 {
+    func parse(_ input: [String]) -> ([String], [Edge]) {
+        var vertices = Set<String>()
+        var edges = Set<Edge>()
+
+        for line in input {
+            let components = line.replacingOccurrences(of: ": ", with: " ")
+                .components(separatedBy: " ")
+
+            let p1 = components[0]
+            vertices.insert(p1)
+
+            for p2 in Array(components[1...]) {
+                vertices.insert(p2)
+                edges.insert(Edge(a: p1, b: p2))
+            }
+        }
+        
+        return (Array(vertices), Array(edges))
     }
 }
--- Tests/Year2023Tests/Day25Tests.swift
+++ Tests/Year2023Tests/Day25Tests.swift
@@ -9,13 +9,27 @@ class Day25Tests: XCTestCase {
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(559143, day.part1(input))
 //        }
     }
     
-    func test_part2() {
-//        measure {
-        XCTAssertEqual(1, day.part2(input))
-//        }
+    func test_part1_example() {
+        /* hfx/pzl, bvb/cmg, nvd/jqt */
+        let input = """
+jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+""".lines
+        XCTAssertEqual(54, day.part1(input))
     }
 }
