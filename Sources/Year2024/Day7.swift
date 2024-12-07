a333cbadc8767e8388af7616f62f40e834e739f9
PJ <pjcook@users.noreply.github.com>
2024-12-07 10:52:58 +0000

    AoC 2024 - Day 7 minor code clean up

--- Sources/Year2024/Day7.swift
+++ Sources/Year2024/Day7.swift
@@ -20,18 +20,15 @@ public struct Day7Item {
     public let values: [Int]
     
     public init(_ input: String) {
-        var components = input.components(separatedBy: CharacterSet(arrayLiteral: ":", " "))
-        self.result = Int(components.removeFirst())!
-        _ = components.removeFirst()
-        self.values = components.map { Int($0)! }
+        let components = input.components(separatedBy: CharacterSet(arrayLiteral: ":", " "))
+        self.result = Int(components.first!)!
+        self.values = components[2...].map { Int($0)! }
     }
     
     public func isValid(with operators: [Operators]) -> Bool {
-        var remaining = self.values
         var results = [Int]()
-        results.append(remaining.removeFirst())
-        while !remaining.isEmpty {
-            let next = remaining.removeFirst()            
+        results.append(values.first!)
+        for next in values[1...] {
             var newResults = [Int]()
             for result in results {
                 for op in operators {
@@ -49,28 +46,18 @@ public struct Day7 {
     public init() {}
     
     public func part1(_ input: [String]) -> Int {
-        let items = input.map(Day7Item.init)
-        var result = 0
-        
-        for item in items {
-            if item.isValid(with: [.addition, .multiply]) {
-                result += item.result
-            }
-        }
-        
-        return result
+        evaluate(input, operators: [.addition, .multiply])
     }
     
     public func part2(_ input: [String]) -> Int {
-        let items = input.map(Day7Item.init)
-        var result = 0
-        
-        for item in items {
-            if item.isValid(with: [.addition, .multiply, .concatenate]) {
-                result += item.result
+        evaluate(input, operators: [.addition, .multiply, .concatenate])
+    }
+    
+    private func evaluate(_ input: [String], operators: [Day7Item.Operators]) -> Int {
+        input
+            .map(Day7Item.init)
+            .reduce(0) { result, item in
+                item.isValid(with: operators) ? result + item.result : result
             }
-        }
-        
-        return result
     }
 }
