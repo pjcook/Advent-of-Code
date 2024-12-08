255960025a7ab53608ed38b8a35b00bb0f585087
PJ <pjcook@users.noreply.github.com>
2024-12-08 09:02:59 +0000

    AoC 2024 - Day 8

--- Sources/Year2024/Day8.swift
+++ Sources/Year2024/Day8.swift
@@ -4,104 +4,84 @@ import StandardLibraries
 public struct Day8 {
     public init() {}
     
-    let startNodeID = "AAA"
-    let endNodeID = "ZZZ"
-    
     public func part1(_ input: [String]) -> Int {
-        let (directions, nodes) = parse(input)
-        var count = 0
-        var directionIndex = 0
-        var node = nodes[startNodeID]!
+        let grid = Grid<String>(input)
+        var results = Set<Point>()
+        var antenna = [String: [Node]]()
         
-        while node.id != endNodeID {
-            count += 1
+        for i in (0..<grid.items.count) {
+            let char = grid.items[i]
+            guard char != "." else { continue }
             
-            let direction = directions[directionIndex]
-            let nextID = direction == .left ? node.left : node.right
-            node = nodes[nextID]!
-            
-            directionIndex += 1
-            if directionIndex >= directions.count {
-                directionIndex = 0
-            }
+            let point = grid.point(for: i)
+            var nodes = antenna[char, default: []]
+            nodes.append(Node(point: point, char: char))
+            antenna[char] = nodes
         }
-        
-        return count
-    }
-    
-    // RLA, AAA, JSA, RXA, QFA, QLA
-    public func part2(_ input: [String]) -> Int {
-        let (directions, nodes) = parse(input)
-        var count = 0
-        var directionIndex = 0
-        var currentNodes = nodes.compactMap { $0.key.hasSuffix("A") ? $0.value : nil }
-        var counts = Array(repeating: 0, count: currentNodes.count)
 
-        while counts.contains(0) {
-            count += 1
-            
-            let direction = directions[directionIndex]
-            
-            for i in (0..<currentNodes.count) {
-                guard counts[i] == 0 else { continue }
-                let node = currentNodes[i]
-                let nextID = direction == .left ? node.left : node.right
-                currentNodes[i] = nodes[nextID]!
-                if nextID.hasSuffix("Z") {
-                    counts[i] = count
+        for (_, items) in antenna where items.count > 1 {
+            for n in (0..<items.count-1) {
+                for j in (n..<items.count-1) {
+                    let p1 = items[n].point
+                    let p2 = items[j+1].point
+                    let d2 = p1 - p2
+                    let point1 = p1 + d2
+                    let point2 = p2 - d2
+                    if point1.isValid(max: grid.bottomRight) {
+                        results.insert(point1)
+                    }
+                    if point2.isValid(max: grid.bottomRight) {
+                        results.insert(point2)
+                    }
                 }
             }
+        }
+        
+        return results.count
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        let grid = Grid<String>(input)
+        var results = Set<Point>()
+        var antenna = [String: [Node]]()
+        
+        for i in (0..<grid.items.count) {
+            let char = grid.items[i]
+            guard char != "." else { continue }
             
-            directionIndex += 1
-            if directionIndex >= directions.count {
-                directionIndex = 0
-            }
+            let point = grid.point(for: i)
+            var nodes = antenna[char, default: []]
+            nodes.append(Node(point: point, char: char))
+            antenna[char] = nodes
         }
-        
-        // Find the prime factorization of the numbers and multiply together to find the smallest common number that they all go into, luckily all the minimum divisors already happen to be prime numbers
-        var primeValues = Set<Int>()
-        
-        for count in counts {
-            var i = 2
-            while count % i != 0 {
-                i += 1
-            }
-            primeValues.insert(i)
-            primeValues.insert(count / i)
-        }
-        
-        return primeValues.sorted().reduce(1, *)
-    }
-    
-    public struct Node {
-        public let id: String
-        public let left: String
-        public let right: String
-    }
-    
-    public enum Direction: Character {
-        case left = "L"
-        case right = "R"
-    }
-}
 
-extension Day8 {
-    public func parse(_ input: [String]) -> ([Direction], [String:Node]) {
-        let directions = input[0].map({ Direction(rawValue: $0)! })
-        var nodes = [String:Node]()
-        
-        for i in (2..<input.count) {
-            let line = input[i]
-                .replacingOccurrences(of: " =", with: "")
-                .replacingOccurrences(of: "(", with: "")
-                .replacingOccurrences(of: ")", with: "")
-                .replacingOccurrences(of: ",", with: "")
-            guard !line.isEmpty else { continue }
-            let components = line.components(separatedBy: " ")
-            let node = Node(id: components[0], left: components[1], right: components[2])
-            nodes[node.id] = node
+        for (_, items) in antenna where items.count > 1 {
+            for n in (0..<items.count-1) {
+                for j in (n..<items.count-1) {
+                    let p1 = items[n].point
+                    let p2 = items[j+1].point
+                    results.insert(p1)
+                    results.insert(p2)
+                    let d2 = p1 - p2
+                    var point1 = p1 + d2
+                    var point2 = p2 - d2
+                    while point1.isValid(max: grid.bottomRight) {
+                        results.insert(point1)
+                        point1 = point1 + d2
+                    }
+                    while point2.isValid(max: grid.bottomRight) {
+                        results.insert(point2)
+                        point2 = point2 - d2
+                    }
+                }
+            }
         }
         
-        return (directions, nodes)
+        return results.count
+    }
+    
+    public struct Node: Hashable {
+        public let point: Point
+        public let char: String
     }
 }
--- Tests/Year2024Tests/Day8Tests.swift
+++ Tests/Year2024Tests/Day8Tests.swift
@@ -10,63 +10,49 @@ class Day8Tests: XCTestCase {
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(18157, day.part1(input))
+        XCTAssertEqual(400, day.part1(input))
 //        }
-    }
-    
-    func test_parser() {
-        let input = """
-RL
-
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ)
-""".lines
-        let (directions, nodes) = day.parse(input)
-        XCTAssertEqual(directions.count, 2)
-        XCTAssertEqual(nodes.count, 7)
-        
-        XCTAssertEqual(directions, [.right, .left])
     }
     
     func test_part1_example1() {
         let input = """
-RL
-
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ)
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
 """.lines
-        XCTAssertEqual(2, day.part1(input))
+        XCTAssertEqual(14, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(14299763833181, day.part2(input))
+        XCTAssertEqual(1280, day.part2(input))
 //        }
     }
     
     func test_part2_example1() {
         let input = """
-LR
-
-11A = (11B, XXX)
-11B = (XXX, 11Z)
-11Z = (11B, XXX)
-22A = (22B, XXX)
-22B = (22C, 22C)
-22C = (22Z, 22Z)
-22Z = (22B, 22B)
-XXX = (XXX, XXX)
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
 """.lines
-        XCTAssertEqual(6, day.part2(input))
+        XCTAssertEqual(34, day.part2(input))
     }
 }
