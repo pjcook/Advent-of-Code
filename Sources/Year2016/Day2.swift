c068756becaf55f603687aae0f941eebb313e068
PJ <pjcook@users.noreply.github.com>
2024-12-30 12:22:41 +0000

    AoC 2016 - Day 2

--- Sources/Year2016/Day2.swift
+++ Sources/Year2016/Day2.swift
@@ -7,15 +7,66 @@
 //
 
 import Foundation
+import InputReader
+import StandardLibraries
 
 public struct Day2 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> String {
+        let grid = Grid<Int>(columns: 3, items: [1,2,3,4,5,6,7,8,9])
+        var point = Point(1,1)
+        var results = [Int]()
+        
+        for line in input {
+            for char in line {
+                var next = point
+                switch char {
+                case "U": next.y -= 1
+                case "D": next.y += 1
+                case "L": next.x -= 1
+                case "R": next.x += 1
+                default: continue
+                }
+                if next.isValid(max: grid.bottomRight) {
+                    point = next
+                }
+            }
+            results.append(grid[point])
+        }
+        
+        return results.map(String.init).joined()
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> String {
+        let keypad = """
+XX1XX
+X234X
+56789
+XABCX
+XXDXX
+""".lines
+        let grid = Grid<String>(keypad)
+        var point = Point(1,1)
+        var results = [String]()
+        
+        for line in input {
+            for char in line {
+                var next = point
+                switch char {
+                case "U": next.y -= 1
+                case "D": next.y += 1
+                case "L": next.x -= 1
+                case "R": next.x += 1
+                default: continue
+                }
+                if next.isValid(max: grid.bottomRight) && grid[next] != "X" {
+                    point = next
+                }
+            }
+            results.append(grid[point])
+        }
+        
+        return results.reduce("") { $0 + $1 }
     }
 }
--- Tests/Year2016Tests/Day2Tests.swift
+++ Tests/Year2016Tests/Day2Tests.swift
@@ -4,18 +4,29 @@ import Year2016
 
 class Day2Tests: XCTestCase {
     
-    let input = Input("Day2.input", Bundle.module).integers
+    let input = Input("Day2.input", Bundle.module).lines
     let day = Day2()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual("69642", day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+ULL
+RRDDD
+LURDL
+UUUUD
+""".lines
+        
+        XCTAssertEqual("1985", day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual("8CB23", day.part2(input))
 //        }
     }
 }
