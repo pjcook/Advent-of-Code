3efba636fc324688ed4128f196ec126621ce93b1
PJ <pjcook@users.noreply.github.com>
2024-12-30 14:37:27 +0000

    AoC 2016 - Day 6

--- Sources/Year2016/Day6.swift
+++ Sources/Year2016/Day6.swift
@@ -7,15 +7,52 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day6 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> String {
+        let cols = input[0].count
+        var columns = Array(repeating: [String: Int](), count: cols)
+        
+        for row in input {
+            for i in 0..<cols {
+                let c = String(row[i])
+                var dict = columns[i]
+                dict[c] = dict[c, default: 0] + 1
+                columns[i] = dict
+            }
+        }
+        
+        var word = ""
+        
+        for col in columns {
+            word.append(col.sorted(by: { $0.value > $1.value }).first!.key)
+        }
+        
+        return word
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> String {
+        let cols = input[0].count
+        var columns = Array(repeating: [String: Int](), count: cols)
+        
+        for row in input {
+            for i in 0..<cols {
+                let c = String(row[i])
+                var dict = columns[i]
+                dict[c] = dict[c, default: 0] + 1
+                columns[i] = dict
+            }
+        }
+        
+        var word = ""
+        
+        for col in columns {
+            word.append(col.sorted(by: { $0.value < $1.value }).first!.key)
+        }
+        
+        return word
     }
 }
--- Tests/Year2016Tests/Day6Tests.swift
+++ Tests/Year2016Tests/Day6Tests.swift
@@ -4,18 +4,62 @@ import Year2016
 
 class Day6Tests: XCTestCase {
     
-    let input = Input("Day6.input", Bundle.module).integers
+    let input = Input("Day6.input", Bundle.module).lines
     let day = Day6()
 
     func test_part6() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual("umcvzsmw", day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar
+""".lines
+        XCTAssertEqual("easter", day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual("rwqoacfz", day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar
+""".lines
+        XCTAssertEqual("advent", day.part2(input))
     }
 }
