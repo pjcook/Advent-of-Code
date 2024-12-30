0ae250e157435bb4859806eadfc40d7a9d63e6f1
PJ <pjcook@users.noreply.github.com>
2024-12-30 12:47:48 +0000

    AoC 2016 - Day 3

--- Sources/Year2016/Day3.swift
+++ Sources/Year2016/Day3.swift
@@ -7,15 +7,61 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day3 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        parse(input)
+            .compactMap {
+                isValid($0) ? $0 : nil
+            }
+            .count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        let values = parse(input)
+        var count = 0
+        var i = 2
+        
+        while i < values.count {
+            for j in 0..<3 {
+                if isValid([
+                    values[i-2][j],
+                    values[i-1][j],
+                    values[i][j]
+                ]) {
+                    count += 1
+                }
+            }
+            
+            i += 3
+        }
+                
+        return count
+    }
+    
+    func isValid(_ values: [Int]) -> Bool {
+        let o = values.sorted(by: <)
+        return o[0] + o[1] > o[2]
+    }
+}
+
+extension Day3 {
+    func parse(_ input: [String]) -> [[Int]] {
+        var values = [[Int]]()
+        
+        for line in input {
+            values.append(line
+                .replacingOccurrences(of: "    ", with: " ")
+                .replacingOccurrences(of: "   ", with: " ")
+                .replacingOccurrences(of: "  ", with: " ")
+                .split(separator: " ")
+                .map { Int($0)! }
+            )
+        }
+        
+        return values
     }
 }
--- Tests/Year2016Tests/Day3Tests.swift
+++ Tests/Year2016Tests/Day3Tests.swift
@@ -4,18 +4,18 @@ import Year2016
 
 class Day3Tests: XCTestCase {
     
-    let input = Input("Day3.input", Bundle.module).integers
+    let input = Input("Day3.input", Bundle.module).lines
     let day = Day3()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(993, day.part1(input))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(1849, day.part2(input))
 //        }
     }
 }
