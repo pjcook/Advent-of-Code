52a4316d40f8a3b8a05acfa352afd218a856882d
PJ <pjcook@users.noreply.github.com>
2024-12-30 13:44:53 +0000

    AoC 2016 - Day 4

--- Sources/Year2016/Day4.swift
+++ Sources/Year2016/Day4.swift
@@ -7,15 +7,76 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day4 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        var count = 0
+        
+        for line in input {
+            let (components, sectorID, checksum) = parse(line)
+            var chars = [Character: Int]()
+            for component in components {
+                for c in component {
+                    chars[c] = chars[c, default: 0] + 1
+                }
+            }
+            if chars
+                .sorted(by: {
+                    if $0.value == $1.value {
+                        return $0.key < $1.key
+                    }
+                    return $0.value > $1.value
+                })
+                .prefix(5)
+                .map(\.key)
+                .sorted()
+                .map(String.init)
+                .joined() == checksum.sorted().map(String.init).joined() {
+                count += sectorID
+            }
+        }
+        
+        return count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        for line in input {
+            if decrypt(line) == "northpole object storage" {
+                let (_, sectorID, _) = parse(line)
+                return sectorID
+            }
+        }
+        
+        return -1
+    }
+    
+    public func decrypt(_ input: String) -> String {
+        let (components, sectorID, _) = parse(input)
+        let m = sectorID % 26
+        
+        var words = [String]()
+        
+        for component in components {
+            var word = ""
+            for c in component {
+                let index =  (Alphabet.lettersList.firstIndex(of: c)! + m) % 26
+                word.append(Alphabet.lettersList[index])
+            }
+            words.append(word)
+        }
+        
+        print(words.joined(separator: " "))
+        return words.joined(separator: " ")
+    }
+    
+    public func parse(_ input: String) -> ([String], Int, String) {
+        let parts = input.replacingOccurrences(of: "]", with: "").split(separator: "[")
+        let checksum = String(parts[1])
+        var components = parts[0].split(separator: "-")
+        let sectorID = Int(String(components.removeLast()))!
+        return (components.map(String.init), sectorID, checksum)
     }
 }
--- Tests/Year2016Tests/Day4Tests.swift
+++ Tests/Year2016Tests/Day4Tests.swift
@@ -4,7 +4,7 @@ import Year2016
 
 class Day4Tests: XCTestCase {
     
-    let input = Input("Day4.input", Bundle.module).integers
+    let input = Input("Day4.input", Bundle.module).lines
     let day = Day4()
 
     func test_part4() {
@@ -13,9 +13,23 @@ class Day4Tests: XCTestCase {
 //        }
     }
     
+    func test_part4_example() {
+        let input = """
+aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
+""".lines
+        XCTAssertEqual(1514, day.part1(input))
+    }
+    
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(548, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual("very encrypted name", day.decrypt("qzmt-zixmtkozy-ivhz-343[abc]"))
     }
 }
