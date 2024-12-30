284ffb962c6f82b23d7d1e9332093e9414cab30a
PJ <pjcook@users.noreply.github.com>
2024-12-30 15:19:53 +0000

    AoC 2016 - Day 7

--- Sources/Year2016/Day7.swift
+++ Sources/Year2016/Day7.swift
@@ -7,15 +7,156 @@
 //
 
 import Foundation
+import StandardLibraries
 
-public struct Day7 {
+public class Day7 {
+    private var cache = [String: Bool]()
+    
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let lists = parse(input)
+        var count = 0
+        
+        outerLoop: for list in lists {
+            var insides = [Values]()
+            var outsides = [Values]()
+            var isValid = false
+            
+            for value in list {
+                switch value {
+                case let .inside(v):
+                    insides.append(value)
+                    if isABBA(v) {
+                        continue outerLoop
+                    }
+                    
+                case .outside:
+                    outsides.append(value)                    
+                }
+            }
+            
+            innerLoop: for value in outsides {
+                switch value {
+                case .inside:
+                    continue
+                    
+                case let .outside(v):
+                    if isABBA(v) {
+                        isValid = true
+                        break innerLoop
+                    }
+                }
+            }
+            
+            count += isValid ? 1 : 0
+        }
+        
+        return count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        let lists = parse(input)
+        var count = 0
+        
+        outerLoop: for list in lists {
+            var abas = Set<String>()
+            var babs = Set<String>()
+            
+            for value in list {
+                switch value {
+                case let .inside(v):
+                    findBAB(v).forEach { abas.insert($0) }
+                    
+                case let .outside(v):
+                    findABA(v).forEach { babs.insert($0) }
+                }
+            }
+            
+            let found = abas.intersection(babs).count > 0
+            count += found ? 1 : 0
+        }
+        
+        return count
+    }
+    
+    enum Values {
+        case inside(String)
+        case outside(String)
+    }
+}
+
+extension Day7 {
+    func findABA(_ word: String) -> [String] {
+        var abas = [String]()
+        
+        var i = 2
+        while i < word.count {
+            if word[i-2] == word[i] && word[i] != word[i-1] {
+                abas.append(String(word[i-2] + word[i-1] + word[i]))
+            }
+            i += 1
+        }
+        
+        return abas
+    }
+    
+    func findBAB(_ word: String) -> [String] {
+        var abas = [String]()
+        
+        var i = 2
+        while i < word.count {
+            if word[i-2] == word[i] && word[i] != word[i-1] {
+                abas.append(String(word[i-1] + word[i-2] + word[i-1]))
+            }
+            i += 1
+        }
+        
+        return abas
+    }
+    
+    func isABBA(_ word: String) -> Bool {
+        if let result = cache[word] {
+            return result
+        }
+        
+        var i = 3
+        while i < word.count {
+            let a = word[i-3] + word[i-2]
+            let b = word[i] + word[i-1]
+            if a == b && a != String(a.reversed()) {
+                cache[word] = true
+                return true
+            }
+            i += 1
+        }
+        
+        cache[word] = false
+        return false
+    }
+    
+    func parse(_ input: [String]) -> [[Values]] {
+        var results = [[Values]]()
+        
+        for line in input {
+            var values = [Values]()
+            var word = ""
+            var isOutside = true
+            for c in line {
+                switch c {
+                case "[", "]":
+                    isOutside ? values.append(.outside(word)) : values.append(.inside(word))
+                    word = ""
+                    isOutside.toggle()
+                    
+                default:
+                    word.append(c)
+                }
+            }
+            isOutside ? values.append(.outside(word)) : values.append(.inside(word))
+            results.append(values)
+        }
+        
+        return results
     }
 }
--- Tests/Year2016Tests/Day7Tests.swift
+++ Tests/Year2016Tests/Day7Tests.swift
@@ -4,18 +4,38 @@ import Year2016
 
 class Day7Tests: XCTestCase {
     
-    let input = Input("Day7.input", Bundle.module).integers
+    let input = Input("Day7.input", Bundle.module).lines
     let day = Day7()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(115, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn
+""".lines
+        XCTAssertEqual(2, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(231, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb
+""".lines
+        XCTAssertEqual(3, day.part2(input))
     }
 }
