04ad035ca03210edcc3a9a37c00f590064f51dbe
PJ <pjcook@users.noreply.github.com>
2024-12-31 18:52:04 +0000

    AoC 2016 - Day 19

--- Sources/Year2016/Day19.swift
+++ Sources/Year2016/Day19.swift
@@ -11,11 +11,94 @@ import Foundation
 public struct Day19 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: Int) -> Int {
+        let firstElf = Elf(id: 1)
+        var elf = firstElf
+        var i = 2
+        while i <= input {
+            let newElf = Elf(id: i)
+            elf.next = newElf
+            newElf.previous = elf
+            elf = newElf
+            i += 1
+        }
+        elf.next = firstElf
+        firstElf.previous = elf
+        elf = firstElf
+
+        while elf.next != nil {
+            elf = elf.tick()
+        }
+        return elf.id
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    final class Elf {
+        let id: Int
+        var next: Elf?
+        var previous: Elf?
+        
+        init(id: Int) {
+            self.id = id
+        }
+        
+        func tick() -> Elf {
+            guard let stoleFrom = next else { return self }
+            stoleFrom.previous = nil
+            self.next = stoleFrom.next
+            stoleFrom.next = nil
+            guard let next else { return self }
+            next.previous = self
+            return next
+        }
+        
+        func remove() {
+            self.previous?.next = self.next
+            self.next?.previous = self.previous
+            self.next = nil
+            self.previous = nil
+        }
+    }
+    
+    public func part2(_ input: Int) -> Int {
+        let firstElf = Elf(id: 1)
+        var elf = firstElf
+        var i = 2
+        while i <= input {
+            let newElf = Elf(id: i)
+            elf.next = newElf
+            newElf.previous = elf
+            elf = newElf
+            i += 1
+        }
+        elf.next = firstElf
+        firstElf.previous = elf
+        elf = firstElf
+        
+        var elfCount = input
+        var opposite = find(elf: elf, distance: elfCount / 2)
+
+        while elf.next != nil {
+            var nextOpposite = opposite?.next
+            if elfCount.isOdd {
+                nextOpposite = nextOpposite?.next
+            }
+            opposite?.remove()
+            opposite = nextOpposite
+            
+            if let next = elf.next {
+                elf = next
+            }
+            elfCount -= 1
+        }
+        return elf.id
+    }
+    
+    func find(elf: Elf, distance: Int) -> Elf? {
+        var elf = elf
+        for _ in 0..<distance {
+            guard let next = elf.next else { break }
+            elf = next
+        }
+        return elf
     }
 }
--- Tests/Year2016Tests/Day19Tests.swift
+++ Tests/Year2016Tests/Day19Tests.swift
@@ -4,18 +4,25 @@ import Year2016
 
 class Day19Tests: XCTestCase {
     
-    let input = Input("Day19.input", Bundle.module).integers
     let day = Day19()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(1830117, day.part1(3012210))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(3, day.part1(5))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(1417887, day.part2(3012210))
 //        }
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(2, day.part2(5))
     }
 }
