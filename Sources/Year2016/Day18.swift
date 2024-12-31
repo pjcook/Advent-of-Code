e685ced2449ad217d442e998bce6a0c9cfd8a89e
PJ <pjcook@users.noreply.github.com>
2024-12-31 16:25:53 +0000

    AoC 2016 - Day 18

--- Sources/Year2016/Day18.swift
+++ Sources/Year2016/Day18.swift
@@ -11,11 +11,46 @@ import Foundation
 public struct Day18 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: String, rowsToGenerate: Int) -> Int {
+        var previousRow = input.map({ Tile(rawValue: $0)! })
+        var count = previousRow.reduce(0) { $0 + ($1 == .safe ? 1 : 0) }
+        var i = 1
+        
+        while i < rowsToGenerate {
+            var row = [Tile]()
+            for i in 0..<previousRow.count {
+                let l = i > 0 ? previousRow[i-1] : .safe
+                let c = previousRow[i]
+                let r = i < previousRow.count-1 ? previousRow[i+1] : .safe
+                if isTrap(l: l, c: c, r: r) {
+                    row.append(.trap)
+                } else {
+                    row.append(.safe)
+                    count += 1
+                }
+            }
+            previousRow = row
+            i += 1
+        }
+        
+        return count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    enum Tile: Character, CustomDebugStringConvertible {
+        case trap = "^"
+        case safe = "."
+        
+        var debugDescription: String {
+            String(rawValue)
+        }
+    }
+}
+
+extension Day18 {
+    func isTrap(l: Tile, c: Tile, r: Tile) -> Bool {
+        (l == .trap && c == .trap && r == .safe) ||
+        (l == .safe && c == .trap && r == .trap) ||
+        (l == .trap && c == .safe && r == .safe) ||
+        (l == .safe && c == .safe && r == .trap)
     }
 }
--- Tests/Year2016Tests/Day18Tests.swift
+++ Tests/Year2016Tests/Day18Tests.swift
@@ -4,18 +4,22 @@ import Year2016
 
 class Day18Tests: XCTestCase {
     
-    let input = Input("Day18.input", Bundle.module).integers
+    let input = Input("Day18.input", Bundle.module).input
     let day = Day18()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(1951, day.part1(input, rowsToGenerate: 40))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(38, day.part1(".^^.^.^^^^", rowsToGenerate: 10))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(20002936, day.part1(input, rowsToGenerate: 400000))
 //        }
     }
 }
