788002a50a21f944a80a6e5c99db4d6687a9f593
PJ <pjcook@users.noreply.github.com>
2024-12-31 13:26:47 +0000

    AoC 2016 - Day 15

--- Sources/Year2016/Day15.swift
+++ Sources/Year2016/Day15.swift
@@ -11,11 +11,37 @@ import Foundation
 public struct Day15 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let discs = input.map(Disc.init)
+        var i = 0
+        
+        while !check(discs: discs, time: i) {
+            i += 1
+        }
+        
+        return i
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    struct Disc {
+        let id: Int
+        let positions: Int
+        let position: Int
+        
+        init(_ value: String) {
+            // Disc #1 has 5 positions; at time=0, it is at position 4.
+            let values = value
+                .replacingOccurrences(of: "#", with: "")
+                .replacingOccurrences(of: ".", with: "")
+                .split(separator: " ")
+            self.id = Int(values[1])!
+            self.positions = Int(values[3])!
+            self.position = Int(values[11])!
+        }
+    }
+}
+
+extension Day15 {
+    func check(discs: [Disc], time: Int) -> Bool {
+        discs.reduce(0, { $0 + ((time + $1.id + $1.position) % $1.positions) }) == 0
     }
 }
--- Tests/Year2016Tests/Day15Tests.swift
+++ Tests/Year2016Tests/Day15Tests.swift
@@ -4,18 +4,29 @@ import Year2016
 
 class Day15Tests: XCTestCase {
     
-    let input = Input("Day15.input", Bundle.module).integers
+    let input = Input("Day15.input", Bundle.module).lines
     let day = Day15()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(400589, day.part1(input))
 //        }
     }
     
+    func test_part1_example() {
+        let input = """
+Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.
+""".lines
+        XCTAssertEqual(5, day.part1(input))
+    }
+    
     func test_part2() {
+        var input2 = input
+        input2.append("Disc #7 has 11 positions; at time=0, it is at position 0.")
+        
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(3045959, day.part1(input2))
 //        }
     }
 }
