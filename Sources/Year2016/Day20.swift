e00e48be81fca729063eee72383ded0213a3becf
PJ <pjcook@users.noreply.github.com>
2024-12-31 19:42:01 +0000

    AoC 2016 - Day 20

--- Sources/Year2016/Day20.swift
+++ Sources/Year2016/Day20.swift
@@ -7,15 +7,67 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day20 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let sortedRanges = reduce(ranges: parse(input)).sorted(by: { $0.lowerBound < $1.lowerBound })
+        var i = 0
+        var value = 0
+        while i < sortedRanges.count {
+            let range = sortedRanges[i]
+            if range.contains(value) {
+                value = range.upperBound + 1
+            } else {
+                return value
+            }
+            i += 1
+        }
+                
+        return value
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        4294967295 - reduce(ranges: parse(input)).reduce(0) { $0 + ($1.upperBound - $1.lowerBound) + 1 } + 1
+    }
+}
+
+extension Day20 {
+    func parse(_ input: [String]) -> [ClosedRange<Int>] {
+        var ranges = [ClosedRange<Int>]()
+        
+        for line in input {
+            let values = line.split(separator: "-").map { Int($0)! }
+            let range = values.first!...values.last!
+            ranges.append(range)
+        }
+        
+        return ranges
+    }
+    
+    func reduce(ranges: [ClosedRange<Int>]) -> [ClosedRange<Int>] {
+        var ranges = Set(ranges)
+        var results = Set<ClosedRange<Int>>()
+        
+        while ranges.isEmpty == false {
+            let range = ranges.removeFirst()
+            var overlapped = false
+            for r in ranges {
+                if r.overlaps(range) {
+                    ranges.remove(r)
+                    
+                    let newRange = min(r.lowerBound, range.lowerBound)...max(r.upperBound, range.upperBound)
+                    ranges.insert(newRange)
+                    overlapped = true
+                }
+            }
+            if !overlapped {
+                results.insert(range)
+            }
+        }
+        
+        return Array(results)
     }
 }
--- Tests/Year2016Tests/Day20Tests.swift
+++ Tests/Year2016Tests/Day20Tests.swift
@@ -4,18 +4,54 @@ import Year2016
 
 class Day20Tests: XCTestCase {
     
-    let input = Input("Day20.input", Bundle.module).integers
+    let input = Input("Day20.input", Bundle.module).lines
     let day = Day20()
 
     func test_part20() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(19449262, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+5-8
+0-2
+4-7
+""".lines
+        XCTAssertEqual(3, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        // Low  4294967012
+        // High 4294967177
+        XCTAssertEqual(119, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+5-8
+0-2
+4-7
+""".lines
+        XCTAssertEqual(4294967288, day.part2(input))
+    }
+    
+    func test_example() {
+        let range1 = 1...3
+        let range2 = 6...9
+        let range3 = 2...5
+        
+        XCTAssertTrue(range1.overlaps(range3))
+        XCTAssertFalse(range2.overlaps(range1))
+        XCTAssertFalse(range2.overlaps(range3))
+        
+        let range4 = min(range1.lowerBound, range3.lowerBound)...max(range1.upperBound, range3.upperBound)
+        XCTAssertTrue(range1.overlaps(range4))
+        XCTAssertTrue(range3.overlaps(range4))
+        XCTAssertFalse(range2.overlaps(range4))
+        print(range4)
     }
 }
