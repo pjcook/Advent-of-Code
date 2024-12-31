bed3fca0ea89d1a3412baa0f3f21563b7b69fb70
PJ <pjcook@users.noreply.github.com>
2024-12-31 10:16:29 +0000

    AoC 2016 - Day 13

--- Sources/Year2016/Day13.swift
+++ Sources/Year2016/Day13.swift
@@ -7,15 +7,49 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day13 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: Int, size: Point, destination: Point) -> Int {
+        var grid = Grid<String>(size: size, fill: ".")
+        populate(grid: &grid, input: input)
+//        grid.draw()
+        return grid.dijkstra(start: Point(x: 1, y: 1), end: destination, calculateScore: { _ in 1 }, canEnter: { point in grid[point] != "#" })
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: Int, size: Point) -> Int {
+        var grid = Grid<String>(size: size, fill: ".")
+        populate(grid: &grid, input: input)
+        var count = 0
+        
+        for x in 0..<grid.columns {
+            for y in 0..<grid.rows {
+                let point = Point(x: x, y: y)
+                if grid[point] != "#" {
+                    let cost = grid.dijkstra(start: Point(x: 1, y: 1), end: point, calculateScore: { _ in 1 }, canEnter: { point in grid[point] != "#" })
+                    if cost >= 0, cost <= 50 {
+                        count += 1
+                    }
+                }
+            }
+        }
+        
+        return count
+    }
+}
+
+extension Day13 {
+    func populate(grid: inout Grid<String>, input: Int) {
+        for x in 0..<grid.columns {
+            for y in 0..<grid.rows {
+                let point = Point(x: x, y: y)
+                let value = x * x + 3 * x + 2 * x * y + y + y * y + input
+                if value.binary.reduce(0, { $0 + ($1 == "1" ? 1 : 0) }) % 2 != 0 {
+                    grid[point] = "#"
+                }
+            }
+        }
     }
 }
--- Tests/Year2016Tests/Day13Tests.swift
+++ Tests/Year2016Tests/Day13Tests.swift
@@ -1,21 +1,25 @@
 import XCTest
 import InputReader
+import StandardLibraries
 import Year2016
 
 class Day13Tests: XCTestCase {
     
-    let input = Input("Day13.input", Bundle.module).integers
     let day = Day13()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(82, day.part1(1362, size: Point(50,50), destination: Point(31,39)))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(11, day.part1(10, size: Point(10,10), destination: Point(7,4)))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(138, day.part2(1362, size: Point(30,30)))
 //        }
     }
 }
