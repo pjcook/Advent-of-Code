078ad955eb49f4e6c2f4df4eb70daa44d00e527f
PJ <pjcook@users.noreply.github.com>
2025-01-01 16:44:36 +0000

    AoC 2016 - Day 24

--- Sources/Year2016/Day24.swift
+++ Sources/Year2016/Day24.swift
@@ -6,16 +6,39 @@
 //  Created by PJ on 26/11/2022.
 //
 
+import Algorithms
 import Foundation
+import StandardLibraries
 
 public struct Day24 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
-    }
-    
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String], shouldReturn: Bool) -> Int {
+        let grid = Grid<String>(input)
+        let start = grid.point(for: "0")!
+        let pointsToReach = (0...9).compactMap({ grid.point(for: "\($0)") })
+        var distances = [[Point]: Int]()
+        for p1 in pointsToReach {
+            for p2 in pointsToReach {
+                guard p1 != p2, distances[[p1, p2]] == nil else { continue }
+                let distance = grid.dijkstra(start: p1, end: p2, calculateScore: { _ in 1}, canEnter: { grid[$0] != "#" })
+                distances[[p1, p2]] = distance
+                distances[[p2, p1]] = distance
+            }
+        }
+        
+        var smallest = Int.max
+        for x in pointsToReach.permutations() where x.first! == start {
+            var distance = 0
+            for i in 1..<x.count {
+                distance += distances[[x[i - 1], x[i]]]!
+            }
+            if shouldReturn {
+                distance += distances[[x.last!, start]]!
+            }
+            smallest = min(distance, smallest)
+        }
+        
+        return smallest
     }
 }
--- Tests/Year2016Tests/Day24Tests.swift
+++ Tests/Year2016Tests/Day24Tests.swift
@@ -4,18 +4,29 @@ import Year2016
 
 class Day24Tests: XCTestCase {
     
-    let input = Input("Day24.input", Bundle.module).integers
+    let input = Input("Day24.input", Bundle.module).lines
     let day = Day24()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(448, day.part1(input, shouldReturn: false))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+###########
+#0.1.....2#
+#.#######.#
+#4.......3#
+###########
+""".lines
+        XCTAssertEqual(14, day.part1(input, shouldReturn: false))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(672, day.part1(input, shouldReturn: true))
 //        }
     }
 }
