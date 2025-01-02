4d1f7b0f9b0dd9b2861e33abff7e57421898b89b
PJ <pjcook@users.noreply.github.com>
2025-01-02 23:22:55 +0000

    AoC 2017 - Day 1

--- Sources/Year2017/Day1.swift
+++ Sources/Year2017/Day1.swift
@@ -12,10 +12,35 @@ public struct Day1 {
     public init() {}
     
     public func part1(_ input: [Int]) -> Int {
-        return 1
+        var i = 1
+        var count = 0
+        
+        while i < input.count {
+            if input[i-1] == input[i] {
+                count += input[i]
+            }
+            i += 1
+        }
+        
+        if input.first == input.last {
+            count += input[0]
+        }
+        
+        return count
     }
     
     public func part2(_ input: [Int]) -> Int {
-        return 1
+        var dx = input.count / 2
+        var i = 0
+        var count = 0
+        
+        while i < input.count {
+            if input[i] == input[(i + dx) % input.count] {
+                count += input[i]
+            }
+            i += 1
+        }
+        
+        return count
     }
 }
--- Tests/Year2017Tests/Day1Tests.swift
+++ Tests/Year2017Tests/Day1Tests.swift
@@ -4,18 +4,18 @@ import Year2017
 
 class Day1Tests: XCTestCase {
     
-    let input = Input("Day1.input", Bundle.module).integers
+    let input = Input("Day1.input", Bundle.module).input.map({ Int(String($0))! })
     let day = Day1()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(1177, day.part1(input))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(1060, day.part2(input))
 //        }
     }
 }
