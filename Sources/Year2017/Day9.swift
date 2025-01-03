863935b8ca0e6a8f0882d7365252f84fcdadda04
PJ <pjcook@users.noreply.github.com>
2025-01-03 13:26:49 +0000

    AoC 2017 - Day 9

--- Sources/Year2017/Day9.swift
+++ Sources/Year2017/Day9.swift
@@ -11,11 +11,93 @@ import Foundation
 public struct Day9 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: String) -> Int {
+        var index = 0
+        return resolve(input, index: &index)
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: String) -> Int {
+        var index = 0
+        return resolve2(input, index: &index)
+    }
+}
+
+extension Day9 {
+    func resolve(_ input: String, index: inout Int, depth: Int = 0) -> Int {
+        var count = 0
+        var foundGarbage = false
+        var ignoreNext = false
+        
+        outerloop: while index < input.count {
+            switch (ignoreNext, foundGarbage, input[index]) {
+            case (false, false, "{"):
+                index += 1
+                count += resolve(input, index: &index, depth: depth + 1)
+
+            case (false, false, "}"):
+                break outerloop
+                
+            case (false, false, "<"):
+                foundGarbage = true
+
+            case (false, true, ">"):
+                foundGarbage = false
+                
+            case (false, false, ">"):
+                fatalError("oops")
+                
+            case (false, _, "!"):
+                ignoreNext = true
+                index += 1
+                continue
+                
+            default:
+                break
+            }
+            index += 1
+            ignoreNext = false
+        }
+        
+        return count + depth
+    }
+    
+    func resolve2(_ input: String, index: inout Int, depth: Int = 0) -> Int {
+        var count = 0
+        var foundGarbage = false
+        var ignoreNext = false
+        
+        outerloop: while index < input.count {
+            switch (ignoreNext, foundGarbage, input[index]) {
+            case (false, false, "{"):
+                index += 1
+                count += resolve2(input, index: &index, depth: depth + 1)
+
+            case (false, false, "}"):
+                break outerloop
+                
+            case (false, false, "<"):
+                foundGarbage = true
+
+            case (false, true, ">"):
+                foundGarbage = false
+                
+            case (false, false, ">"):
+                fatalError("oops")
+                
+            case (false, _, "!"):
+                ignoreNext = true
+                index += 1
+                continue
+                
+            default:
+                if foundGarbage, !ignoreNext {
+                    count += 1
+                }
+            }
+            index += 1
+            ignoreNext = false
+        }
+        
+        return count
     }
 }
--- Tests/Year2017Tests/Day9Tests.swift
+++ Tests/Year2017Tests/Day9Tests.swift
@@ -4,18 +4,39 @@ import Year2017
 
 class Day9Tests: XCTestCase {
     
-    let input = Input("Day9.input", Bundle.module).integers
+    let input = Input("Day9.input", Bundle.module).input
     let day = Day9()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(9662, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(1, day.part1("{}"))
+        XCTAssertEqual(6, day.part1("{{{}}}"))
+        XCTAssertEqual(5, day.part1("{{},{}}"))
+        XCTAssertEqual(16, day.part1("{{{},{},{{}}}}"))
+        XCTAssertEqual(1, day.part1("{<a>,<a>,<a>,<a>}"))
+        XCTAssertEqual(9, day.part1("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+        XCTAssertEqual(9, day.part1("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
+        XCTAssertEqual(3, day.part1("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(4903, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(0, day.part2("<>"))
+        XCTAssertEqual(17, day.part2("<random characters>"))
+        XCTAssertEqual(3, day.part2("<<<<>"))
+        XCTAssertEqual(2, day.part2("<{!>}>"))
+        XCTAssertEqual(0, day.part2("<!!>"))
+        XCTAssertEqual(0, day.part2("<!!!>>"))
+        XCTAssertEqual(10, day.part2("<{o\"i!a,<{i<a>"))
     }
 }
