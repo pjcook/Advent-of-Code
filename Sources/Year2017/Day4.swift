bd72029b928e0668f8200f8988b5d533a8d6e7b0
PJ <pjcook@users.noreply.github.com>
2025-01-03 00:55:11 +0000

    AoC 2017 - Day 4

--- Sources/Year2017/Day4.swift
+++ Sources/Year2017/Day4.swift
@@ -7,15 +7,43 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day4 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        var count = 0
+        
+        outerloop: for line in input {
+            var words = Set<String>()
+            for word in line.split(separator: " ").map(String.init) {
+                if words.contains(word) {
+                    continue outerloop
+                }
+                words.insert(word)
+            }
+            count += 1
+        }
+        
+        return count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        var count = 0
+        
+        outerloop: for line in input {
+            var words = Set<String>()
+            for word in line.split(separator: " ").map(String.init) {
+                let sortedWord = String(word.sorted())
+                if words.contains(sortedWord) {
+                    continue outerloop
+                }
+                words.insert(sortedWord)
+            }
+            count += 1
+        }
+        
+        return count
     }
 }
--- Tests/Year2017Tests/Day4Tests.swift
+++ Tests/Year2017Tests/Day4Tests.swift
@@ -4,18 +4,18 @@ import Year2017
 
 class Day4Tests: XCTestCase {
     
-    let input = Input("Day4.input", Bundle.module).integers
+    let input = Input("Day4.input", Bundle.module).lines
     let day = Day4()
 
     func test_part4() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(477, day.part1(input))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(167, day.part2(input))
 //        }
     }
 }
