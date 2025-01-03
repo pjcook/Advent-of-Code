2fbebb8af6761124248e3dd0b16df590ff7f6617
PJ <pjcook@users.noreply.github.com>
2025-01-03 16:01:35 +0000

    AoC 2017 - Day 12

--- Sources/Year2017/Day12.swift
+++ Sources/Year2017/Day12.swift
@@ -7,15 +7,95 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day12 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let programs = parse(input)
+        var queue = [[Int]]()
+        queue.append([0])
+        var results = Set<Int>()
+        
+        while queue.isEmpty == false {
+            let list = queue.removeFirst()
+            guard let children = programs[list.last!] else { continue }
+            let remaining = children.filter { !list.contains($0) }
+            guard remaining.isEmpty == false else {
+                for item in list {
+                    results.insert(item)
+                }
+                continue
+            }
+            
+            for c in remaining {
+                queue.append(list + [c])
+            }
+        }
+        
+        return results.count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        let programs = parse(input)
+        var ids = Array(programs.keys)
+        var count = 0
+        while ids.isEmpty == false {
+            let id = ids.removeFirst()
+            let nodes = findNodes(from: id, programs: programs)
+            if !nodes.isEmpty {
+                count += 1
+            }
+            for n in nodes {
+                if let index = ids.firstIndex(of: n) {
+                    ids.remove(at: index)
+                }
+            }
+        }
+        
+        return count
+    }
+}
+
+extension Day12 {
+    func findNodes(from: Int, programs: [Int:[Int]]) -> Set<Int> {
+        var queue = [[Int]]()
+        queue.append([from])
+        var results = Set<Int>()
+        
+        while queue.isEmpty == false {
+            let list = queue.removeFirst()
+            guard let children = programs[list.last!] else { continue }
+            let remaining = children.filter { !list.contains($0) }
+            guard remaining.isEmpty == false else {
+                for item in list {
+                    results.insert(item)
+                }
+                continue
+            }
+            
+            for c in remaining {
+                queue.append(list + [c])
+            }
+        }
+        
+        return results
+    }
+    
+    func parse(_ input: [String]) -> [Int:[Int]] {
+        var results = [Int:[Int]]()
+        
+        for line in input {
+            let parts = line
+                .replacingOccurrences(of: " <-> ", with: ",")
+                .replacingOccurrences(of: ", ", with: ",")
+                .split(separator: ",")
+                .map(String.init)
+                .compactMap(Int.init)
+            results[parts[0]] = Array(parts[1...])
+        }
+        
+        return results
     }
 }
--- Tests/Year2017Tests/Day12Tests.swift
+++ Tests/Year2017Tests/Day12Tests.swift
@@ -4,18 +4,44 @@ import Year2017
 
 class Day12Tests: XCTestCase {
     
-    let input = Input("Day12.input", Bundle.module).integers
+    let input = Input("Day12.input", Bundle.module).lines
     let day = Day12()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(113, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+""".lines
+        XCTAssertEqual(6, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(202, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+""".lines
+        XCTAssertEqual(2, day.part2(input))
     }
 }
