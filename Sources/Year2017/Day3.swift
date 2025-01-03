8d7e15947b42573984a45798264969e422c35da8
PJ <pjcook@users.noreply.github.com>
2025-01-03 00:37:58 +0000

    AoC 2017 - Day 3

--- Sources/Year2017/Day3.swift
+++ Sources/Year2017/Day3.swift
@@ -7,15 +7,72 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day3 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: Int) -> Int {
+        // l*4+4+sum(n-1...)
+        var count = 1
+        var i = 1
+        var length = 1
+        while true {
+            if count + length * 4 + 4 < input {
+                count += length * 4 + 4
+                length += 2
+                i += 1
+            } else {
+                break
+            }
+        }
+        
+        var point = Point(i, i-1)
+        length += 2
+        i = 1
+        count += 1
+        var direction = Direction.up
+        while count != input {
+            point = point + direction.point
+            count += 1
+            i += 1
+            if i == length-1 {
+                direction = direction.rotateLeft()
+                i = 0
+            }
+        }
+        
+        print(point)
+        return point.manhattanDistance(to: .zero)
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: Int) -> Int {
+        var points = [Point.zero: 1]
+        var i = 1
+        var length = 3
+        var point = Point(1,0)
+        var direction = Direction.up
+        
+        while true {
+            points[point] = point.neighbors(true).reduce(0) {
+                $0 + points[$1, default: 0]
+            }
+            print(points[point]!, point.x, point.y, length, i)
+            if points[point]! > input {
+                return points[point]!
+            }
+            point = point + direction.point
+            i += 1
+            if (i == length - 1 && direction != .right) || (direction == .right && i == length) {
+                direction = direction.rotateLeft()
+                i = 0
+                if direction == .up {
+                    length += 2
+                    i += 1
+                }
+            }
+        }
+        
+        return -1
     }
 }
--- Tests/Year2017Tests/Day3Tests.swift
+++ Tests/Year2017Tests/Day3Tests.swift
@@ -4,18 +4,27 @@ import Year2017
 
 class Day3Tests: XCTestCase {
     
-    let input = Input("Day3.input", Bundle.module).integers
     let day = Day3()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(480, day.part1(347991))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(3, day.part1(12))
+        XCTAssertEqual(2, day.part1(23))
+        XCTAssertEqual(31, day.part1(1024))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(349975, day.part2(347991))
 //        }
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(806, day.part2(747))
     }
 }
