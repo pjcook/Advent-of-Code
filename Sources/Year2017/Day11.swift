fda79cbe789bbbdf3a2ee8ecbd01c2cbc98bf228
PJ <pjcook@users.noreply.github.com>
2025-01-03 15:36:48 +0000

    AoC 2017 - Day 11

--- Sources/Year2017/Day11.swift
+++ Sources/Year2017/Day11.swift
@@ -7,15 +7,45 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day11 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: String) -> Int {
+        let steps = input.split(separator: ",").compactMap { HexDirection(rawValue: String($0)) }
+        var position = Point.zero
+        for step in steps {
+            position = step.move(from: position)
+        }
+        
+        return position.manhattanDistance(to: .zero) / 2
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: String) -> Int {
+        let steps = input.split(separator: ",").compactMap { HexDirection(rawValue: String($0)) }
+        var position = Point.zero
+        var furthest = 0
+        for step in steps {
+            position = step.move(from: position)
+            furthest = max(furthest, position.manhattanDistance(to: .zero) / 2)
+        }
+        
+        return furthest
+    }
+    
+    enum HexDirection: String {
+        case ne,nw,n,s,se,sw
+        
+        func move(from currentPosition: Point) -> Point {
+            switch self {
+            case .n: return currentPosition + Point(0,-2)
+            case .s: return currentPosition + Point(0,2)
+            case .ne: return currentPosition + Point(1,-1)
+            case .nw: return currentPosition + Point(-1,-1)
+            case .se: return currentPosition + Point(1,1)
+            case .sw: return currentPosition + Point(-1,1)
+            }
+        }
     }
 }
--- Tests/Year2017Tests/Day11Tests.swift
+++ Tests/Year2017Tests/Day11Tests.swift
@@ -4,18 +4,25 @@ import Year2017
 
 class Day11Tests: XCTestCase {
     
-    let input = Input("Day11.input", Bundle.module).integers
+    let input = Input("Day11.input", Bundle.module).input
     let day = Day11()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(808, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(3, day.part1("ne,ne,ne"))
+        XCTAssertEqual(0, day.part1("ne,ne,sw,sw"))
+        XCTAssertEqual(2, day.part1("ne,ne,s,s"))
+        XCTAssertEqual(3, day.part1("se,sw,se,sw,sw"))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(1556, day.part2(input))
 //        }
     }
 }
