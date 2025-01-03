f8b9eef246054df07abebbea40e89d0bf6d0f05b
PJ <pjcook@users.noreply.github.com>
2025-01-03 10:37:39 +0000

    AoC 2017 - Day 6

--- Sources/Year2017/Day6.swift
+++ Sources/Year2017/Day6.swift
@@ -12,10 +12,56 @@ public struct Day6 {
     public init() {}
     
     public func part1(_ input: [Int]) -> Int {
-        return 1
+        var list = input
+        var count = 0
+        var seen: Set<[Int]> = []
+        
+        while !seen.contains(list) {
+            seen.insert(list)
+            var remainder = findNext(list)
+            var i = list.firstIndex(of: remainder)!
+            list[i] = 0
+            while remainder > 0 {
+                i += 1
+                if i == list.count {
+                    i = 0
+                }
+                list[i] += 1
+                remainder -= 1
+            }
+            count += 1
+        }
+        
+        return count
     }
     
     public func part2(_ input: [Int]) -> Int {
-        return 1
+        var list = input
+        var count = 0
+        var seen: [[Int]: Int] = [:]
+        
+        while seen[list] == nil {
+            seen[list] = count
+            var remainder = findNext(list)
+            var i = list.firstIndex(of: remainder)!
+            list[i] = 0
+            while remainder > 0 {
+                i += 1
+                if i == list.count {
+                    i = 0
+                }
+                list[i] += 1
+                remainder -= 1
+            }
+            count += 1
+        }
+        
+        return count - seen[list]!
+    }
+}
+
+extension Day6 {
+    func findNext(_ list: [Int]) -> Int {
+        list.sorted().last!
     }
 }
--- Tests/Year2017Tests/Day6Tests.swift
+++ Tests/Year2017Tests/Day6Tests.swift
@@ -4,18 +4,26 @@ import Year2017
 
 class Day6Tests: XCTestCase {
     
-    let input = Input("Day6.input", Bundle.module).integers
-    let day = Day1()
+    let input = Input("Day6.input", Bundle.module).input.components(separatedBy: "    ").compactMap(Int.init)
+    let day = Day6()
 
     func test_part6() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(11137, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        XCTAssertEqual(5, day.part1([0,2,7,0]))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(1037, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        XCTAssertEqual(4, day.part2([0,2,7,0]))
     }
 }
