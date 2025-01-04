0a328191caf08771af1431769ee0a74490bc1ab6
PJ <pjcook@users.noreply.github.com>
2025-01-04 23:07:26 +0000

    AoC 2017 - Day 16

--- Sources/Year2017/Day16.swift
+++ Sources/Year2017/Day16.swift
@@ -11,11 +11,94 @@ import Foundation
 public struct Day16 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: String, list: String) -> String {
+        let operations = parse(input)
+        var programs = list.map(Character.init)
+        
+        for op in operations {
+            switch op {
+            case let .spin(a):
+                programs = Array(programs[(programs.count-a)...]) + Array(programs[..<(programs.count-a)])
+                
+            case let .exchance(a, b):
+                let value = programs[a]
+                programs[a] = programs[b]
+                programs[b] = value
+                
+            case let .partner(a, b):
+                let indexA = programs.firstIndex(of: a)!
+                let indexB = programs.firstIndex(of: b)!
+                let value = programs[indexA]
+                programs[indexA] = programs[indexB]
+                programs[indexB] = value
+            }
+        }
+        
+        return programs.map(String.init).joined()
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: String, list: String, count: Int) -> String {
+        let operations = parse(input)
+        var programs = list.map(Character.init)
+        var keys = [String]()
+
+        for _ in 0..<count {
+            let key = programs.map(String.init).joined()
+            if keys.contains(key) {
+                return keys[count % keys.count]
+            }
+            
+            for op in operations {
+                switch op {
+                case let .spin(a):
+                    programs = Array(programs[(programs.count-a)...]) + Array(programs[..<(programs.count-a)])
+                    
+                case let .exchance(a, b):
+                    let value = programs[a]
+                    programs[a] = programs[b]
+                    programs[b] = value
+                    
+                case let .partner(a, b):
+                    let indexA = programs.firstIndex(of: a)!
+                    let indexB = programs.firstIndex(of: b)!
+                    let value = programs[indexA]
+                    programs[indexA] = programs[indexB]
+                    programs[indexB] = value
+                }
+            }
+            
+            keys.append(key)
+        }
+        
+        return programs.map(String.init).joined()
+    }
+    
+    enum Op {
+        case spin(Int)
+        case exchance(Int, Int)
+        case partner(Character, Character)
+        
+        init(_ input: String) {
+            var input = input
+            let value = input.removeFirst()
+            
+            switch value {
+            case "s": self = .spin(Int(input)!)
+            case "x":
+                let parts = input.split(separator: "/").map(String.init).compactMap(Int.init)
+                self = .exchance(parts[0], parts[1])
+            case "p":
+                let parts = input.split(separator: "/").map(Character.init)
+                self = .partner(parts[0], parts[1])
+                
+            default: fatalError("Unknown op \(value)")
+            }
+        }
+    }
+}
+
+extension Day16 {
+    func parse(_ input: String) -> [Op] {
+        input.split(separator: ",").map(String.init).map(Op.init)
     }
 }
--- Tests/Year2017Tests/Day16Tests.swift
+++ Tests/Year2017Tests/Day16Tests.swift
@@ -4,18 +4,36 @@ import Year2017
 
 class Day16Tests: XCTestCase {
     
-    let input = Input("Day16.input", Bundle.module).integers
+    let input = Input("Day16.input", Bundle.module).input
     let day = Day16()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual("nlciboghjmfdapek", day.part1(input, list: "abcdefghijklmnop"))
+//        }
+    }
+    
+    func test_part1_example() {
+//        measure {
+        XCTAssertEqual("baedc", day.part1("s1,x3/4,pe/b", list: "abcde"))
+        XCTAssertEqual("ceadb", day.part1("s1,x3/4,pe/b", list: "baedc"))
 //        }
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        // eagidkbohjfpnmlc
+        // nlciboghjmfdapek
+        // njcihakofgmbpeld
+        // nlciboghmkedpfja
+        // nmcihpaoegkbfjld
+        XCTAssertEqual("nlciboghmkedpfja", day.part2(input, list: "abcdefghijklmnop", count: 1000000000))
+//        }
+    }
+    
+    func test_part2_example() {
+//        measure {
+        XCTAssertEqual("ceadb", day.part2("s1,x3/4,pe/b", list: "abcde", count: 6))
 //        }
     }
 }
