a1b9c8fe954d30eb2e4d0e45811210e8baf17bea
PJ <pjcook@users.noreply.github.com>
2025-01-05 23:25:56 +0000

    AoC 2017 - Day 22

--- Sources/Year2017/Day22.swift
+++ Sources/Year2017/Day22.swift
@@ -7,15 +7,120 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day22 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    typealias System = [Point: Int]
+    
+    public func part1(_ input: [String], bursts: Int) -> Int {
+        var system = parse(input)
+        var point = Point(input.count / 2, input.count / 2)
+        var direction = Direction.up
+        var count = 0
+        
+        for _ in 0..<bursts {
+            if system[point, default: 0] == 0 {
+                system[point] = 1
+                direction = direction.rotateLeft()
+                count += 1
+            } else {
+                system[point] = 0
+                direction = direction.rotateRight()
+            }
+            point = point + direction.point
+//            draw(system: system, point: point, direction: direction)
+        }
+        
+//        draw(system: system, point: point, direction: direction)
+        
+        return count
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    enum State: Int {
+        case clean = 0
+        case infected = 1
+        case weakened = 2
+        case flagged = 3
+        
+        func changeDirection(direction: Direction) -> Direction {
+            switch self {
+            case .clean:
+                return direction.rotateLeft()
+                
+            case .infected:
+                return direction.rotateRight()
+                
+            case .weakened:
+                return direction
+                
+            case .flagged:
+                return direction.rotateLeft().rotateLeft()
+            }
+        }
+        
+        func newState() -> State {
+            switch self {
+            case .clean: .weakened
+            case .infected: .flagged
+            case .weakened: .infected
+            case .flagged: .clean
+            }
+        }
+    }
+    
+    public func part2(_ input: [String], bursts: Int) -> Int {
+        var system = parse(input)
+        var point = Point(input.count / 2, input.count / 2)
+        var direction = Direction.up
+        var count = 0
+        
+        for _ in 0..<bursts {
+            let state = State(rawValue: system[point, default: 0])!
+            direction = state.changeDirection(direction: direction)
+            system[point] = state.newState().rawValue
+            if state == .weakened { count += 1 }
+            point = point + direction.point
+        }
+        
+        return count
+    }
+}
+
+extension Day22 {
+    func draw(system: System, point: Point, direction: Direction) {
+        let (dmin, dmax) = system.keys.extremes()
+        let dx = dmax.x - min(point.x, dmin.x) + 1
+        let dy = dmax.y - min(point.y, dmin.y) + 1
+        let dp = Point(abs(min(point.x, dmin.x)), abs(min(point.y, dmin.y)))
+        var grid = Grid<String>(size: Point(dx, dy), fill: ".")
+        for (point, value) in system where value == 1 {
+            grid[point + dp] = "#"
+        }
+        
+        switch direction {
+        case .up: grid[point + dp] = "^"
+        case .down: grid[point + dp] = "v"
+        case .left: grid[point + dp] = "<"
+        case .right: grid[point + dp] = ">"
+        }
+        
+        print("Final point", grid[point + dp] == "#" ? "filled" : "empty")
+        grid.draw()
+        print()
+    }
+    
+    func parse(_ input: [String]) -> System {
+        var system: System = [:]
+        
+        for y in 0..<input.count {
+            let row = input[y]
+            for x in 0..<row.count {
+                system[Point(x,y)] = row[x] == "#" ? 1 : 0
+            }
+        }
+        
+        return system
     }
 }
--- Tests/Year2017Tests/Day22Tests.swift
+++ Tests/Year2017Tests/Day22Tests.swift
@@ -4,18 +4,37 @@ import Year2017
 
 class Day22Tests: XCTestCase {
     
-    let input = Input("Day22.input", Bundle.module).integers
+    let input = Input("Day22.input", Bundle.module).lines
     let day = Day22()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(5256, day.part1(input, bursts: 10000))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+..#
+#..
+...
+""".lines
+        XCTAssertEqual(41, day.part1(input, bursts: 70))
+        XCTAssertEqual(5587, day.part1(input, bursts: 10000))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(2511345, day.part2(input, bursts: 10000000))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+..#
+#..
+...
+""".lines
+        XCTAssertEqual(2511944, day.part2(input, bursts: 10000000))
     }
 }
