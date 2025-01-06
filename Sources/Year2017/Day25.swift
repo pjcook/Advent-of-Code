eb34bac5d21871e2e63df67250dc30b6470bd47c
PJ <pjcook@users.noreply.github.com>
2025-01-06 10:00:37 +0000

    AoC 2017 - Day 25

--- Sources/Year2017/Day25.swift
+++ Sources/Year2017/Day25.swift
@@ -11,11 +11,40 @@ import Foundation
 public struct Day25 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ states: [String: State], steps: Int) -> Int {
+        var cursor = 0
+        var tape: [Int: Int] = [0:0]
+        var state = states["A"]!
+        
+        for _ in 0..<steps {
+            let instruction = state.instructions[tape[cursor, default: 0]]
+            tape[cursor] = instruction.write
+            cursor += instruction.move
+            state = states[instruction.nextState]!
+        }
+        
+        return tape.values.reduce(0, +)
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public struct Instruction {
+        public let write: Int
+        public let move: Int
+        public let nextState: String
+        
+        public init(write: Int, move: Int, nextState: String) {
+            self.write = write
+            self.move = move
+            self.nextState = nextState
+        }
+    }
+    
+    public struct State {
+        public let id: String
+        public let instructions: [Instruction]
+        
+        public init(id: String, instructions: [Instruction]) {
+            self.id = id
+            self.instructions = instructions
+        }
     }
 }
--- Tests/Year2017Tests/Day25Tests.swift
+++ Tests/Year2017Tests/Day25Tests.swift
@@ -4,18 +4,84 @@ import Year2017
 
 class Day25Tests: XCTestCase {
     
-    let input = Input("Day25.input", Bundle.module).integers
+    let input = Input("Day25.input", Bundle.module).lines
     let day = Day25()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        let stateA = Day25.State(id: "A", instructions: [
+            Day25.Instruction(write: 1, move: 1, nextState: "B"),
+            Day25.Instruction(write: 0, move: -1, nextState: "C"),
+        ])
+        let stateB = Day25.State(id: "B", instructions: [
+            Day25.Instruction(write: 1, move: -1, nextState: "A"),
+            Day25.Instruction(write: 1, move: -1, nextState: "D"),
+        ])
+        let stateC = Day25.State(id: "C", instructions: [
+            Day25.Instruction(write: 1, move: 1, nextState: "D"),
+            Day25.Instruction(write: 0, move: 1, nextState: "C"),
+        ])
+        let stateD = Day25.State(id: "D", instructions: [
+            Day25.Instruction(write: 0, move: -1, nextState: "B"),
+            Day25.Instruction(write: 0, move: 1, nextState: "E"),
+        ])
+        let stateE = Day25.State(id: "E", instructions: [
+            Day25.Instruction(write: 1, move: 1, nextState: "C"),
+            Day25.Instruction(write: 1, move: -1, nextState: "F"),
+        ])
+        let stateF = Day25.State(id: "F", instructions: [
+            Day25.Instruction(write: 1, move: -1, nextState: "E"),
+            Day25.Instruction(write: 1, move: 1, nextState: "A"),
+        ])
+        let states = [
+            "A": stateA,
+            "B": stateB,
+            "C": stateC,
+            "D": stateD,
+            "E": stateE,
+            "F": stateF,
+        ]
+        XCTAssertEqual(2474, day.part1(states, steps: 12172063))
 //        }
     }
     
-    func test_part2() {
-//        measure {
-        XCTAssertEqual(1, day.part2(input))
-//        }
+    func test_part1_example() {
+        let input = """
+Begin in state A.
+Perform a diagnostic checksum after 6 steps.
+
+In state A:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state B.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state B.
+
+In state B:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.
+""".lines
+        let stateA = Day25.State(id: "A", instructions: [
+            Day25.Instruction(write: 1, move: 1, nextState: "B"),
+            Day25.Instruction(write: 0, move: -1, nextState: "B"),
+        ])
+        let stateB = Day25.State(id: "B", instructions: [
+            Day25.Instruction(write: 1, move: -1, nextState: "A"),
+            Day25.Instruction(write: 1, move: 1, nextState: "A"),
+        ])
+        let states = [
+            "A": stateA,
+            "B": stateB
+        ]
+        XCTAssertEqual(3, day.part1(states, steps: 6))
     }
 }
