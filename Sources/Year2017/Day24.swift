5b70b54ee2fed7209fa875f00cf73f70faa89c8d
PJ <pjcook@users.noreply.github.com>
2025-01-06 09:12:41 +0000

    AoC 2017 - Day 24

--- Sources/Year2017/Day24.swift
+++ Sources/Year2017/Day24.swift
@@ -7,15 +7,92 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day24 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let ports = Set(input.map {
+            $0.split(separator: "/").map(String.init).compactMap(Int.init).sorted()
+        })
+        
+        var strongest = 0
+        let queue = PriorityQueue<Bridge>()
+        var seen = Set<Bridge>()
+        for port in ports where port[0] == 0 {
+            queue.enqueue(Bridge(list: [port], end: port.last!), priority: 1)
+        }
+        
+        while !queue.isEmpty {
+            let bridge = queue.dequeue()!
+            guard !seen.contains(bridge) else { continue }
+            seen.insert(bridge)
+            if bridge.strength > strongest {
+                strongest = bridge.strength
+                print(strongest, queue.queuedItems.count, bridge.list.count)
+            }
+            
+            let next = ports.filter {
+                !bridge.list.contains($0) && ( $0.first == bridge.end || $0.last == bridge.end)
+            }
+            for n in next {
+                queue.enqueue(Bridge(list: bridge.list + [n], end: n.first(where: { $0 != bridge.end }) ?? n.last!), priority: strongest - bridge.strength)
+            }
+        }
+        
+        return strongest
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    public func part2(_ input: [String]) -> Int {
+        let ports = Set(input.map {
+            $0.split(separator: "/").map(String.init).compactMap(Int.init).sorted()
+        })
+        
+        var longest = 0
+        var longestStrongestBridge: Bridge?
+        let queue = PriorityQueue<Bridge>()
+        var seen = Set<Bridge>()
+        for port in ports where port[0] == 0 {
+            queue.enqueue(Bridge(list: [port], end: port.last!), priority: 1)
+        }
+        
+        while !queue.isEmpty {
+            let bridge = queue.dequeue()!
+            guard !seen.contains(bridge) else { continue }
+            seen.insert(bridge)
+            if bridge.list.count > longest || (bridge.list.count == longest && longestStrongestBridge?.strength ?? 0 < bridge.strength) {
+                longestStrongestBridge = bridge
+                longest = bridge.list.count
+                print(longest, queue.queuedItems.count, bridge.strength, longestStrongestBridge?.strength ?? -1)
+            }
+            
+            let next = ports.filter {
+                !bridge.list.contains($0) && ( $0.first == bridge.end || $0.last == bridge.end)
+            }
+            for n in next {
+                queue.enqueue(Bridge(list: bridge.list + [n], end: n.first(where: { $0 != bridge.end }) ?? n.last!), priority: -bridge.list.count)
+            }
+        }
+        
+        return longestStrongestBridge?.strength ?? -1
+    }
+    
+    struct Bridge: Hashable {
+        let list: [[Int]]
+        let end: Int
+        let strength: Int
+        
+        init(list: [[Int]], end: Int) {
+            self.list = list
+            self.end = end
+            self.strength = list.reduce([Int](), +).reduce(0, +)
+        }
+        
+        init(list: [[Int]], end: Int, score: Int) {
+            self.list = list
+            self.end = end
+            self.strength = score
+        }
     }
 }
--- Tests/Year2017Tests/Day24Tests.swift
+++ Tests/Year2017Tests/Day24Tests.swift
@@ -1,21 +1,50 @@
 import XCTest
 import InputReader
+import StandardLibraries
 import Year2017
 
 class Day24Tests: XCTestCase {
     
-    let input = Input("Day24.input", Bundle.module).integers
+    let input = Input("Day24.input", Bundle.module).lines
     let day = Day24()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        XCTAssertEqual(1695, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+""".lines
+        XCTAssertEqual(31, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(1673, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+""".lines
+        XCTAssertEqual(19, day.part2(input))
     }
 }
