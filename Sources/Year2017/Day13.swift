a0ba9849b6a23c6c11e435a769ffff6606e5bf76
PJ <pjcook@users.noreply.github.com>
2025-01-06 11:05:56 +0000

    AoC 2017 - Day 13

--- Sources/Year2017/Day13.swift
+++ Sources/Year2017/Day13.swift
@@ -7,15 +7,96 @@
 //
 
 import Foundation
+import StandardLibraries
 
 public struct Day13 {
     public init() {}
     
-    public func part1(_ input: [Int]) -> Int {
-        return 1
+    public func part1(_ input: [String]) -> Int {
+        let nodes = input.map(Node.init)
+        var severity = 0
+        
+        for node in nodes {
+            guard node.position > 0 else {
+                severity += node.position * node.length
+                continue
+            }
+            
+            var pointer = 0
+            var dx = 1
+            for _ in 0..<node.position {
+                pointer += dx
+                if pointer == 0 {
+                    dx = 1
+                } else if pointer == node.length-1 {
+                    dx = -1
+                }
+            }
+            if pointer == 0 {
+                severity += node.position * node.length
+            }
+        }
+        
+        return severity
     }
     
-    public func part2(_ input: [Int]) -> Int {
-        return 1
+    final class NodeWrapper {
+        let node: Node
+        var pointer = 0
+        var dx = 1
+        
+        init(_ node: Node) {
+            self.node = node
+            for _ in 0..<node.position {
+                pointer += dx
+                if pointer == 0 {
+                    dx = 1
+                } else if pointer == node.length-1 {
+                    dx = -1
+                }
+            }
+        }
+        
+        func tick() {
+            pointer += dx
+            if pointer == 0 {
+                dx = 1
+            } else if pointer == node.length-1 {
+                dx = -1
+            }
+        }
+    }
+    
+    public func part2(_ input: [String]) -> Int {
+        let nodes = input.map(Node.init).map(NodeWrapper.init)
+        var delay = 1
+        
+        while true {
+            var failed = false
+            
+            for node in nodes {
+                node.tick()
+                if node.pointer == 0 {
+                    failed = true
+                }
+            }
+            if failed == false {
+                break
+            }
+            delay += 1
+        }
+        
+        return delay
+    }
+    
+    struct Node {
+        let position: Int
+        let length: Int
+        
+        init(_ value: String) {
+            let parts = value.split(separator: ": ").map(String.init).compactMap(Int.init)
+            position = parts[0]
+            length = parts[1]
+        }
     }
 }
--- Tests/Year2017Tests/Day13Tests.swift
+++ Tests/Year2017Tests/Day13Tests.swift
@@ -4,18 +4,41 @@ import Year2017
 
 class Day13Tests: XCTestCase {
     
-    let input = Input("Day13.input", Bundle.module).integers
+    let input = Input("Day13.input", Bundle.module).lines
     let day = Day13()
 
     func test_part1() {
 //        measure {
-        XCTAssertEqual(1, day.part1(input))
+        // low  110
+        // low  142
+        // high 56474
+        XCTAssertEqual(1476, day.part1(input))
 //        }
+    }
+    
+    func test_part1_example() {
+        let input = """
+0: 3
+1: 2
+4: 4
+6: 4
+""".lines
+        XCTAssertEqual(24, day.part1(input))
     }
     
     func test_part2() {
 //        measure {
-        XCTAssertEqual(1, day.part2(input))
+        XCTAssertEqual(3937334, day.part2(input))
 //        }
+    }
+    
+    func test_part2_example() {
+        let input = """
+0: 3
+1: 2
+4: 4
+6: 4
+""".lines
+        XCTAssertEqual(10, day.part2(input))
     }
 }
