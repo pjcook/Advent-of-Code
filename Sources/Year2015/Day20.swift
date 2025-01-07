4ba4308742bb4679fe17d5c60e112add223c568e
PJ <pjcook@users.noreply.github.com>
2025-01-07 00:27:14 +0000

    AoC 2015 - Day 20

--- Sources/Year2015/Day20.swift
+++ Sources/Year2015/Day20.swift
@@ -5,26 +5,52 @@ public struct Day20 {
     public init() {}
     
     public func part1(_ input: Int, start: Int = 1) -> Int {
-        var result = 0
-        var count = start
-        while result != input {
+        var i = 0
+        let a = 856800
+        var lowest = Int.max
+        for count in (0...a).reversed() {
+            i += 1
             var newResult = 0
-            for i in (1..<(count/2)+1) {
+            let range: [Int] = Array(1...max(1,count/2)) + [count]
+            for i in range {
                 if count % i == 0 {
-//                    print(count, i, count / i * 10)
-                    newResult += count / i * 10
+                    newResult += i * 10
                 }
             }
-            newResult += 10 // For the current number
-            result = newResult
-//            print("-", count, result)
-//            print()
-            count += 1
+            if newResult >= input {
+                print(count, newResult, newResult >= input)
+                lowest = min(lowest, count)
+            }
+            if lowest - count > 50000 {
+                break
+            }
         }
-        return count-1
+        return lowest
     }
     
     public func part2(_ input: Int) -> Int {
-        return 0
+        var i = 0
+        let a = 850000
+        var lowest = Int.max
+        for count in a...Int.max {
+            i += 1
+            var newResult = 0
+            let rangeMax = count/2
+            let rangeMin = max(1, min(count / 50, rangeMax))
+            let range: [Int] = Array(rangeMin...rangeMax) + [count]
+            for i in range {
+                if (count - (i * 50)) <= i, count % i == 0 {
+                    newResult += i * 11
+                }
+            }
+            if newResult >= input {
+                print(count, newResult, newResult >= input)
+                lowest = min(lowest, count)
+            }
+            if lowest != Int.max {
+                break
+            }
+        }
+        return lowest
     }
 }
--- Tests/Year2015Tests/Day20Tests.swift
+++ Tests/Year2015Tests/Day20Tests.swift
@@ -7,16 +7,46 @@ class Day20Tests: XCTestCase {
     let day = Day20()
 
     func test_part1() {
-        // too high 1854765
-        // too high 1049160
-        XCTAssertEqual(1049160, day.part1(36000000))
+        XCTAssertEqual(831600, day.part1(36000000))
+    }
+    
+    func test_part1_example() {
+        var i = 0
+        let a = 856800
+        var lowest = Int.max
+        for count in (0...a).reversed() {
+            i += 1
+            var newResult = 0
+            let range: [Int] = Array(1...max(1,count/2)) + [count]
+            for i in range {
+                if count % i == 0 {
+                    newResult += i * 10
+                }
+            }
+            if newResult >= 36000000 {
+                print(count, newResult, newResult >= 36000000)
+                lowest = min(lowest, count)
+            }
+            if lowest - count > 50000 {
+                break
+            }
+        }
+        XCTAssertEqual(831600, lowest)
     }
     
     func test_part2() {
-        XCTAssertEqual(0, day.part2(36000000))
+        XCTAssertEqual(884520, day.part2(36000000))
     }
     
-    func test_example() {
-        XCTAssertEqual(8, day.part1(150))
-    }
+//    func test_example() {
+//        XCTAssertEqual(1, day.part1(10))
+//        XCTAssertEqual(2, day.part1(30))
+//        XCTAssertEqual(3, day.part1(40))
+//        XCTAssertEqual(4, day.part1(70))
+//        XCTAssertEqual(4, day.part1(60))
+//        XCTAssertEqual(6, day.part1(120))
+//        XCTAssertEqual(6, day.part1(80))
+//        XCTAssertEqual(8, day.part1(150))
+//        XCTAssertEqual(8, day.part1(130))
+//    }
 }
