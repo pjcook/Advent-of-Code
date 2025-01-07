fdf194b0ee62fe66a4eb0880bd3ad39bd7b2f097
PJ <pjcook@users.noreply.github.com>
2025-01-07 20:47:13 +0000

    AoC 2015 - Day 25

--- Sources/Year2015/Day25.swift
+++ Sources/Year2015/Day25.swift
@@ -4,11 +4,18 @@ import StandardLibraries
 public struct Day25 {
     public init() {}
     
-    public func part1(_ input: [String]) -> Int {
-        return 0
+    public func part1(_ input: Point) -> Int {
+        let index = convert(point: input)
+        return (1..<index).reduce(20151125) { p, _ in
+            calculate(value: p)
+        }
     }
     
-    public func part2(_ input: [String]) -> Int {
-        return 0
+    public func convert(point: Point) -> Int {
+        (0..<point.y-1).reduce((1...point.x).reduce(0, +)) { $0 + point.x + $1}
+    }
+    
+    public func calculate(value: Int) -> Int {
+        value * 252533 % 33554393
     }
 }
--- Tests/Year2015Tests/Day25Tests.swift
+++ Tests/Year2015Tests/Day25Tests.swift
@@ -1,17 +1,31 @@
 import XCTest
 import InputReader
+import StandardLibraries
 import Year2015
 
 class Day25Tests: XCTestCase {
     
-    let input = Input("Day25.input", .module).lines
     let day = Day25()
 
     func test_part1() {
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(19980801, day.part1(Point(3029,2947)))
     }
     
-    func test_part2() {
-        XCTAssertEqual(0, day.part2(input))
+    func test_part1_example() {
+        XCTAssertEqual(21345942, day.part1(Point(3,4)))
+    }
+    
+    func test_calculate() {
+        XCTAssertEqual(18, day.convert(point: Point(3,4)))
+        XCTAssertEqual(6, (1...3).reduce(0, +))
+        XCTAssertEqual(31916031, day.calculate(value: 20151125))
+    }
+    
+    func test_stuff() {
+        let point = Point(3,4)
+        let x = (1...point.x).reduce(0, +)
+        XCTAssertEqual(6, x)
+        let dy = (0..<point.y-1).reduce(x) { $0 + point.x + $1}
+        XCTAssertEqual(18, dy)
     }
 }
