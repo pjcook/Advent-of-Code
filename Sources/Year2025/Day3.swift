d09dd76cefe755fc0b804789b4f6db5de3fb8db6
PJ <pjcook@users.noreply.github.com>
2025-12-03 14:59:09 +0000

    2025 Day 3

--- Sources/Year2025/Day3.swift
+++ Sources/Year2025/Day3.swift
@@ -5,10 +5,80 @@ public struct Day3 {
     public init() {}
 
     public func part1(_ input: [String]) -> Int {
-        0
+        parse(input)
+            .map { findLargestJoltage(input: $0, count: 2) }
+            .reduce(0, +)
     }
 
     public func part2(_ input: [String]) -> Int {
-        0
+        parse(input)
+            .map { findLargestJoltage(input: $0, count: 12) }
+            .reduce(0, +)
+    }
+
+    struct Info {
+        let value: Int
+        let index: Int
+    }
+}
+
+extension Day3 {
+    func findLargestJoltage(input: [Int], count: Int) -> Int {
+        var values = [Info]()
+        var indexesToIgnore = [Int]()
+        var lowestIndex = 0
+
+        // Find the index of the first largest value at a valid index
+        let range = lowestIndex..<input.count - count + 1
+        let initialInfo = findLargestInRange(input: input, range: range, ignoringIndexes: [])
+        values.append(initialInfo)
+        indexesToIgnore.append(initialInfo.index)
+        lowestIndex = initialInfo.index
+
+        // Calculate the remaining values
+        while values.count < count {
+            let range = lowestIndex..<input.count - count + 1 + values.count
+            let info = findLargestInRange(input: input, range: range, ignoringIndexes: indexesToIgnore)
+            let stillNeed = count - 1 - values.count + indexesToIgnore.filter { $0 > lowestIndex }.count
+            let lastIndex = input.count - stillNeed
+
+            if info.index <= lastIndex {
+                lowestIndex = info.index
+            }
+
+            values.append(info)
+            indexesToIgnore.append(info.index)
+        }
+
+        // Convert results to value
+        return Int(values
+            .sorted(by: { $0.index < $1.index })
+            .map { $0.value }
+            .map(String.init)
+            .joined()
+        )!
+    }
+
+    func findLargestInRange(input: [Int], range: Range<Int>, ignoringIndexes: [Int]) -> Info {
+        var largest = 0
+        var largestIndex = range.lowerBound
+
+        for i in range where !ignoringIndexes.contains(i) {
+            if input[i] > largest {
+                largest = input[i]
+                largestIndex = i
+                if largest == 9 {
+                    break   // exit early, can't get any higher, excessive optimisation that makes 0.0001s difference to part 2
+                }
+            }
+        }
+
+        return Info(value: largest, index: largestIndex)
+    }
+
+    func parse(_ input: [String]) -> [[Int]] {
+        input.map { line in
+            line.compactMap { Int(String($0)) }
+        }
     }
 }
--- Tests/Year2025Tests/Day3Tests.swift
+++ Tests/Year2025Tests/Day3Tests.swift
@@ -9,27 +9,83 @@ class Day3Tests: XCTestCase {
 
     func test_part1() throws {
 //        measure {
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(17166, day.part1(input))
 //        }
     }
     
     func test_part1_example() throws {
         let input = """
-0
+987654321111111
+811111111111119
+234234234234278
+818181911112111
 """.lines
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(357, day.part1(input))
     }
-    
+
+    func test_part1_example2() throws {
+        let input = """
+3432324344242432243242442431213424312243322133232222316433433435212164332223444243334342533334253482
+""".lines
+        XCTAssertEqual(82, day.part1(input))
+    }
+
+
     func test_part2() throws {
 //        measure {
-        XCTAssertEqual(0, day.part2(input))
+        XCTAssertEqual(169077317650774, day.part2(input))
 //        }
     }
     
     func test_part2_example() throws {
         let input = """
-0
+987654321111111
+811111111111119
+234234234234278
+818181911112111
 """.lines
-        XCTAssertEqual(0, day.part2(input))
+        XCTAssertEqual(3121910778619, day.part2(input))
+    }
+
+    func test_part2_example2() throws {
+        let input = """
+234234234234278
+""".lines
+        XCTAssertEqual(434234234278, day.part2(input))
+    }
+
+    func test_part2_example3() throws {
+        let input = """
+818181911112111
+""".lines
+        XCTAssertEqual(888911112111, day.part2(input))
+    }
+
+    func test_part2_example4() throws {
+        let input = """
+4426546555433545424424345444644242452452532444564422646557424354153538454225332755435544545533324152
+""".lines
+        XCTAssertEqual(875555555452, day.part2(input))
+    }
+
+    func test_part2_example5() throws {
+        let input = """
+4325282447422434333445212333343451413231333423353426332541123254243232342322553322244341312543932333
+""".lines
+        XCTAssertEqual(876555932333, day.part2(input))
+    }
+
+    func test_part2_example6() throws {
+        let input = """
+7233222122232253233222242323323223314121222243233553222212213232332233632222221633531222213332222322
+""".lines
+        XCTAssertEqual(766533332322, day.part2(input))
+    }
+
+    func test_part2_example7() throws {
+        let input = """
+2542562623212274282227172127123225532255466656628222232244712231262232124222523356442314912654421246
+""".lines
+        XCTAssertEqual(912654421246, day.part2(input))
     }
 }
