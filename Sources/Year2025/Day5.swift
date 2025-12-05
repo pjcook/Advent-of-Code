f684f90f4a6861ad0f841918cf060a7465daa9e2
PJ <pjcook@users.noreply.github.com>
2025-12-05 08:47:43 +0000

    2025 Day 5

--- Sources/Year2025/Day5.swift
+++ Sources/Year2025/Day5.swift
@@ -5,10 +5,64 @@ public struct Day5 {
     public init() {}
 
     public func part1(_ input: [String]) -> Int {
-        0
+        var freshIngredients = 0
+        let (ranges, ingredients) = parse(input)
+
+        for ingredient in ingredients {
+            for range in ranges {
+                if range.contains(ingredient) {
+                    freshIngredients += 1
+                    break
+                }
+            }
+        }
+
+        return freshIngredients
     }
 
     public func part2(_ input: [String]) -> Int {
-        0
+        let (ranges, _) = parse(input)
+        return ranges.reduce(0) { $0 + $1.upperBound - $1.lowerBound + 1 }
+    }
+}
+
+extension Day5 {
+    func parse(_ input: [String]) -> ([ClosedRange<Int>], [Int]) {
+        var ranges: [ClosedRange<Int>] = []
+        var ids: [Int] = []
+        var parseRanges = true
+
+        mainLoop: for line in input {
+            if line.isEmpty {
+                parseRanges = false
+                continue
+            }
+
+            if parseRanges {
+                let parts = line.split(separator: "-").map(String.init).map(Int.init)
+                var range = parts[0]!...parts[1]!
+                var overlaps: [ClosedRange<Int>] = []
+                var indexesToRemove: [Int] = []
+
+                for i in 0..<ranges.count {
+                    let a = ranges[i]
+                    if a.overlaps(range) {
+                        indexesToRemove.append(i)
+                        overlaps.append(a)
+                    }
+                }
+
+                for i in indexesToRemove.reversed() {
+                    ranges.remove(at: i)
+                }
+
+                range = overlaps.reduce(range) { min($0.lowerBound, $1.lowerBound)...max($0.upperBound, $1.upperBound) }
+                ranges.append(range)
+            } else {
+                ids.append(Int(line)!)
+            }
+        }
+
+        return (ranges, ids)
     }
 }
--- Tests/Year2025Tests/Day5Tests.swift
+++ Tests/Year2025Tests/Day5Tests.swift
@@ -9,28 +9,48 @@ class Day5Tests: XCTestCase {
     let day = Day5()
 
     func test_part1() throws {
-        //        measure {
-        XCTAssertEqual(0, day.part1(input))
-        //        }
+//                measure {
+        XCTAssertEqual(525, day.part1(input))
+//                }
     }
 
     func test_part1_example() throws {
         let input = """
-0
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
 """.lines
-        XCTAssertEqual(0, day.part1(input))
+        XCTAssertEqual(3, day.part1(input))
     }
 
     func test_part2() throws {
-        //        measure {
-        XCTAssertEqual(0, day.part2(input))
-        //        }
+//                measure {
+        XCTAssertEqual(333892124923577, day.part2(input))
+//                }
     }
 
     func test_part2_example() throws {
         let input = """
-0
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
 """.lines
-        XCTAssertEqual(0, day.part2(input))
+        XCTAssertEqual(14, day.part2(input))
     }
 }
