4e7f2bca6798515b129a70170bc65defce0cb554
PJ <pjcook@users.noreply.github.com>
2025-12-08 11:27:34 +0000

    2025 Day8 remove duplicate code

--- Sources/Year2025/Day8.swift
+++ Sources/Year2025/Day8.swift
@@ -17,39 +17,39 @@ public struct Day8 {
             v1.distance(to: v2)
         }
     }
-
+    
     public func part1(_ input: [String], iterations: Int) -> Int {
         let boxes = parse(input).sorted(by: <)
-        var pairs: Set<Pair> = []
-
-        for i in 0..<boxes.count {
-            let v1 = boxes[i]
-            for j in i+1..<boxes.count {
-                let v2 = boxes[j]
-                pairs.insert(Pair(v1, v2))
-            }
-        }
-
-        let sortedDistances = pairs.sorted { $0.distance < $1.distance }
+        let sortedPairs = sortedPairs(of: boxes)
         var groups: [Set<Vector>] = []
 
         for i in 0..<iterations {
-            let pair = sortedDistances[i]
-            var newGroup = Set([pair.v1, pair.v2])
-            var newGroups: [Set<Vector>] = []
-
-            for group in groups {
-                if group.intersection(newGroup).count > 0 {
-                    newGroup = newGroup.union(group)
-                } else {
-                    newGroups.append(group)
-                }
-            }
-
-            newGroups.append(newGroup)
-            groups = newGroups
+            let pair = sortedPairs[i]
+            reduceGroups(with: pair, &groups)
         }
 
+        return calculatePart1Total(groups)
+    }
+
+    public func part2(_ input: [String]) -> Int {
+        let boxes = parse(input)
+        let sortedPairs = sortedPairs(of: boxes)
+        var groups: [Set<Vector>] = []
+
+        mainLoop: for pair in sortedPairs {
+            reduceGroups(with: pair, &groups)
+
+            if groups.count == 1, groups.first!.count == boxes.count {
+                return pair.v1.x * pair.v2.x
+            }
+        }
+        
+        return 0 // Failed
+    }
+}
+
+extension Day8 {
+    func calculatePart1Total(_ groups: [Set<Vector>]) -> Int {
         let results = groups.map { $0.count }.sorted(by: >)
         var total = 1
 
@@ -59,9 +59,8 @@ public struct Day8 {
 
         return total
     }
-
-    public func part2(_ input: [String]) -> Int {
-        let boxes = parse(input)
+    
+    func sortedPairs(of boxes: [Vector]) -> [Pair] {
         var pairs: Set<Pair> = []
 
         for i in 0..<boxes.count {
@@ -72,34 +71,25 @@ public struct Day8 {
             }
         }
 
-        let sortedDistances = pairs.sorted { $0.distance < $1.distance }
-        var groups: [Set<Vector>] = []
+        return pairs.sorted { $0.distance < $1.distance }
+    }
 
-        mainLoop: for pair in sortedDistances {
-            var newGroup = Set([pair.v1, pair.v2])
-            var newGroups: [Set<Vector>] = []
+    func reduceGroups(with pair: Day8.Pair, _ groups: inout [Set<Vector>]) {
+        var newGroup = Set([pair.v1, pair.v2])
+        var newGroups: [Set<Vector>] = []
 
-            for group in groups {
-                if group.intersection(newGroup).count > 0 {
-                    newGroup = newGroup.union(group)
-                } else {
-                    newGroups.append(group)
-                }
-            }
-
-            newGroups.append(newGroup)
-            groups = newGroups
-
-            if groups.count == 1, groups.first!.count == boxes.count {
-                return pair.v1.x * pair.v2.x
+        for group in groups {
+            if group.intersection(newGroup).count > 0 {
+                newGroup = newGroup.union(group)
+            } else {
+                newGroups.append(group)
             }
         }
-        
-        return 0
-    }
-}
 
-extension Day8 {
+        newGroups.append(newGroup)
+        groups = newGroups
+    }
+
     func parse(_ input: [String]) -> [Vector] {
         input.map {
             $0.components(separatedBy: ",")
